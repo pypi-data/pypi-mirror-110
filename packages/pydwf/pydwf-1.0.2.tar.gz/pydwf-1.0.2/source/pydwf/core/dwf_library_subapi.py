b473b3ac884d16b53fdb1661a918e0d1414edd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package pydwf/1.0.2/pydwf-1.0.2.tar.gz

---
+++ packages/pydwf/pydwf-1.0.2.tar.gz/pydwf-1.0.2/source/pydwf/core/dwf_library_subapi.py
@@ -0,0 +1,35 @@
+"""The |pydwf.core.dwf_library_subapi| module implements a single class: |AbstractDwfLibrarySubApi|."""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pydwf.core.dwf_library import DwfLibrary
+
+
+class AbstractDwfLibrarySubApi:
+    """Abstract base class for the sub-API attributes of a |DwfLibrary|."""
+
+    def __init__(self, dwf: 'DwfLibrary'):
+        self._dwf = dwf
+
+    @property
+    def dwf(self):
+        """Return the |DwfLibrary| instance of which we are an attribute.
+
+        Returns:
+            DwfLibrary: The |DwfLibrary| instance.
+
+        :meta private:
+        """
+        return self._dwf
+
+    @property
+    def lib(self):
+        """Return the |ctypes:link| library.
+
+        Returns:
+            ctypes.CDLL: The |ctypes| library.
+
+        :meta private:
+        """
+        return self.dwf.lib
