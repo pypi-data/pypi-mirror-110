d193ae4b358f584705be9b14114086a2e390a3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:46 +0000

    Add package london-unified-prayer-times/1.2.6/london_unified_prayer_times-1.2.6.tar.gz

---
+++ packages/london-unified-prayer-times/london_unified_prayer_times-1.2.6.tar.gz/london_unified_prayer_times-1.2.6/tests/test_config.py
@@ -0,0 +1,23 @@
+import pytz
+import datetime
+from london_unified_prayer_times import config
+from london_unified_prayer_times import constants
+
+
+ck = constants.ConfigKeys
+
+
+def test_build_config():
+    json = config.default_config_json
+    bconfig = config.build_config(json)
+    assert len(bconfig) == len(json)
+    assert bconfig[ck.TIMEZONE] == pytz.timezone('Europe/London')
+    assert bconfig[ck.CACHE_EXPIRY] == datetime.timedelta(weeks=2)
+
+
+def test_reload_config():
+    cfg = config.load_config(None)
+    backup = cfg[ck.DEFAULT_TIMES]
+    cfg[ck.DEFAULT_TIMES] = []
+    cfg = config.load_config(None)
+    assert cfg[ck.DEFAULT_TIMES] == backup
