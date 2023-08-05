8b6206ed1acc73526309d2bc1a12bd6cff4b0e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:59 +0000

    Add package unikube/0.9.0.dev1/unikube-0.9.0.dev1-py3-none-any.whl

---
+++ packages/unikube/unikube-0.9.0.dev1-py3-none-any.whl/src/helpers.py
@@ -0,0 +1,26 @@
+import sys
+from urllib.parse import urljoin
+
+import click_spinner
+import requests
+from requests import Session
+
+from src import settings
+
+
+def get_requests_session(access_token) -> Session:
+    session = requests.Session()
+    session.headers.update({"Content-type": "application/json", "Authorization": "Bearer " + str(access_token)})
+    return session
+
+
+def download_specs(access_token: str, environment_id: str):
+    session = get_requests_session(access_token=access_token)
+
+    manifest_url = urljoin(settings.MANIFEST_DEFAULT_HOST, environment_id)
+    with click_spinner.spinner(beep=False, disable=False, force=False, stream=sys.stdout):
+        r = session.get(manifest_url)
+    r.raise_for_status()
+
+    manifest = r.json()
+    return manifest
