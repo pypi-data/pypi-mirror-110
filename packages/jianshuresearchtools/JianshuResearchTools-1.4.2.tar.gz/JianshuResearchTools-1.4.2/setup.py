043f31d0f6c99478a4f234523a1261fefc05b0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:48 +0000

    Add package jianshuresearchtools/1.4.2/JianshuResearchTools-1.4.2.tar.gz

---
+++ packages/jianshuresearchtools/JianshuResearchTools-1.4.2.tar.gz/JianshuResearchTools-1.4.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from JianshuResearchTools.main import __version__  # 自动获取版本号
+
+with open("README.md","r",encoding = "utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="JianshuResearchTools",
+    version=__version__,
+    author="FHU-yezi",
+    author_email="yehaowei20060411@qq.com",
+    description="科技赋能创作星辰",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FHU-yezi/JianshuResearchTools",
+    packages=["JianshuResearchTools"],
+    install_requires=["beautifulsoup4>=4.9.3","requests>=2.25.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
