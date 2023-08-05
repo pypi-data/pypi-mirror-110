4bbe5290d531e270da178be771d7da37ea0a6e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:05 +0000

    Add package chroma-feedback/10.0.0/chroma-feedback-10.0.0.tar.gz

---
+++ packages/chroma-feedback/chroma-feedback-10.0.0.tar.gz/chroma-feedback-10.0.0/chroma_feedback/color.py
@@ -0,0 +1,139 @@
+from chroma_feedback.typing import StatusType, ColorConfigModel
+
+COLOR =\
+{
+	'failed': '\033[0;31m',
+	'passed': '\033[0;32m',
+	'started': '\033[0;33m',
+	'end': '\033[0m'
+}
+
+
+def format_passed(text : str) -> str:
+	return COLOR['passed'] + text + COLOR['end']
+
+
+def format_started(text : str) -> str:
+	return COLOR['started'] + text + COLOR['end']
+
+
+def format_failed(text : str) -> str:
+	return COLOR['failed'] + text + COLOR['end']
+
+
+def get_by_status(status : StatusType) -> ColorConfigModel:
+	if status == 'started':
+		return get_started()
+	if status == 'errored':
+		return get_errored()
+	if status == 'failed':
+		return get_failed()
+	return get_passed()
+
+
+def get_passed() -> ColorConfigModel:
+	return\
+	{
+		'name': 'green',
+		'rgb':
+		[
+			0,
+			255,
+			0
+		],
+		'hue': 26000,
+		'saturation':
+		[
+			100,
+			255,
+			65535
+		],
+		'brightness':
+		[
+			100,
+			255,
+			65535
+		],
+		'kelvin': 3500
+	}
+
+
+def get_started() -> ColorConfigModel:
+	return\
+	{
+		'name': 'yellow',
+		'rgb':
+		[
+			255,
+			255,
+			0
+		],
+		'hue': 10000,
+		'saturation':
+		[
+			100,
+			255,
+			65535
+		],
+		'brightness':
+		[
+			100,
+			255,
+			65535
+		],
+		'kelvin': 3500
+	}
+
+
+def get_errored() -> ColorConfigModel:
+	return\
+	{
+		'name': 'white',
+		'rgb':
+		[
+			255,
+			255,
+			255
+		],
+		'hue': 58000,
+		'saturation':
+		[
+			0,
+			0,
+			0
+		],
+		'brightness':
+		[
+			100,
+			255,
+			65535
+		],
+		'kelvin': 3500
+	}
+
+
+def get_failed() -> ColorConfigModel:
+	return\
+	{
+		'name': 'red',
+		'rgb':
+		[
+			255,
+			0,
+			0
+		],
+		'hue': 0,
+		'saturation':
+		[
+			100,
+			255,
+			65535
+		],
+		'brightness':
+		[
+			100,
+			255,
+			65535
+		],
+		'kelvin': 3500
+	}
