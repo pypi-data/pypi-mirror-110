26d7eef5bb9eb3a341ebf61ff71f9e6d77536ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:11 +0000

    Add package django-app-graphql/1.9.5/django_app_graphql-1.9.5-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.9.5-py3-none-any.whl/django_app_graphql/ariadne/schema.py
@@ -0,0 +1,38 @@
+import ariadne
+from ariadne import gql, QueryType, MutationType
+from django.apps import apps
+from django_koldar_utils.functions import file_helpers
+from graphql.type.schema import GraphQLSchema
+
+from django_app_graphql.ariadne import decorators
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+def generate_schema_from_apps() -> GraphQLSchema:
+    """
+    The function scans your project and apps and fetch all the *.graphql files.
+
+    Then, it builds the schema
+    :return:
+    """
+
+    files = []
+
+    for app in apps.get_app_configs():
+        for graphql_file in file_helpers.get_all_files_ending_with(app.path, "graphql"):
+            LOG.info(f"Including graphql file {graphql_file}")
+            # load file content
+            with open(graphql_file, mode="r", encoding="utf8") as f:
+                content = f.read()
+            files.append(content)
+        # now we add all the schema
+
+    typedefs = gql('\n'.join(files))
+    LOG.info(f"number of query resolvers detected: {len(decorators.query._resolvers)}")
+    LOG.info(f"number of mutation resolvers detected: {len(decorators.mutation._resolvers)}")
+
+    return ariadne.make_executable_schema(typedefs, decorators.query, decorators.mutation)
+
+
