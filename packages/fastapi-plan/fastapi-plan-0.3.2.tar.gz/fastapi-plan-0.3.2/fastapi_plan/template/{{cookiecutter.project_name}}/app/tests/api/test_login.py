17457c88af151998fbf9c929728572397749a85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:23 +0000

    Add package fastapi-plan/0.3.2/fastapi-plan-0.3.2.tar.gz

---
+++ packages/fastapi-plan/fastapi-plan-0.3.2.tar.gz/fastapi-plan-0.3.2/fastapi_plan/template/{{cookiecutter.project_name}}/app/tests/api/test_login.py
@@ -0,0 +1,30 @@
+from asyncio import AbstractEventLoop as EventLoop
+from typing import Dict
+
+from fastapi.testclient import TestClient
+
+from app.core.config import settings
+from app.tests.conftest import default_superuser, default_user
+
+
+def test_get_access_token(
+    client: TestClient, event_loop: EventLoop, normal_user_token_headers: Dict[str, str]
+) -> None:
+    login_data = {"username": default_user.email, "password": default_user.password}
+    r = client.post(f"{settings.API_STR}/login/access-token", data=login_data)
+    tokens = r.json()
+    assert r.status_code == 200
+    assert "access_token" in tokens
+    assert tokens["access_token"]
+
+
+def test_use_access_token(
+    client: TestClient, superuser_token_headers: Dict[str, str]
+) -> None:
+    r = client.post(
+        f"{settings.API_STR}/login/test-token",
+        headers=superuser_token_headers,
+    )
+    result = r.json()
+    assert r.status_code == 200
+    assert "email" in result
