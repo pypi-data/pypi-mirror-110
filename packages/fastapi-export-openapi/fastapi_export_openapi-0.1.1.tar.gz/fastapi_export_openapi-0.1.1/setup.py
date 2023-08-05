0358644a5985d5e704ed7a292dd498d9eab5efdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:59 +0000

    Add package fastapi-export-openapi/0.1.1/fastapi_export_openapi-0.1.1.tar.gz

---
+++ packages/fastapi-export-openapi/fastapi_export_openapi-0.1.1.tar.gz/fastapi_export_openapi-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_export_openapi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<6.0.0', 'pydantic>=1.8.2,<2.0.0', 'typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['fastapi-export-openapi = '
+                     'fastapi_export_openapi.main:app']}
+
+setup_kwargs = {
+    'name': 'fastapi-export-openapi',
+    'version': '0.1.1',
+    'description': 'GitHub Action to export OpenAPI schema from FastAPI apps.',
+    'long_description': None,
+    'author': 'Andrew Hoetker',
+    'author_email': 'andrew@hoetker.engineer',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
