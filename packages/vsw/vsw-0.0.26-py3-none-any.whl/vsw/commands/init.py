8dbe452a9c8cf17a06be44d3efbac5825bbd114b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:36 +0000

    Add package vsw/0.0.26/vsw-0.0.26-py3-none-any.whl

---
+++ packages/vsw/vsw-0.0.26-py3-none-any.whl/vsw/commands/init.py
@@ -0,0 +1,144 @@
+import argparse
+import json
+import socket
+import time
+from typing import List
+
+import requests
+
+from vsw.utils import Constant
+import vsw.utils
+from vsw.log import Log
+from multiprocessing.connection import Listener
+
+logger = Log(__name__).logger
+timeout = Constant.TIMEOUT
+vsw_config = vsw.utils.get_vsw_agent()
+vsw_repo_config = vsw.utils.get_repo_host()
+client_header = {"x-api-key": vsw_config.get("seed")}
+software_certificate = vsw_config.get("schema_name")
+test_certificate = vsw_config.get("test_schema_name")
+
+
+def main(args: List[str]) -> bool:
+    args = parse_args(args)
+    print(args)
+    try:
+        if args.connection:
+            connection_repo()
+        if args.credential_definition:
+            do_credential_definition(args.schema)
+    except requests.exceptions.RequestException:
+        logger.error(vsw.utils.Constant.NOT_RUNNING_MSG)
+    except KeyboardInterrupt:
+        print(" ==> Exit init")
+
+
+def get_schema_id_by_name(vsw_config, schema_name):
+    if schema_name == vsw_config.get("test_schema_name"):
+        return vsw_config.get("test_schema_id")
+    else:
+        return vsw_config.get("schema_id")
+
+
+def check_credential_definition(schema_id):
+    cd_check_url = f'http://{vsw_config.get("admin_host")}:{vsw_config.get("admin_port")}/credential-definitions/created?schema_id={schema_id}'
+    resp = requests.get(url=cd_check_url, headers=client_header)
+    check_res = json.loads(resp.text)
+    if len(check_res["credential_definition_ids"]) > 0:
+        return check_res["credential_definition_ids"][0]
+    else:
+        return None
+
+
+def do_credential_definition(schema_name):
+    if schema_name != software_certificate and schema_name != test_certificate:
+        print(f"vsw: error: the schema name must be either {software_certificate} or {test_certificate}")
+        return;
+    schema_id = get_schema_id_by_name(vsw_config, schema_name)
+    # check if created
+    check_result = check_credential_definition(schema_id)
+    if check_result:
+        print(f'vsw: error: creddef already existed, {check_result}')
+        return;
+    credential_definition_url = f'http://{vsw_config.get("admin_host")}:{vsw_config.get("admin_port")}/credential-definitions'
+    res = requests.post(url=credential_definition_url, json={
+        "revocation_registry_size": 100,
+        "support_revocation": True,
+        "schema_id": schema_id,
+        "tag": "default"
+    }, headers=client_header)
+    logger.info(res)
+    credential_definition_res = json.loads(res.text)
+    logger.info(credential_definition_res)
+    print(f'Created credential definition id: {credential_definition_res["credential_definition_id"]}')
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-cd", "--credential-definition", action='store_true')
+    parser.add_argument('-c', '--connection', action='store_true')
+    parser.add_argument('-s', '--schema', default=software_certificate, help="The schema name")
+
+    return parser.parse_args(args)
+
+
+def connection_repo():
+    address = ('localhost', Constant.PORT_NUMBER)
+    listener = Listener(address)
+    listener._listener._socket.settimeout(Constant.TIMEOUT)
+    remove_history_connection(vsw_config)
+    vsw_repo_url = f'{vsw_repo_config.get("host")}/controller/invitation'
+    logger.info(f'Create invitation to: {vsw_repo_url}')
+    response = requests.get(url=vsw_repo_url)
+    logger.info(response)
+    res = json.loads(response.text)
+    logger.info(res)
+    local_url = f'http://{vsw_config.get("admin_host")}:{str(vsw_config.get("admin_port"))}/connections/receive-invitation?alias={vsw_config.get("label")}'
+    logger.info(f'Receive invitation {local_url}')
+    invitation = res["invitation"]
+    body = {
+        "label": invitation["label"],
+        "serviceEndpoint": invitation["serviceEndpoint"],
+        "recipientKeys": invitation["recipientKeys"],
+        "@id": invitation["@id"]
+    }
+    ss = requests.post(url=local_url, json=body, headers=client_header)
+    logger.info(ss)
+    invitation_response = json.loads(ss.text)
+
+    connection_id = invitation_response["connection_id"]
+    while True:
+        try:
+            conn = listener.accept()
+            msg = conn.recv()
+            state = msg["state"]
+            conn.close()
+            logger.info(f'waiting state change, current state is: {state}')
+            if state == 'active':
+                print("Created connection successfully!")
+                listener.close()
+                break
+            else:
+                time.sleep(0.5)
+        except socket.timeout as e:
+            remove_connection(connection_id, vsw_config)
+            logger.error(e)
+            print("vsw: error: request timeout, there might be some issue during initializing connection.")
+            listener.close()
+            break;
+
+
+def remove_history_connection(vsw_config):
+    schema_url = f'http://{vsw_config.get("admin_host")}:{vsw_config.get("admin_port")}/connections'
+    response = requests.get(url=schema_url, headers=client_header)
+    results = json.loads(response.text)["results"]
+    for result in results:
+        schema_url = f'http://{vsw_config.get("admin_host")}:{vsw_config.get("admin_port")}/connections/{result["connection_id"]}/remove'
+        requests.post(url=schema_url, headers=client_header)
+        logger.info(f"Removed history connection id: {result['connection_id']}")
+
+
+def remove_connection(connection_id, vsw_config):
+    schema_url = f'http://{vsw_config.get("admin_host")}:{vsw_config.get("admin_port")}/connections/{connection_id}/remove'
+    requests.post(url=schema_url, headers=client_header)
---
+++ packages/vsw/vsw-0.0.26-py3-none-any.whl/vsw/controller/server.py
@@ -0,0 +1,130 @@
+import logging
+import os
+from multiprocessing.connection import Client
+from pathlib import Path
+from wsgiref.simple_server import make_server
+from vsw.dao import vsw_dao
+from flask import Flask, request, Response
+
+from vsw import utils
+from vsw.utils import Constant
+app = Flask(__name__)
+
+log_folder = str(Path(os.path.expanduser('~')).joinpath("vsw_logs").resolve())
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+controller_log_file = str(Path(os.path.expanduser('~')).joinpath("vsw_logs/vsw-controller.log").resolve())
+logging.basicConfig(filename=controller_log_file, level=logging.DEBUG)
+
+
+@app.route('/')
+def hello():
+    return "Welcome to vsw controller"
+
+
+@app.route('/webhooks/topic/connections/', methods=['POST'])
+def connections():
+    app.logger.info("=======/webhooks/topic/connections/ START===========")
+    app.logger.info(request.json)
+    app.logger.info("=======/webhooks/topic/connections/ END===========")
+    address = ('localhost', Constant.PORT_NUMBER)
+    conn = Client(address)
+    conn.send(request.json)
+    conn.close()
+    return Response(status=200)
+
+
+@app.route("/webhooks/topic/issue_credential/", methods=['POST'])
+def issue_credential():
+    app.logger.info("=======/webhooks/topic/issue_credential/ START===========")
+    app.logger.info(request.json)  # Handle webhook request here
+    app.logger.info("=======/webhooks/topic/issue_credential/ END===========")
+    save_credential(request.json)
+    address = ('localhost', Constant.PORT_NUMBER)
+    conn = Client(address)
+    conn.send(request.json)
+    return Response(status=200)
+
+
+@app.route('/webhooks/topic/present_proof/', methods=['POST'])
+def present_proof():
+    app.logger.info("=======/webhooks/topic/present_proof/ START===========")
+    app.logger.info(request.json)  # Handle webhook request here
+    app.logger.info("=======/webhooks/topic/present_proof/ END===========")
+    address = ('localhost', Constant.PORT_NUMBER)
+    conn = Client(address)
+    conn.send(request.json)
+    return Response(status=200)
+
+
+@app.route("/webhooks/topic/problem_report/", methods=['POST'])
+def problem_report():
+    app.logger.info("=======/webhooks/topic/problem_report/ START===========")
+    app.logger.info(request.json)  # Handle webhook request here
+    app.logger.info("=======/webhooks/topic/problem_report/ END===========")
+    address = ('localhost', Constant.PORT_NUMBER)
+    conn = Client(address)
+    conn.send(request.json)
+    return Response(status=200)
+
+
+@app.route("/webhooks/topic/revocation_registry/", methods=['POST'])
+def revocation_registry():
+    app.logger.info("=======/webhooks/topic/revocation_registry/ START===========")
+    app.logger.info(request.json)  # Handle webhook request here
+    app.logger.info("=======/webhooks/topic/revocation_registry/ END===========")
+    return Response(status=200)
+
+
+@app.route("/webhooks/topic/issuer_cred_rev/", methods=['POST'])
+def issuer_cred_rev():
+    app.logger.info("=======/webhooks/topic/issuer_cred_rev/ START===========")
+    app.logger.info(request.json)  # Handle webhook request here
+    app.logger.info("=======/webhooks/topic/issuer_cred_rev/ END===========")
+    return Response(status=200)
+
+
+@app.route("/webhooks/topic/basicmessages/", methods=['POST'])
+def basicmessages():
+    app.logger.info("=======/webhooks/topic/basicmessages/ START===========")
+    app.logger.info(request.json)  # Handle webhook request here
+    app.logger.info("=======/webhooks/topic/basicmessages/ END===========")
+    return Response(status=200)
+
+
+@app.route("/webhooks/topic/ping/", methods=['POST'])
+def ping():
+    app.logger.info(request.json)  # Handle webhook request here
+    return Response(status=200)
+
+
+def save_credential(msg):
+    state = msg["state"]
+    if state == 'credential_acked':
+        credential = msg["credential"]
+        revocation_id = msg.get("revocation_id", '')
+        values = credential["values"]
+        attrs = {}
+        for key, value in values.items():
+            attrs[key] = value.get("raw", "")
+        content = {
+            "schema_id": credential["schema_id"],
+            "cred_def_id": credential["cred_def_id"],
+            "rev_reg_id": credential["rev_reg_id"],
+            "cred_rev_id": revocation_id,
+            "attrs": attrs
+        }
+        issuer_did = attrs.get("developerDid", "")
+        software_name = attrs.get("softwareName", "")
+        software_did = attrs.get("softwareDid", "")
+        vsw_dao.save_credential(issuer_did, software_name, software_did, "success", content)
+
+
+if __name__ == '__main__':
+    configuration = utils.get_vsw_agent()
+    webhook_port = configuration.get("webhook_port")
+    app.logger.info(f"Started controller, http://127.0.0.1:{webhook_port}")
+    server = make_server('127.0.0.1', int(webhook_port), app)
+    server.serve_forever()
+    app.run()
+
