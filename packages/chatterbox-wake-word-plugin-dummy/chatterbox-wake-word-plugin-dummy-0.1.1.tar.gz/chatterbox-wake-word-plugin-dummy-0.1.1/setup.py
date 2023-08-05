eeee1e02af0e723c9a221fbaa236b5b8664b2858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package chatterbox-wake-word-plugin-dummy/0.1.1/chatterbox-wake-word-plugin-dummy-0.1.1.tar.gz

---
+++ packages/chatterbox-wake-word-plugin-dummy/chatterbox-wake-word-plugin-dummy-0.1.1.tar.gz/chatterbox-wake-word-plugin-dummy-0.1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+PLUGIN_ENTRY_POINT = 'dummy_ww_plug=chatterbox_wake_word_plugin_dummy:DummyWakeWordPlugin'
+setup(
+    name='chatterbox-wake-word-plugin-dummy',
+    version='0.1.1',
+    description='A wake word plugin for mycroft',
+    url='https://github.com/HelloChatterbox/dummy_wakeword_plugin',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['chatterbox_wake_word_plugin_dummy'],
+    install_requires=["ovos-plugin-manager>=0.0.1a3"],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='mycroft plugin wake word',
+    entry_points={'mycroft.plugin.wake_word': PLUGIN_ENTRY_POINT}
+)
