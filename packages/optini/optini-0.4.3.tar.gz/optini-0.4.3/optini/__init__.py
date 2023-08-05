d7092bf226651e86b45b57a56b18fb922ec75a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:21 +0000

    Add package optini/0.4.3/optini-0.4.3.tar.gz

---
+++ packages/optini/optini-0.4.3.tar.gz/optini-0.4.3/optini/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for optini."""
+
+__author__ = """Brendan Strejcek"""
+__email__ = 'brendan@datagazing.com'
+__version__ = '0.4.3'
+
+from .optini import Config, opt, spec, section # noqa F401
