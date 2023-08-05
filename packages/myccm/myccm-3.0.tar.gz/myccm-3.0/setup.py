bb2cd7ced2e628858c33e2de009f4a48a56a97c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:33 +0000

    Add package myccm/3.0/myccm-3.0.tar.gz

---
+++ packages/myccm/myccm-3.0.tar.gz/myccm-3.0/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+try:
+  with open(os.path.join(path, 'README.md')) as f:
+    long_description = f.read()
+except Exception as e:
+  long_description = "customize okta cli"
+
+setup(
+    name = "myccm",
+    version = "3.0",
+
+    description = "this is a causal inference method",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.5.0",
+
+    url = "https://github.com/zhougang2020/CCM",
+    author = "zhougang",
+    author_email = "zg_pem@163.com",
+
+    packages=['src'],
+    include_package_data = True,
+    install_requires = ["requests", "click"],
+    platforms = "any",
+
+    scripts = [],
+    entry_points = {
+        'console_scripts': [
+            'okta-cmd=oktacmd:main_cli'
+        ]
+    }
+)
