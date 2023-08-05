5ba6e7fb3626377512238ab391d0047be63c164d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:48 +0000

    Add package pandect/0.3.0/pandect-0.3.0-py3-none-any.whl

---
+++ packages/pandect/pandect-0.3.0-py3-none-any.whl/pandect/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for pandect."""
+
+__author__ = """Brendan Strejcek"""
+__email__ = 'brendan@datagazing.com'
+__version__ = '0.3.0'
+
+from .pandect import load, save, pandect, sav2dta # noqa
