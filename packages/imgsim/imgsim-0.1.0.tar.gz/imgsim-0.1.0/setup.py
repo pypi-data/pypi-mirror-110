61d4d10e0a4eced95b74b4fa443924e16e7d97f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package imgsim/0.1.0/imgsim-0.1.0.tar.gz

---
+++ packages/imgsim/imgsim-0.1.0.tar.gz/imgsim-0.1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="imgsim", # 名称
+    version="0.1.0", # 版本
+    author="Chen, Mingxiang", # 作者
+    author_email="chenmingxiang110@gmail.com", # 邮箱
+    description="This is an easy-to-use package for calculating the similarities between pictures.", # 简介
+    long_description="", # 详细介绍
+    long_description_content_type="text/markdown", # 详细介绍的文件类型
+    url="https://github.com/chenmingxiang110/AugNet", # 包的链接
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
