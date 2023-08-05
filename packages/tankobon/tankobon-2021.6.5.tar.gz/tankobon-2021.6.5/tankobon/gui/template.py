72075f8a4ef42ffd75b02e458b884b5c1de7fcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:09 +0000

    Add package tankobon/2021.6.5/tankobon-2021.6.5.tar.gz

---
+++ packages/tankobon/tankobon-2021.6.5.tar.gz/tankobon-2021.6.5/tankobon/gui/template.py
@@ -0,0 +1,24 @@
+# coding: utf8
+"""This template generates the HTML view for a manga."""
+
+import string
+
+from PySide6.QtWidgets import QTextEdit
+
+from ..models import Manga
+
+from .utils import resource
+
+TEMPLATE = string.Template(resource(":/view.html").decode("utf8"))
+
+
+def create(manga: Manga, lang: str):
+    # convert table to html from markdown
+    textedit = QTextEdit()
+    textedit.setMarkdown(manga.summary(lang=lang))
+
+    table = textedit.toHtml()
+
+    return TEMPLATE.substitute(
+        url=manga.meta.url, title=manga.meta.title, desc=manga.meta.desc, table=table
+    )
