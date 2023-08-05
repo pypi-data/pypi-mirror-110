1600473c0f9733c67463b16f3ac3fe1578e9260b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:36 +0000

    Add package imlazy-photo/1.0.1/imlazy_photo-1.0.1-py3-none-any.whl

---
+++ packages/imlazy-photo/imlazy_photo-1.0.1-py3-none-any.whl/imlazy_photo/imlazy_photo.py
@@ -0,0 +1,35 @@
+import os
+import time
+import shutil
+
+def sort_photo():
+    c_path = os.getcwd()
+    file_list = os.listdir(c_path)
+    result = {}
+
+    for file_name in file_list:
+        etime = os.path.getmtime(file_name)
+        created = f"{c_path}/{time.strftime('%Y%m%d', time.localtime(etime))}"
+
+        if created in result.keys():
+            result[created].append(file_name)
+        else:
+            result[created] = []
+            result[created].append(file_name)
+
+    return result
+    
+
+def make_dir_move_file(path_dict):
+    for path, file_list in path_dict.items():
+        try:
+            if not os.path.exists(path):
+                os.makedirs(path)
+                _move_file(file_list, path)
+        except OSError:
+            _move_file(file_list, path)
+
+def _move_file(file_list, path):
+    for file_name in file_list:
+        print(file_name)
+        shutil.move(file_name, f'{path}/{file_name}')
