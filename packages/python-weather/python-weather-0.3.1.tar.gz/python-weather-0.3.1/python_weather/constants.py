c4c95cf1da3f76a94bc0be50cf3310d5d02a8765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:18 +0000

    Add package python-weather/0.3.1/python-weather-0.3.1.tar.gz

---
+++ packages/python-weather/python-weather-0.3.1.tar.gz/python-weather-0.3.1/python_weather/constants.py
@@ -0,0 +1,9 @@
+default_client_options = {
+    "src": "outlook",
+    "weadegreetype": "C",
+    "culture": "en-US"
+}
+
+base_url = "https://weather.service.msn.com/find.aspx"
+
+empty_func = lambda d: d
