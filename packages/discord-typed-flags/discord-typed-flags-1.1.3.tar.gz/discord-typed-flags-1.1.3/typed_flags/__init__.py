135ed2297b03f5a6d9badf0acb873371e8e17595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package discord-typed-flags/1.1.3/discord-typed-flags-1.1.3.tar.gz

---
+++ packages/discord-typed-flags/discord-typed-flags-1.1.3.tar.gz/discord-typed-flags-1.1.3/typed_flags/__init__.py
@@ -0,0 +1,10 @@
+from .typed_flags import TypedFlags
+
+__version__ = "1.1.3"
+
+import logging
+from collections import namedtuple
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=1, minor=1, micro=3, releaselevel="final", serial=0)
