1844751952cf3f6622254c11318a568aa563e1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:34 +0000

    Add package firstimpression/0.7.3/firstimpression-0.7.3-py3-none-any.whl

---
+++ packages/firstimpression/firstimpression-0.7.3-py3-none-any.whl/firstimpression/api/__init__.py
@@ -0,0 +1,7 @@
+from firstimpression.api import bbc
+from firstimpression.api import ns
+from firstimpression.api import nu_nl
+from firstimpression.api import traffic
+from firstimpression.api import request
+from firstimpression.api import insta
+from firstimpression.api import weather
