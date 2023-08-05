3c97e3ef37c2b45a7b4256493c0c1487f38cd3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package django-app-graphql/1.8.0/django_app_graphql-1.8.0-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.8.0-py3-none-any.whl/django_app_graphql/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from django_app_graphql.conf import settings
+
+urlpatterns = []
+
+backend = settings.DJANGO_APP_GRAPHQL["BACKEND_TYPE"]
+if backend == "ariadne":
+    from ariadne.contrib.django.views import GraphQLView
+    from ariadne import schema
+    urlpatterns.append(path(settings.DJANGO_APP_GRAPHQL["GRAPHQL_SERVER_URL"],
+                            csrf_exempt(GraphQLView.as_view(schema=schema.get_schema()))))
+elif backend == "graphene":
+    from graphene_django.views import GraphQLView
+    urlpatterns.append(path(settings.DJANGO_APP_GRAPHQL["GRAPHQL_SERVER_URL"],
+                            csrf_exempt(GraphQLView.as_view(graphiql=settings.DJANGO_APP_GRAPHQL["EXPOSE_GRAPHIQL"]))))
+else:
+    raise ValueError(f"backend must be ariadne or graphene")
+
