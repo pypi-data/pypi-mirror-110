40c11148f39e64848f1b5898b7f1f427fcfe1275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:52 +0000

    Add package memories/0.4/memories-0.4.tar.gz

---
+++ packages/memories/memories-0.4.tar.gz/memories-0.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name = 'memories',
+    version = '0.4',
+    url = 'https://github.com/veedata/album-manager',
+    download_url = 'https://github.com/veedata/album-manager/archive/refs/tags/v0.4-beta.tar.gz',
+    author = 'Viraj Thakkar',
+    author_email = 'vdthakkar111@gmail.com',
+    description = 'A library for those who want to convert their older images into digitised format (with metadata), and beautify them using borders and other options.',
+    long_description=long_desc,
+    keywords = ['computer vision', 'image processing', 'opencv', 'album'],
+    project_urls={
+        "Code": "https://github.com/veedata/album-manager",
+        "Issue tracker": "https://github.com/veedata/album-manager/issues",
+    },
+    install_requires=[
+          'piexif',
+          'opencv-python',
+          'Pillow',
+      ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "memories"},
+    packages=setuptools.find_packages(where="memories"),
+    scripts=['bin/range-detector'],
+)
