aec81b6b466c979c72dd7919515ae8a7ace94eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:57 +0000

    Add package dotify/1.1.0/dotify-1.1.0-py3-none-any.whl

---
+++ packages/dotify/dotify-1.1.0-py3-none-any.whl/dotify/models/_image.py
@@ -0,0 +1,15 @@
+import logging
+
+from dotify._model import Model, logger
+
+logger = logging.getLogger("{0}.{1}".format(logger.name, __name__))
+
+
+class Image(Model):
+    """A class representing a Spotify `Image`."""
+
+    class Json(object):
+        """ """
+
+    def __str__(self):
+        return self.url
