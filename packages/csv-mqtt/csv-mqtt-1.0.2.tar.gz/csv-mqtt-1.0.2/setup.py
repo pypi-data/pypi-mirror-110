59d1398577abb03c8aa45f537c630e04fdd8369e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:37 +0000

    Add package csv-mqtt/1.0.2/csv-mqtt-1.0.2.tar.gz

---
+++ packages/csv-mqtt/csv-mqtt-1.0.2.tar.gz/csv-mqtt-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="csv-mqtt",
+    version="1.0.2",
+    author="srijan-sivakumar",
+    author_email="sivakumarsrijan@gmail.com",
+    description="CSV parser meets MQTT Publisher",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/srijan-sivakumar/CSV-MQTT/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'paho-mqtt'
+    ],
+)
