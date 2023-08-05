124dcab9be90f732889741eb598e672b1e975c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:20 +0000

    Add package wordmarker/0.0.6/wordmarker-0.0.6.tar.gz

---
+++ packages/wordmarker/wordmarker-0.0.6.tar.gz/wordmarker-0.0.6/wordmarker/loaders/default_resource_loader.py
@@ -0,0 +1,64 @@
+from wordmarker.data import Resource
+from wordmarker.loaders.resource_loader import ResourceLoader
+from wordmarker.utils import log
+
+
+class DefaultResourceLoader(ResourceLoader):
+    """
+    ::
+
+        默认的资源加载器
+    """
+
+    @log
+    def __init__(self):
+        pass
+
+    def get_resource(self, path=None) -> Resource:
+        """
+        .. note::
+
+            获取资源
+
+        :param path: 路径
+        :return: - 资源实例
+        """
+        return Resource(path, self)
+
+    def load(self, resource: Resource):
+        """
+        .. note::
+
+            加载资源
+
+        :param resource: 资源实例
+        :return: - 资源为文件，获取文件中的数据
+
+                 - 资源为目录，获取目录下所有文件中的数据，返回一个字典
+
+                    - key为文件的绝对路径
+
+                    - value为文件的数据
+        """
+        if resource.exists():
+            if resource.is_file():
+                try:
+                    with open(resource.get_file(), encoding=resource.get_file_encoding()) as f:
+                        data = f.read()
+                        return data
+                except (IOError, ValueError, TypeError) as exc:
+                    self._logger.exception("加载文件时发生错误", exc_info=exc)
+            else:
+                file_list = resource.get_file()
+                encoding_dict = resource.get_file_encoding()
+                data_dict = {}
+                try:
+                    for file in file_list:
+                        with open(file, encoding=encoding_dict.get(file)) as f:
+                            data = f.read()
+                            data_dict[file] = data
+                    return data_dict
+                except (IOError, ValueError, TypeError) as exc:
+                    self._logger.exception("加载文件时发生错误", exc_info=exc)
+        else:
+            self._logger.error("文件或目录不存在")
---
+++ packages/wordmarker/wordmarker-0.0.6.tar.gz/wordmarker-0.0.6/wordmarker/utils/file.py
@@ -0,0 +1,84 @@
+import os
+from typing import Union
+from wordmarker.contexts import SystemContext
+
+
+class PathUtils(SystemContext):
+    """
+    ::
+
+        路径工具类
+    """
+
+    def __init__(self, src, tgt):
+        self.__src = src
+        self.__tgt = tgt
+
+    def get_relative_path(self):
+        """
+        .. note::
+
+            将两个路径拼接起来
+
+            例子：
+
+                ::
+
+                    src = c:/a/b/c
+
+                    tgt = ../../d/c.txt
+
+                    final = c:/a/d/c.txt
+
+                    -------------------------------
+
+                    src = c:/a/b/c/foo.txt
+
+                    tgt = ../../d/c.txt
+
+                    final = c:/a/d/c.txt
+
+        :return: - 最终路径
+        """
+        src: str = self.__src
+        tgt: str = self.__tgt
+        src = os.path.normpath(src)
+        tgt = os.path.normpath(tgt)
+        if os.path.isfile(src):
+            src_dir = os.path.dirname(src)
+        else:
+            src_dir = src
+        src_list = src_dir.split(self.path_separator)
+        tgt_list = tgt.split(self.path_separator)
+        index = 0
+        for i in tgt_list:
+            if i == r'..':
+                src_list.pop()
+                index += 1
+        for i in range(0, index):
+            tgt_list.pop(0)
+        final_list = src_list + tgt_list
+        final_path = self.path_separator.join(final_list)
+        return final_path
+
+    @staticmethod
+    def filter_file(file_list: Union[list, str], suffix_list: Union[list, str]):
+        """
+        .. note::
+
+            通过后缀名，过滤文件
+
+        :param file_list: 文件或文件列表
+        :param suffix_list: 后缀或后缀列表
+        :return: - 过滤后的文件或文件列表
+        """
+        final_list = []
+        if type(file_list) is not list:
+            file_list = [file_list]
+        for item in file_list:
+            file_suffix = os.path.splitext(item)[1]
+            if type(suffix_list) is not list:
+                suffix_list = [suffix_list]
+            if file_suffix in suffix_list:
+                final_list.append(item)
+        return final_list
