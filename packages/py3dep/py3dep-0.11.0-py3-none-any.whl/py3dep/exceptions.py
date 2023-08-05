56d111d2f5f35be3eb11985d3c2a75577b23cd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:46 +0000

    Add package py3dep/0.11.0/py3dep-0.11.0-py3-none-any.whl

---
+++ packages/py3dep/py3dep-0.11.0-py3-none-any.whl/py3dep/exceptions.py
@@ -0,0 +1,42 @@
+"""Customized pygeohydro exceptions."""
+from typing import List, Optional
+
+
+class InvalidInputType(Exception):
+    """Exception raised when a function argument type is invalid.
+
+    Parameters
+    ----------
+    arg : str
+        Name of the function argument
+    valid_type : str
+        The valid type of the argument
+    example : str, optional
+        An example of a valid form of the argument, defaults to None.
+    """
+
+    def __init__(self, arg: str, valid_type: str, example: Optional[str] = None) -> None:
+        self.message = f"The {arg} argument should be of type {valid_type}"
+        if example is not None:
+            self.message += f":\n{example}"
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class MissingColumns(Exception):
+    """Exception raised when a required column is missing from a dataframe.
+
+    Parameters
+    ----------
+    missing : list
+        List of missing columns.
+    """
+
+    def __init__(self, missing: List[str]) -> None:
+        self.message = "The following columns are missing:\n" + f"{', '.join(missing)}"
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
