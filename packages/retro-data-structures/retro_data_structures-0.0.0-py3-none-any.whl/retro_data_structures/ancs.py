b07fbe226eeb536508fb039b6958954a421d66a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:25 +0000

    Add package retro-data-structures/0.0.0/retro_data_structures-0.0.0-py3-none-any.whl

---
+++ packages/retro-data-structures/retro_data_structures-0.0.0-py3-none-any.whl/retro_data_structures/ancs.py
@@ -0,0 +1,129 @@
+"""
+Wiki: https://wiki.axiodl.com/w/ANCS_(File_Format)
+"""
+
+import construct
+from construct import Int16ub, Const, Struct, PrefixedArray, Int32ub, If, Int8ub, Float32b, Terminated, Probe, Array, \
+    Tell
+
+from retro_data_structures import game_check
+from retro_data_structures.common_types import AABox, String, ObjectTag_32, AssetId32
+from retro_data_structures.construct_extensions import WithVersion, BeforeVersion
+from retro_data_structures.evnt import EVNT
+from retro_data_structures.meta_animation import MetaAnimation_AssetId32
+from retro_data_structures.meta_transition import MetaTransition_v1
+from retro_data_structures.pas_database import PASDatabase
+
+# This format is only for Prime 1 and 2, so AssetId is always 32-bit
+AssetId = AssetId32
+
+AnimationName = Struct(
+    animation_id=Int32ub,
+    unknown=BeforeVersion(10, String),
+    name=String,
+)
+ParticleResourceData = Struct(
+    generic_particles=PrefixedArray(Int32ub, AssetId),
+    swoosh_particles=PrefixedArray(Int32ub, AssetId),
+    unknown=WithVersion(6, Int32ub),
+    electric_particles=PrefixedArray(Int32ub, AssetId),
+    spawn_particles=WithVersion(10, PrefixedArray(Int32ub, AssetId)),
+)
+AnimationAABB = Struct(
+    name=String,
+    bounding_box=AABox,
+)
+EffectComponent = Struct(
+    name=String,
+    particle=ObjectTag_32,
+    bone_name=If(game_check.is_prime1, String),
+    bone_id=If(game_check.is_prime2, Int32ub),
+    scale=Float32b,
+    parented_mode=Int32ub,
+    flags=Int32ub,
+)
+Effect = Struct(
+    name=String,
+    components=PrefixedArray(Int32ub, EffectComponent),
+)
+IndexedAnimationAABB = Struct(
+    id=Int32ub,
+    bounding_box=AABox,
+)
+
+Character = Struct(
+    id=Int32ub,
+    version=Int16ub,
+    name=String,
+    model_id=AssetId,
+    skin_id=AssetId,
+    skeleton_id=AssetId,
+    animation_names=PrefixedArray(Int32ub, AnimationName),
+    pas_database=PASDatabase,
+    particle_resource_data=ParticleResourceData,
+    unknown_1=Int32ub,
+    unknown_2=WithVersion(10, Int32ub),
+    animation_aab_array=WithVersion(2, PrefixedArray(Int32ub, AnimationAABB)),
+    effect_array=WithVersion(2, PrefixedArray(Int32ub, Effect)),
+    frozen_model=WithVersion(4, AssetId),
+    frozen_skin=WithVersion(4, AssetId),
+    animation_id_map=WithVersion(5, PrefixedArray(Int32ub, Int32ub)),
+    spatial_primitives_id=WithVersion(10, AssetId),
+    unknown_3=WithVersion(10, Int8ub),
+    indexed_animation_aabb_array=WithVersion(10, PrefixedArray(Int32ub, IndexedAnimationAABB)),
+)
+
+CharacterSet = Struct(
+    version=Const(1, Int16ub),
+    characters=PrefixedArray(Int32ub, Character),
+)
+
+Animation = Struct(
+    name=String,
+    meta=MetaAnimation_AssetId32,
+)
+
+Transition = Struct(
+    unknown=Int32ub,
+    animation_id_a=Int32ub,
+    animation_id_b=Int32ub,
+    transition=MetaTransition_v1,
+)
+
+AdditiveAnimation = Struct(
+    animation_id=Int32ub,
+    fade_in_time=Float32b,
+    fade_out_time=Float32b,
+)
+
+HalfTransitions = Struct(
+    animation_id=Int32ub,
+    transition=MetaTransition_v1,
+)
+
+AnimationResourcePair = Struct(
+    anim_id=AssetId,
+    event_id=AssetId,
+)
+
+AnimationSet = Struct(
+    table_count=Int16ub,
+    animations=PrefixedArray(Int32ub, Animation),
+    transitions=PrefixedArray(Int32ub, Transition),
+    default_transition=MetaTransition_v1,
+    additive=If(construct.this.table_count >= 2, Struct(
+        additive_animations=PrefixedArray(Int32ub, AdditiveAnimation),
+        default_fade_in_time=Float32b,
+        default_fade_out_time=Float32b,
+    )),
+    half_transitions=If(construct.this.table_count >= 3, PrefixedArray(Int32ub, HalfTransitions)),
+    animation_resources=If(game_check.is_prime1, PrefixedArray(Int32ub, AnimationResourcePair)),
+    event_sets=If(game_check.is_prime2, PrefixedArray(Int32ub, EVNT)),
+)
+
+ANCS = Struct(
+    version=Const(1, Int16ub),
+    character_set=CharacterSet,
+    animation_set=AnimationSet,
+    _terminated=Terminated,
+)
---
+++ packages/retro-data-structures/retro_data_structures-0.0.0-py3-none-any.whl/retro_data_structures/cli.py
@@ -0,0 +1,184 @@
+import argparse
+import asyncio
+import itertools
+import json
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+from typing import Optional
+
+from retro_data_structures import mlvl, construct_extensions
+from retro_data_structures.ancs import ANCS
+from retro_data_structures.anim import ANIM
+from retro_data_structures.cinf import CINF
+from retro_data_structures.cmdl import CMDL
+from retro_data_structures.cskr import CSKR
+from retro_data_structures.mlvl import MLVL
+from retro_data_structures.mrea import MREA
+from retro_data_structures.pak import PAK
+
+types_per_game = {
+    "metroid_prime_1": {
+        "mlvl": mlvl.Prime1MLVL,
+    },
+    "metroid_prime_2": {
+        "mlvl": mlvl.Prime2MLVL,
+    },
+    "metroid_prime_3": {
+        "mlvl": mlvl.Prime3MLVL,
+    },
+}
+ALL_FORMATS = {
+    "ancs": ANCS,
+    "cmdl": CMDL,
+    "mlvl": MLVL,
+    "mrea": MREA,
+    "pak": PAK,
+    "anim": ANIM,
+    "cinf": CINF,
+    "cskr": CSKR,
+}
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+
+    subparser = parser.add_subparsers(dest="command", required=True)
+
+    ksy_export = subparser.add_parser("ksy-export")
+    ksy_export.add_argument("output_path", type=Path)
+
+    decode = subparser.add_parser("decode")
+    decode.add_argument("--game", help="Hint the game of the file", type=int)
+    decode.add_argument("--format", help="Hint the format of the file. Defaults to extension.")
+    decode.add_argument("--re-encode", help="Re-encode afterwards and compares to the original.", action="store_true")
+    decode.add_argument("input_path", type=Path, help="Path to the file")
+
+    compare = subparser.add_parser("compare-files")
+    compare.add_argument("--game", help="Hint the game of the file", type=int)
+    compare.add_argument("--format", help="Hint the format of the file")
+    compare.add_argument("--limit", help="Limit the number of files to test", type=int)
+    compare.add_argument("input_path", type=Path, help="Path to the directory to glob")
+
+    return parser
+
+
+def do_ksy_export(args):
+    output_path: Path = args.output_path
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    for game, formats in types_per_game.items():
+        for format_name, cls in formats.items():
+            print(f"Exporting {game} / {format_name}")
+            cls.export_ksy(f"{game}_{format_name}", output_path.joinpath(f"{game}_{format_name}.ksy"))
+
+
+def dump_to(path: Path, decoded):
+    def default(o):
+        if callable(o):
+            o = o()
+        if isinstance(o, bytes):
+            return len(o)
+
+        raise TypeError(f'Object of type {o.__class__.__name__} '
+                        f'is not JSON serializable')
+
+    with path.open("w") as f:
+        x = construct_extensions.convert_to_raw_python(decoded)
+        f.write(json.JSONEncoder(indent=4, default=default).encode(x))
+
+
+def do_decode(args):
+    input_path: Path = args.input_path
+    file_format = args.format
+    game = args.game
+    re_encode = args.re_encode
+
+    if file_format is None:
+        file_format = input_path.suffix[1:]
+
+    construct_class = ALL_FORMATS[file_format.lower()]
+
+    raw = input_path.read_bytes()
+    decoded_from_raw = construct_class.parse(raw, target_game=game)
+    print(decoded_from_raw)
+
+    if re_encode:
+        encoded = construct_class.build(decoded_from_raw, target_game=game)
+        if raw != encoded:
+            print(f"{input_path}: Results differ (len(raw): {len(raw)}; len(encoded): {len(encoded)})")
+
+
+def decode_encode_compare_file(file_path: Path, game: int, file_format: str):
+    construct_class = ALL_FORMATS[file_format.lower()]
+
+    try:
+        raw = file_path.read_bytes()
+        decoded_from_raw = construct_class.parse(raw, target_game=game)
+        encoded = construct_class.build(decoded_from_raw, target_game=game)
+
+        if raw != encoded:
+            return f"{file_path}: Results differ (len(raw): {len(raw)}; len(encoded): {len(encoded)})"
+        return None
+
+    except Exception as e:
+        return f"{file_path}: Received error - {e}"
+
+
+async def compare_all_files_in_path(args):
+    input_path: Path = args.input_path
+    file_format: str = args.format
+    game: int = args.game
+    limit: Optional[int] = args.limit
+
+    def apply_limit(it):
+        if limit is None:
+            return it
+        else:
+            return itertools.islice(it, limit)
+
+    loop = asyncio.get_running_loop()
+
+    try:
+        import tqdm
+    except ImportError:
+        tqdm = None
+
+    errors = []
+
+    with ProcessPoolExecutor() as executor:
+        files = apply_limit(input_path.rglob(f"*.{file_format.upper()}"))
+        if tqdm is not None:
+            files = tqdm.tqdm(files, unit=" file")
+
+        results = [loop.run_in_executor(executor, decode_encode_compare_file, f, game, file_format)
+                   for f in files]
+        as_completed = asyncio.as_completed(results)
+        if tqdm is not None:
+            as_completed = tqdm.tqdm(as_completed, total=len(results), unit=" file")
+
+        for c in as_completed:
+            message = await c
+            if message:
+                if tqdm is not None:
+                    errors.append(message)
+                    as_completed.set_postfix_str(f"{len(errors)} errors")
+                else:
+                    print(message)
+
+    if errors:
+        print("Errors:")
+        for m in errors:
+            print(m)
+
+
+def main():
+    args = create_parser().parse_args()
+
+    if args.command == "ksy-export":
+        do_ksy_export(args)
+    elif args.command == "decode":
+        do_decode(args)
+    elif args.command == "compare-files":
+        asyncio.run(compare_all_files_in_path(args))
+    else:
+        raise ValueError(f"Unknown command: {args.command}")
