577fcb095e251e7a3a0f98cb20296938e327b4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:26 +0000

    Add package plover-fancytext/1.7.0/plover_fancytext-1.7.0.tar.gz

---
+++ packages/plover-fancytext/plover_fancytext-1.7.0.tar.gz/plover_fancytext-1.7.0/plover_fancytext/common.py
@@ -0,0 +1,25 @@
+from .zalgo import Zalgo
+from .uwu import Uwu
+from .crytyping import CryTyping
+from .sarcasm import Sarcasm
+from .substitute import Substitute
+from .figlet import Figletise
+from .character_helpers import \
+    BUBBLE_MAP, MEDIEVAL_MAP, UPSIDE_DOWN_MAP,\
+    FULLWIDTH_MAP, MORSE_MAP, SMALLCAPS_MAP
+
+TRANSFORMERS = {
+    'bubble': lambda: Substitute(BUBBLE_MAP),
+    'morse': lambda: Substitute(MORSE_MAP, ' '),
+    'crytyping': lambda: CryTyping(),
+    'medieval': lambda: Substitute(MEDIEVAL_MAP),
+    'fullwidth': lambda: Substitute(FULLWIDTH_MAP),
+    'uwu': lambda: Uwu(intense=False),
+    'UwU': lambda: Uwu(intense=True),
+    'sarcasm': lambda: Sarcasm(),
+    'figlet': lambda font='slant': Figletise(font),
+    'upsidedown': lambda: Substitute(UPSIDE_DOWN_MAP),
+    'smallcaps': lambda: Substitute(SMALLCAPS_MAP),
+    'zalgo': lambda minimum=1, maximum=3: Zalgo(int(minimum),
+                                                int(maximum))
+}
