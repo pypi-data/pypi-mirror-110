750f5b7ea2d1eea1f5e1cdc027aab1251c53807f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:07 +0000

    Add package TicTacToe3/0.0.1/TicTacToe3-0.0.1.tar.gz

---
+++ packages/TicTacToe3/TicTacToe3-0.0.1.tar.gz/TicTacToe3-0.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "A game of Tic Tac Toe, that you may RARELY WIN"
+
+file = open("readme.md", encoding="utf-8")
+LONG_DESCRIPTION = file.read()
+
+# Setting up
+setup(
+    name="TicTacToe3",
+    version=VERSION,
+    author="Programmin-in-Python (MK)",
+    author_email="<kalanithi6014@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    python_requires=">=3",
+    project_urls={"HomePage":"https://github.com/Programmin-in-Python/TicTacToe-cli"},
+    keywords=['python3', 'Tic Tac Toe', 'Tic-Tac-Toe', 'tic tac toe',
+                'tic-tac-toe', 'tic-tac-toe-cli', 'probability'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ]
+)
