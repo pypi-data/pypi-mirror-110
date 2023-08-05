25849c6e9b931cd2c2306a9961a6ab045395aef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:37 +0000

    Add package django-app-graphql/1.9.15/django_app_graphql-1.9.15-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.9.15-py3-none-any.whl/django_app_graphql/conf.py
@@ -0,0 +1,33 @@
+import os
+
+from django.conf import settings
+from appconf import AppConf
+
+
+class DjangoAppGraphQLAppConf(AppConf):
+    class Meta:
+        prefix = "DJANGO_APP_GRAPHQL"
+        #holder = "django_app_graphql.conf.settings"
+
+    BACKEND_TYPE: str = "graphene"
+    """
+    Type of the backend. May either be "graphene" or "ariadne". 
+    """
+    EXPOSE_GRAPHIQL = True
+    """
+    If set, we will expose the graphiql UI
+    """
+    GRAPHQL_SERVER_URL = ""
+    """
+    the endpoint where the graphql server is located
+    """
+    ENABLE_GRAPHQL_FEDERATION = True
+    """
+    If True, we will build the grpahql schema using graphql federation.
+    False to disable.
+    """
+    SAVE_GRAPHQL_SCHEMA = os.path.join("output", "graphql", "schema.graphql")
+    """
+    If not None, represents the file where we will dump the generated grpahql schema.
+    """
+
