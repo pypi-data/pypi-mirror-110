c09c503872436e2a432534e2cd7900af216da501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:30 +0000

    Add package polymarket-trading/0.1.0/polymarket_trading-0.1.0.tar.gz

---
+++ packages/polymarket-trading/polymarket_trading-0.1.0.tar.gz/polymarket_trading-0.1.0/polymarket/redeem.py
@@ -0,0 +1,12 @@
+from .utils import conditional_token_address, load_evm_abi, usdc_address
+
+hash_zero = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+
+def redeem(web3_provider, condition_id, num_outcomes):
+    conditional_token_abi = load_evm_abi('ConditionalTokens.json')
+
+    index_set = [1 << x for x in range(num_outcomes)]
+
+    contract = web3_provider.eth.contract(address=conditional_token_address, abi=conditional_token_abi)
+    return contract.functions.redeemPositions(usdc_address, hash_zero, condition_id, index_set).transact()
