da091557de2f372a76853b2f66a5384151a460ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:15 +0000

    Add package sc-dandelion/0.1.3.post1/sc-dandelion-0.1.3.post1.tar.gz

---
+++ packages/sc-dandelion/sc-dandelion-0.1.3.post1.tar.gz/sc-dandelion-0.1.3.post1/dandelion/logging/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# @Author: kt16
+# @Date:   2020-05-12 18:11:20
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2021-04-03 16:53:10
+
+from ._logging import print_versions, print_header
+from ._metadata import __version__, __author__, __email__, __url__, __docs__, __classifiers__
