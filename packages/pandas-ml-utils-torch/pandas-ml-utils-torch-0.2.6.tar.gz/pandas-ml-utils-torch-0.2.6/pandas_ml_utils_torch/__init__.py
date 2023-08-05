00333aa4fc4aad91bffc8a6f31697c11827313a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:37 +0000

    Add package pandas-ml-utils-torch/0.2.6/pandas-ml-utils-torch-0.2.6.tar.gz

---
+++ packages/pandas-ml-utils-torch/pandas-ml-utils-torch-0.2.6.tar.gz/pandas-ml-utils-torch-0.2.6/pandas_ml_utils_torch/__init__.py
@@ -0,0 +1,4 @@
+from .pytorch_model import PytorchModel, PytorchAutoEncoderModel
+from .pytorch_base import PytorchNN, PytorchNNFactory
+from .layers import Reshape, KerasLikeLSTM, ResidualLayer, Flatten, Squeeze, LambdaSplitter
+from .loss import MultiObjectiveLoss, CrossEntropyLoss, HeteroscedasticityLoss
