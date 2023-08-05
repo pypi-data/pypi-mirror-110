a9b48c0b4e4c8d042087d698aee2e8e41e99d284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:08 +0000

    Add package JSONifier/0.0.4/JSONifier-0.0.4.tar.gz

---
+++ packages/JSONifier/JSONifier-0.0.4.tar.gz/JSONifier-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setup(
+  name='JSONifier',
+  version='0.0.4',
+  description="A package for reading, writing, and editing of JSON files, Python dictionaries, and such.",
+  py_modules=["jsonify"],
+  package_dir={"": "src"},
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author="minecraftpr03",
+  author_email=None,
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
+  ]
+)
