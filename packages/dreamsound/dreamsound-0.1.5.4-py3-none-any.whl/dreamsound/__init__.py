04ea8280c5d70eb2c9522f390a4b8025bfde2092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package dreamsound/0.1.5.4/dreamsound-0.1.5.4-py3-none-any.whl

---
+++ packages/dreamsound/dreamsound-0.1.5.4-py3-none-any.whl/dreamsound/__init__.py
@@ -0,0 +1,8 @@
+###############################################################################
+#
+#  This file is part of the dreamsound package.
+#  
+#  packate initialization routines
+#
+###############################################################################
+from .dreamsound import DreamSound
