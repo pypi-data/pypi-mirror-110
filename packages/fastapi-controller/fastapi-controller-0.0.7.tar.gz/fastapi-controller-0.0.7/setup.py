44dc3e5558df742e8e79fe2ef2a5c4aeee04ce6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:27 +0000

    Add package fastapi-controller/0.0.7/fastapi-controller-0.0.7.tar.gz

---
+++ packages/fastapi-controller/fastapi-controller-0.0.7.tar.gz/fastapi-controller-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("Readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fastapi-controller",
+    version="0.0.7",
+    author="Akshar Patel",
+    author_email="akshar.patel.47@gmail.com",
+    description="Package to create API controllers that can inherit routes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aksharpatel47/fastapi-controller",
+    packages=["fastapi_controller"],
+    install_requires=[
+        "fastapi[all]>=0.65.2",
+    ],
+    extra_require={
+        'dev': [
+            "pytest>=6.2.4",
+            "pytest-asyncio>=0.15.1",
+            "httpx>=0.18.1"
+        ]
+    },
+    python_requires=">=3.6",
+)
