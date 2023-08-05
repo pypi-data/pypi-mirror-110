ae74352c285ee4aa73696fb5a849fc8c5595f0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:42 +0000

    Add package misty2py/4.1.5/misty2py-4.1.5.tar.gz

---
+++ packages/misty2py/misty2py-4.1.5.tar.gz/misty2py-4.1.5/misty2py/utils/env_loader.py
@@ -0,0 +1,19 @@
+"""EnvLoader class"""
+from typing import Optional
+
+from dotenv import dotenv_values
+
+
+class EnvLoader:
+    """Loads environmental values.
+
+    Attributes:
+        values [OrderedDict]: The loaded environmental values, might be empty.
+    """
+
+    def __init__(self, env_path: str = ".env") -> None:
+        self.values = dotenv_values(env_path)
+
+    def get_ip(self) -> Optional[str]:
+        """Obtains MISTY_IP_ADDRESS from environmental values."""
+        return self.values.get("MISTY_IP_ADDRESS")
