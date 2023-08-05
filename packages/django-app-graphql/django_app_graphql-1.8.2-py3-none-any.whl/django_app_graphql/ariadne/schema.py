f7f64691d5cc5aad877fa519085bd9487405cabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:23 +0000

    Add package django-app-graphql/1.8.2/django_app_graphql-1.8.2-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.8.2-py3-none-any.whl/django_app_graphql/ariadne/schema.py
@@ -0,0 +1,19 @@
+from django.apps import apps
+
+
+def get_schema():
+    """
+    The function scans your project and apps and fetch all the *.graphql files.
+
+    Then, it builds the schema
+    :return:
+    """
+
+    files = []
+
+    for app in apps.get_app_configs():
+        yield app
+
+    return ""
+
+
