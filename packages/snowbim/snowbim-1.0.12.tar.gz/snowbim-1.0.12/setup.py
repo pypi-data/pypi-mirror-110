4c0eea7d0e53f0a6eae2dce615bf139f8064ecfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:05 +0000

    Add package snowbim/1.0.12/snowbim-1.0.12.tar.gz

---
+++ packages/snowbim/snowbim-1.0.12.tar.gz/snowbim-1.0.12/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from snowbim import __VERSION__, __NAME__
+
+setup(
+     name=__NAME__,
+     version=__VERSION__,
+     author='datnguye',
+     author_email='datnguyen.it09@gmail.com',
+     packages=find_packages(),
+     url='https://github.com/datnguye/snowbim',
+     license='MIT',
+     description='A package to refresh power bi model .bim file',
+     long_description_content_type="text/markdown",
+     long_description=open('README.md').read(),
+     install_requires=[
+        'pyyaml==5.4.1',
+        'snowflake-connector-python==2.3.6',
+        'snowflake-connector-python[pandas]'
+     ],
+     python_requires='>=3.7.5',
+     entry_points = {
+        'console_scripts': [
+            'snowbim = snowbim.__main__:main'
+        ],
+    }
+)
