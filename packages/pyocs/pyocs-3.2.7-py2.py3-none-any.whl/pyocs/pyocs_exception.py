22e187ccdfa42acce75a90cc62fe1e24d3f65590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package pyocs/3.2.7/pyocs-3.2.7-py2.py3-none-any.whl

---
+++ packages/pyocs/pyocs-3.2.7-py2.py3-none-any.whl/pyocs/pyocs_exception.py
@@ -0,0 +1,40 @@
+class CustomerNoSupportError(Exception):
+    pass
+
+
+class ProjectNoSupportError(Exception):
+    pass
+
+
+class NoRegionError(Exception):
+    pass
+
+
+class OcsRegionError(Exception):
+    pass
+
+
+class NoSoftwareError(Exception):
+    pass
+
+
+class ReviewExcelError(Exception):
+    pass
+
+
+class ReadPwmError(Exception):
+    pass
+
+
+class No358GaiaAI(Exception):
+    pass
+
+
+class OcsSystemError(Exception):
+    pass
+
+class GitError(Exception):
+    pass
+
+class NoConfirmSoftwareError(Exception):
+    pass
