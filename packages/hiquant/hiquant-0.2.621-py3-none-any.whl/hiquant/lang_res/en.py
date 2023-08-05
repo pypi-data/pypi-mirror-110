0de850ebbad6be67eb42fcc23b4438a7e71b45ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:34 +0000

    Add package hiquant/0.2.621/hiquant-0.2.621-py3-none-any.whl

---
+++ packages/hiquant/hiquant-0.2.621-py3-none-any.whl/hiquant/lang_res/en.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from ..core.lang import add_lang
+
+add_lang('en', {
+    'buy': 'buy',
+    'sell': 'sell',
+    'stop loss': 'stop loss',
+    'stop earn': 'stop earn',
+    'signal': 'signal',
+})
