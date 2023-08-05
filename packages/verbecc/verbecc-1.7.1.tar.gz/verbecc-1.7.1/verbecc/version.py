401048b63ec248f46cc770041451f53bc24e2309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:19 +0000

    Add package verbecc/1.7.1/verbecc-1.7.1.tar.gz

---
+++ packages/verbecc/verbecc-1.7.1.tar.gz/verbecc-1.7.1/verbecc/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.7.1'
+__author__ = 'Brett Tolbert'
+__maintainer__ = 'Brett Tolbert'
+__copyright__ = 'Copyright (c) 2021 Brett Tolbert'
+__license__ = 'GNU General Public License (GPL)'
+__credits__ = ('Pierre Sarrazin', 'Sekou Diao')
+__status__ = 'Production'
