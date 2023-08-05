f3ea2c8c939c87cb10ca943041c10b6b90a82f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:28 +0000

    Add package comet-ml/3.12.1/comet_ml-3.12.1.tar.gz

---
+++ packages/comet-ml/comet_ml-3.12.1.tar.gz/comet_ml-3.12.1/comet_ml/file_utils.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# *******************************************************
+#   ____                     _               _
+#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
+# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
+# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
+#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
+#
+#  Sign up for free at http://www.comet.ml
+#  Copyright (C) 2015-2021 Comet ML INC
+#  This file can not be copied and/or distributed without the express
+#  permission of Comet ML Inc.
+# *******************************************************
+
+
+import hashlib
+
+
+def file_sha1sum(file_path):
+    sha1sum = hashlib.sha1()
+
+    with open(file_path, "rb") as source:
+        block = source.read(2 ** 16)
+        while len(block) != 0:
+            sha1sum.update(block)
+            block = source.read(2 ** 16)
+
+    return sha1sum.hexdigest()
