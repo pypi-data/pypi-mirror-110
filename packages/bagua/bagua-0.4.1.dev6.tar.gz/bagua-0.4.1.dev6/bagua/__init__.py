f2f909ec09b10c9c50b4c557323b14cd42e4baa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:05 +0000

    Add package bagua/0.4.1.dev6/bagua-0.4.1.dev6.tar.gz

---
+++ packages/bagua/bagua-0.4.1.dev6.tar.gz/bagua-0.4.1.dev6/bagua/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+Bagua_ is a communication library
+developed by Kuaishou Technology and DS3 Lab for deep learning.
+
+See tutorials_ for Bagua's rationale and benchmark.
+
+.. _Bagua: https://github.com/BaguaSys/bagua
+.. _tutorials: https://baguasys.github.io/tutorials/
+"""
+
+__docformat__ = "numpy"
+
+import bagua_core
+from . import torch_api, autotune, script, bagua_define
+from .version import __version__
