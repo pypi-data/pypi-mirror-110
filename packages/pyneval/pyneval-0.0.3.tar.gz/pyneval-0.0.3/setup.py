8b91d702b2bb932ec5e9e6f5ccad994662658e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:24 +0000

    Add package pyneval/0.0.3/pyneval-0.0.3.tar.gz

---
+++ packages/pyneval/pyneval-0.0.3.tar.gz/pyneval-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import glob
+
+setup(name='pyneval',
+      version='0.0.3',
+      description='pyneval first edition',
+      author='ZhangHan',
+      author_email='benniehan98@gmail.com',
+      url='https://github.com/bennieHan/PyNeval.git',
+      packages=['pyneval', 'pyneval.cli', 'test', 'test.test_model',
+                'test.test_model.diadem_metric', 'test.test_model.length_metric',
+                'pyneval.io', 'pyneval.metric', 'pyneval.metric.utils', 'pyneval.metric.utils.klib', 'pyneval.model',
+                'pyneval.tools', 'pyneval.tools.optimize'],
+      py_modules=['pyneval', 'pyneval.cli', 'test', 'test.test_model',
+                'test.test_model.diadem_metric', 'test.test_model.length_metric',
+                'pyneval.io', 'pyneval.metric', 'pyneval.metric.utils', 'pyneval.metric.utils.klib', 'pyneval.model',
+                'pyneval.tools', 'pyneval.tools.optimize'],
+      data_files=[('config/pyneval', glob.glob('config/*.json')),
+                  ("config/pyneval/schemas", glob.glob('config/schemas/*.json'))],
+      install_requires=[
+            'anytree>=2.7.2',
+            'kdtree>=0.16',
+            'numpy>=1.0',
+            'rtree>=0.8',
+            'jsonschema>=3.2.0'
+      ],
+      entry_points={
+          'console_scripts': [
+              'pyneval=pyneval.cli.pyneval:run',
+              'pyneval_tools=pyneval.cli.pyneval_tools:run',
+          ]
+      }
+)
+
