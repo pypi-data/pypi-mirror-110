6a2762639251e30ff7afd92e125691deb9fd35b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package korean-handspellpy/1.0.1/korean_handspellpy-1.0.1.tar.gz

---
+++ packages/korean-handspellpy/korean_handspellpy-1.0.1.tar.gz/korean_handspellpy-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+long_des = '''
+```py
+from handspell.handspell import spell_check as check
+print(check('text'))
+'''
+setup(
+    name='korean_handspellpy',
+    version='1.0.1',
+    description='handspell',
+    license='MIT',
+    author='hminkoo10',
+    author_email='hmin.koo10@gmail.com',
+    long_description=long_des,
+    long_description_content_type='text/markdown',
+
+    install_requires=[],
+
+    packages=[],
+    keywords=['맞춤법 검사','맞춥법','handspell','koreanhandspell','korean'],
+
+    python_requires='>=3'
+)
