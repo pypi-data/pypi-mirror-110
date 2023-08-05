8cdc1e132de225a3ee3f9553e7d50068e8c21a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:10 +0000

    Add package viewNtTest/0.0.3/viewNtTest-0.0.3.tar.gz

---
+++ packages/viewNtTest/viewNtTest-0.0.3.tar.gz/viewNtTest-0.0.3/menu/get_view.py
@@ -0,0 +1,12 @@
+from menu.menu.view import gui_window
+from menu.menu.menu_widget import FrameMenu
+
+def show():
+    gui = gui_window('menu/menu/img/favicon.png')
+    frame_menu = FrameMenu(gui.window)
+    frame_menu.frame_menu.grid()
+    gui.show()
+
+
+if __name__ == '__main__':
+    show()
