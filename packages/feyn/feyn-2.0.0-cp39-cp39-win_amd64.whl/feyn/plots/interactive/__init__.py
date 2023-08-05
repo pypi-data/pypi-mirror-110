3e9ea85a76a7e3c0a055bf1116066f8819bf5ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package feyn/2.0.0/feyn-2.0.0-cp39-cp39-win_amd64.whl

---
+++ packages/feyn/feyn-2.0.0-cp39-cp39-win_amd64.whl/feyn/plots/interactive/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module contains interactive counterparts of some plotting functions for interactive IPython environments (such as Jupyter).
+"""
+
+from ._graph_flow import interactive_activation_flow
+
+__all__ = [
+    "interactive_activation_flow"
+]
