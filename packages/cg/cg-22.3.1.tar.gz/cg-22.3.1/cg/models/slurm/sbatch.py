92f307fa861294bfef6f40ad0b9a241e53cc0eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:06 +0000

    Add package cg/22.3.1/cg-22.3.1.tar.gz

---
+++ packages/cg/cg-22.3.1.tar.gz/cg-22.3.1/cg/models/slurm/sbatch.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from pydantic import BaseModel
+from typing_extensions import Literal
+
+from cg.constants.priority import SlurmQos
+
+
+class Sbatch(BaseModel):
+    job_name: str
+    account: str
+    number_tasks: int
+    memory: int
+    log_dir: str
+    email: str
+    hours: int
+    minutes: str = "00"
+    priority: SlurmQos = SlurmQos.LOW
+    commands: str
+    error: Optional[str]
