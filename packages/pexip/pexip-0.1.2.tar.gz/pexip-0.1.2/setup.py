62dcb91daa5c98cecb9c50d9437ac9eb6722c752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:59 +0000

    Add package pexip/0.1.2/pexip-0.1.2.tar.gz

---
+++ packages/pexip/pexip-0.1.2.tar.gz/pexip-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pexip', 'pexip.cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['pexip = pexip.core:main']}
+
+setup_kwargs = {
+    'name': 'pexip',
+    'version': '0.1.2',
+    'description': 'CLI utility to manually provision a Pexip Transcoder Client with a Pexip Manager.',
+    'long_description': None,
+    'author': 'Colin Bruner',
+    'author_email': 'colin.d.bruner@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
