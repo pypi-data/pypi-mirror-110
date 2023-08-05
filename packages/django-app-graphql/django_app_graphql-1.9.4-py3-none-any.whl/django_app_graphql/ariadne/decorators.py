8ecd79c2353f3825e6abfb6ea98dc6ad6b932046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:00 +0000

    Add package django-app-graphql/1.9.4/django_app_graphql-1.9.4-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.9.4-py3-none-any.whl/django_app_graphql/ariadne/decorators.py
@@ -0,0 +1,10 @@
+from ariadne import QueryType, MutationType
+
+query = QueryType()
+"""
+Decorator used to register query resolvers
+"""
+mutation = MutationType()
+"""
+Decorator used to register mutation resolvers
+"""
