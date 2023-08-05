93eab6178b690e8d1d5c088c174f78ec7eb48093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:43 +0000

    Add package Tweezepy/1.0.3/Tweezepy-1.0.3.tar.gz

---
+++ packages/Tweezepy/Tweezepy-1.0.3.tar.gz/Tweezepy-1.0.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 16:31:29 2020
+
+Author: Ian L. Morgan
+email: ilmorgan@ucsb.edu
+"""
+import setuptools
+import json
+import os
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+pkginfo_path = os.path.join(this_directory,
+                            'tweezepy',
+                            'tweezepy_info.json')
+pkginfo = json.load(open(pkginfo_path))
+
+# This call to setup() does all the work
+setuptools.setup(name=pkginfo['name'],
+                 version=pkginfo['version'],
+                 description=pkginfo['description'],
+                 author=pkginfo['author'],
+                 author_email=pkginfo['author_email'],
+                 url=pkginfo['url'],
+                 license=pkginfo['license'],
+                 packages=setuptools.find_packages(),
+                 long_description=long_description,
+                 long_description_content_type = 'text/markdown',
+                 install_requires = ['numba',
+                                     'numpy',
+                                     'scipy',
+                                     'autograd',
+                                     'emcee'],
+                 tests_require=['unittest',
+                                'numpy'],
+                 ) 
