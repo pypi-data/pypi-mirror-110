f75357033597f927527d2b820e02edd9830423e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:42 +0000

    Add package cmlbib/0.1.1/cmlbib-0.1.1.tar.gz

---
+++ packages/cmlbib/cmlbib-0.1.1.tar.gz/cmlbib-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='cmlbib',
+    version='0.1.1',
+    py_modules=['cmlbib'],
+    install_requires=[
+        'Click==8.*',
+        'bibtexparser==1.2.0',
+        'tqdm==4.*'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cmlbib-sanitize = cmlbib:cli',
+            'cmlbib-export = cmlbib:export',
+        ],
+    },
+)
