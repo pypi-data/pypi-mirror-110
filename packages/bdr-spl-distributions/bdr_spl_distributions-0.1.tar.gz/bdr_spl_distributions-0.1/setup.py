9a848fecc92c007f0ad2a5e95f0874f35fdd37a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package bdr-spl-distributions/0.1/bdr_spl_distributions-0.1.tar.gz

---
+++ packages/bdr-spl-distributions/bdr_spl_distributions-0.1.tar.gz/bdr_spl_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bdr_spl_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bdr_spl_distributions'],
+      author = 'bdr spl',
+      zip_safe=False)
