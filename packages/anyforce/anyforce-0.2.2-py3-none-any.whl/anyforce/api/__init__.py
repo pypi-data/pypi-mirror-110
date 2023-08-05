5efb3084f8f07fc466bf439d6537877b2165f3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:34 +0000

    Add package anyforce/0.2.2/anyforce-0.2.2-py3-none-any.whl

---
+++ packages/anyforce/anyforce-0.2.2-py3-none-any.whl/anyforce/api/__init__.py
@@ -0,0 +1,3 @@
+from .api import API, CreateForm, UpdateForm
+
+__all__ = ["API", "UserModel", "Model", "CreateForm", "UpdateForm"]
---
+++ packages/anyforce/anyforce-0.2.2-py3-none-any.whl/anyforce/model/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseModel
+from .mock import MockModel
+from .recoverialbe import RecoverableModel
+
+__all__ = ["BaseModel", "RecoverableModel", "MockModel", "API"]
