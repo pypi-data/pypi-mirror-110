7f0f46c1d6c6a0db0372121bb428fd1425dfe6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:23 +0000

    Add package s3a/0.4.10/s3a-0.4.10.tar.gz

---
+++ packages/s3a/s3a-0.4.10.tar.gz/s3a-0.4.10/s3a/processing/guiwrapper.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import traceback
+import warnings
+
+import numpy as np
+
+from s3a.generalutils import augmentException
+from s3a.structures import ComplexXYVertices, XYVertices
+from utilitys import NestedProcWrapper
+from utilitys.fns import warnLater
+from .processing import *
+
+__all__ = ['ImgProcWrapper', 'NestedProcWrapper']
+
+class ImgProcWrapper(NestedProcWrapper):
+  output: np.ndarray
+
+  def run(self, **kwargs):
+    newIo = self._ioDictFromRunKwargs(kwargs)
+    try:
+      with warnings.catch_warnings():
+        warnings.simplefilter('ignore', UserWarning)
+        result = self.processor.run(newIo)
+    except Exception as ex:
+      msg = 'Exception during processor run:\n'
+      augmentException(ex, msg)
+      result = ProcessIO(image=kwargs['prevCompMask'])
+      warnLater(str(ex), UserWarning)
+
+    outImg = result['image'].astype(bool)
+    if outImg.ndim > 2:
+      outImg = np.bitwise_or.reduce(outImg, 2)
+    self.output = outImg
+    return outImg
+
+  def resultAsVerts(self, localEstimate=True):
+    initialList = ComplexXYVertices.fromBwMask(self.output)
+    if len(initialList) == 0:
+      return initialList
+    if not localEstimate:
+      return [ComplexXYVertices([lst]) for lst in initialList]
+    # else, all vertices belong to the same component
+    else:
+      return [initialList]
+
+  @staticmethod
+  def _ioDictFromRunKwargs(runKwargs):
+    image = runKwargs.get('image', None)
+    if image is None:
+      raise RuntimeError('Cannot run processor without an image')
+
+    runKwargs.setdefault('firstRun', True)
+    for name in 'fgVerts', 'bgVerts':
+      runKwargs.setdefault(name, XYVertices())
+
+    if runKwargs.get('prevCompMask', None) is None:
+      noPrevMask = True
+      runKwargs['prevCompMask'] = np.zeros(image.shape[:2], bool)
+    else:
+      noPrevMask = False
+
+    return ProcessIO(**runKwargs, noPrevMask=noPrevMask)
