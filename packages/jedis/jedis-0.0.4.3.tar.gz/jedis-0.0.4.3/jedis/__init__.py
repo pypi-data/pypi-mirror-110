a1d8dc41ac8f1b00062c048107194e3f7484f770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:01 +0000

    Add package jedis/0.0.4.3/jedis-0.0.4.3.tar.gz

---
+++ packages/jedis/jedis-0.0.4.3.tar.gz/jedis-0.0.4.3/jedis/__init__.py
@@ -0,0 +1,14 @@
+# Jedis
+# Code Json like Redis easier
+# t.me/Kobsser
+
+__version__ = "0.0.4.3"
+__author__ = "Kobsser"
+__copyright__ = "Copyright (C) 2021 Kobsser"
+__description__ = "t.me/Kobsser"
+
+from .jedis import Jedis, load, dump
+
+__all__ = [
+    Jedis, load, dump
+]
