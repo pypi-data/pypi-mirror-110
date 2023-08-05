f42773483bebe6ce5709cae0359104aaac1447ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package goodreads-user-scraper/0.0.8/goodreads_user_scraper-0.0.8-py3-none-any.whl

---
+++ packages/goodreads-user-scraper/goodreads_user_scraper-0.0.8-py3-none-any.whl/scraper/__main__.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+from urllib.request import urlopen
+import os
+import bs4
+from scraper import shelves
+
+
+def scrape_user(args):
+    shelves.get_all_shelves(args)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--user_id", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, default="goodreads-data")
+    args = parser.parse_args()
+
+    args.output_dir = (
+        args.output_dir if args.output_dir.endswith("/") else args.output_dir + "/"
+    )
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    scrape_user(args)
+
+
+if __name__ == "__main__":
+    main()
