b47bf72c943213e6c16186bfe22674981f0109cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package snappi/0.4.5/snappi-0.4.5-py2.py3-none-any.whl

---
+++ packages/snappi/snappi-0.4.5-py2.py3-none-any.whl/snappi/tests/test_create_meshed_flow.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+def test_create_meshed_flow(api):
+    """Demonstrates a fully meshed configuration
+    """
+    config = api.config()
+
+    for i in range(1, 33):
+        config.ports.port(name='Port %s' % i, location='localhost/%s' % i)
+        device = config.devices.device(name='Device %s' % i)[-1]
+        device.container_name = 'Port %s' % i
+        device.ethernet.name = 'Eth %s' % i
+        device.ethernet.ipv4.name = 'Ipv4 %s' % i
+
+    flow = config.flows.flow(name='Fully Meshed Flow')[0]
+    flow.tx_rx.device.tx_names = [tx.name for tx in config.devices]
+    flow.tx_rx.device.rx_names = [rx.name for rx in config.devices]
+    flow.tx_rx.device.mode = flow.tx_rx.device.MESH
+
+    flow.size.fixed = 128
+    flow.rate.pps = 1000
+    flow.duration.fixed_packets.packets = 10000
+    flow.packet.ethernet().vlan().ipv4().tcp()
+
+    print(config)
+
+
+if __name__ == '__main__':
+    pytest.main(['-vv', '-s', __file__])
