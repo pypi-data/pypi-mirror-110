256ed6a211e736e076dfcb646e4e49696ac4d2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:50 +0000

    Add package linpg/3.0.10/linpg-3.0.10.tar.gz

---
+++ packages/linpg/linpg-3.0.10.tar.gz/linpg-3.0.10/src/linpg/__init__.py
@@ -0,0 +1,9 @@
+"""
+结构:
+config -> lang -> api -> core -> ui -> dialog -> battle -> interface
+"""
+from .interface import *
+import platform
+
+print("linpg {0} (pygame {1}, python {2})".format(get_current_version(),pygame.version.ver,platform.python_version()))
+print("Hello from the linpg community. {}".format(get_repository_url()))
