fd44d28ada70a8c282d8b44c0c3907b4420812c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:08 +0000

    Add package arc-utils/1.1/arc_utils-1.1.tar.gz

---
+++ packages/arc-utils/arc_utils-1.1.tar.gz/arc_utils-1.1/tests/conftest.py
@@ -0,0 +1,81 @@
+"""module wide test fixtures"""
+import pytest
+import arcpy
+import os
+import random
+import collections
+
+testfolder = 'tests'
+
+@pytest.fixture(scope='module')
+def testgdb(pytestconfig):
+    gdb_name = "arc_utils_test_tmp.gdb"
+    return str(pytestconfig.rootdir.join(testfolder).join(gdb_name))
+
+@pytest.fixture(scope='module')
+def testmxd(pytestconfig):
+    mxd_name = "arc_utils_test.mxd"
+    return str(pytestconfig.rootdir.join(testfolder).join(mxd_name))
+
+@pytest.fixture(scope='module')
+def testaprx(pytestconfig):
+    aprx_name = "arc_utils_test_pro.aprx"
+    return str(pytestconfig.rootdir.join(testfolder).join(aprx_name))
+
+@pytest.fixture(scope='module')
+def testdatabase(request, testgdb):
+    # create a testing gdb
+    print("Creating test geodatabase")
+    testgdbpath = testgdb # os.path.join(arcpy.env.scratchFolder, "arc_utils_test.gdb")
+    if arcpy.Exists(testgdbpath):
+        print("Deleting " + testgdbpath)
+        arcpy.Delete_management(testgdbpath)
+    arcpy.CreateFileGDB_management(os.path.dirname(testgdbpath), os.path.basename(testgdbpath))
+    # add a domain for each field in testfc
+    ftext_dom_name = "ftext_coded"
+    fint_dom_name = "fint_range"
+    arcpy.CreateDomain_management(in_workspace=testgdbpath,
+                                  domain_name=ftext_dom_name,
+                                  domain_description="text domain",
+                                  field_type="TEXT",
+                                  domain_type="CODED")
+    arcpy.CreateDomain_management(in_workspace=testgdbpath,
+                                  domain_name=fint_dom_name,
+                                  field_type="SHORT",
+                                  domain_type="RANGE")
+    ftextDict = {"val1": "val1", "val2": "val2", "val3": "val3"}
+    for code in ftextDict:
+        arcpy.AddCodedValueToDomain_management(testgdbpath, ftext_dom_name, code, ftextDict[code])
+    arcpy.SetValueForRangeDomain_management(testgdbpath, fint_dom_name, 1, 12)
+    print("Creating test featureclasses")
+    sr = arcpy.SpatialReference(4326)
+    fc_names = ['test_fc', 'test_fc2']
+    fc_paths = []
+    fc_fields = (('ftext', 'TEXT', None, None, 20, '', 'NULLABLE', 'NON_REQUIRED', ftext_dom_name),
+                 ('fint', 'SHORT', 0, 0, 0, '', 'NULLABLE', 'NON_REQUIRED', fint_dom_name))
+
+    for name in fc_names:
+        arcpy.CreateFeatureclass_management(testgdbpath, name, "POINT", spatial_reference=sr)
+        fc = os.path.join(testgdbpath, name)
+        fc_paths.append(fc)
+        for fc_field in fc_fields:
+            arcpy.AddField_management(fc, *fc_field)
+        print("loading data")
+        records = (("val1", None),("val1", 4), ("val1", 4), ("val2", 7), ("val02", 7), ("val1", 10), ("val2", 5), ("val1", 10), ("val2", 5), ("val1", None), (None, 5))
+        with arcpy.da.InsertCursor(fc, ["ftext", "fint", "SHAPE@XY"]) as cursor:
+            for key, val in records:
+                lon = -122.3 + float(random.randint(-9, 9)) / 100
+                lat = 47.6 + float(random.randint(-9, 9)) / 100
+                cursor.insertRow([key, val, (lon, lat)])
+
+    result = collections.namedtuple('testdata', 'gdb, test_fc1, test_fc2')
+    result.gdb = testgdbpath
+    result.fc1 = fc_paths[0]
+    result.fc2 = fc_paths[1]
+
+    def testdata_teardown():
+        print('Deleting test geodatabase')
+        arcpy.Delete_management(testgdbpath)
+
+    request.addfinalizer(testdata_teardown)
+    return result
---
+++ packages/arc-utils/arc_utils-1.1.tar.gz/arc_utils-1.1/tests/test_table.py
@@ -0,0 +1,69 @@
+from arc_utils import table
+import collections
+import os
+
+def test_tableobj_properties(testdatabase):
+    # test table object properties
+    testdata = testdatabase
+    tbl = table.TableObj(testdata.fc1)
+    assert tbl.name == 'test_fc'
+    assert tbl.path == testdata.fc1
+    assert tbl.type == u'Point'
+    assert tbl.fields == [u'OBJECTID', u'Shape', u'ftext', u'fint']
+    assert tbl.fields2 == [u'ftext', u'fint']
+    assert tbl.field_dict['Shape']['type'] == u'Geometry'
+    assert tbl.field_dict['ftext']['aliasName']== u'ftext'
+    assert tbl.field_dict['OBJECTID']['required']== True
+    assert tbl.field_dict['fint']['type']== u'SmallInteger'
+
+
+def test_tableobj_single_text_field_methods(testdatabase):
+    # test table object methods
+    tbl = table.TableObj(testdatabase.fc1)
+    ftext_set = tbl.get_field_value_set('ftext')
+    assert isinstance(ftext_set, set)
+    assert sorted(ftext_set) == [u'NULL', 'val02', 'val1', 'val2']
+    assert tbl.get_max_field_value('ftext') == 'val2'
+    assert tbl.get_max_field_value('ftext', True) == 'val02'
+    assert tbl.get_max_field_value_length('ftext') == 5
+
+
+def test_tableobj_single_text_field_methods(testdatabase):
+    # test table object methods
+    tbl = table.TableObj(testdatabase.fc1)
+    fint_set = tbl.get_field_value_set('fint')
+    assert isinstance(fint_set, set)
+    assert tbl.get_field_value_set('fint') == set([4, 5, 7, 10, u'NULL'])
+    assert tbl.get_max_field_value('fint') == 10
+    assert tbl.get_max_field_value_length('fint') == 2
+
+
+def test_tableobj_multi_field_methods(testdatabase):
+    # test table object methods
+    tbl = table.TableObj(testdatabase.fc1)
+    multi_field = tbl.get_multiple_field_value_set(['ftext', 'fint'])
+    assert isinstance(multi_field, set)
+    assert sorted(multi_field) == [u'NULL:5', u'val02:7', u'val1:0', u'val1:10', u'val1:4', u'val1:5', u'val2:5', u'val2:7']
+   
+    
+
+def test_tableobj_compare_methods(testdatabase):
+    # test table object compare methods
+    tbl = table.TableObj(testdatabase.fc1)
+    result = tbl.compare_field_values_to_domain('fint', testdatabase.gdb, "fint_range")
+    result.match.sort()
+    assert result.match == [4, 5, 7, 10]
+    assert result.unmatched == []
+
+    result = tbl.compare_field_values_to_domain('ftext', testdatabase.gdb, "ftext_coded")
+    result.match.sort()
+    result.unmatched.sort()
+    assert result.match == ['val1', 'val2']
+    assert result.unmatched == ['NULL', 'val02']
+
+
+def test_table_fc_methods(testdatabase):
+    # test non-object table methods
+    #table.compare_schema(testdata.fc, testdata.fc2)
+    assert True
+
