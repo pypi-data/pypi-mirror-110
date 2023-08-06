98e8018c52cbab6b2f2feea39890a91d11c040e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:59 +0000

    Add package dacite-ignore-case/1.0.0/dacite_ignore_case-1.0.0.tar.gz

---
+++ packages/dacite-ignore-case/dacite_ignore_case-1.0.0.tar.gz/dacite_ignore_case-1.0.0/setup.py
@@ -0,0 +1,35 @@
+import os
+
+import setuptools
+
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(own_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='dacite_ignore_case',
+    version='1.0.0',
+    description='Extends dacite.from_dict with a ignore case function',
+    author='zkdev',
+    author_email='dev@zeekay.dev',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    project_urls={
+        'Homepage': 'https://github.com/zkdev/dacite-ignore-case',
+        'Bug Tracker': 'https://github.com/zkdev/dacite-ignore-case/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+       'dacite',
+    ],
+    license='MIT',
+    python_requires='>=3.9.*',
+    package_dir={'': 'dacite_ignore_case'},
+    packages=setuptools.find_packages(where='dacite_ignore_case'),
+)
