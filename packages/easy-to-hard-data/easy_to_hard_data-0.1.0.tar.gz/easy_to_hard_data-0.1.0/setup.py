79f0de76fedcf6863157e2099736883179e7579f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:18 +0000

    Add package easy-to-hard-data/0.1.0/easy_to_hard_data-0.1.0.tar.gz

---
+++ packages/easy-to-hard-data/easy_to_hard_data-0.1.0.tar.gz/easy_to_hard_data-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='easy_to_hard_data',
+    version='0.1.0',
+    py_modules=['easy_to_hard_data'],
+    install_requires=[
+        'numpy',
+        'torch',
+        'torchvision',
+        'tqdm'
+    ]
+)
