60a4e3dbf1e9566321034655b3eff605048dc098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:31 +0000

    Add package saveit/1.2.1/saveit-1.2.1.tar.gz

---
+++ packages/saveit/saveit-1.2.1.tar.gz/saveit-1.2.1/README.md
@@ -0,0 +1,9 @@
+# SaveIt
+Just a hello world kind of package.
+
+A basic package to write data in a file directly from command line
+
+###  Usage
+$ saveit --string "hello world" foo.txt
+
+$ saveit --help
---
+++ packages/saveit/saveit-1.2.1.tar.gz/saveit-1.2.1/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+
+setup(
+    name="saveit",
+    version="1.2.1",
+    description="A utility to write text in a file right from terminal",
+    long_description_content_type="text/markdown",
+    author="Subham Singh",
+    author_email="imsks007@gmail.com",
+    url="https://github.com/imsubhamsingh/saveit",
+    issues="https://github.com/imsubhamsingh/saveit/issues",
+    license="MIT License",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    py_modules=["saveit"],
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["Click"],
+    entry_points={"console_scripts": ["saveit=saveit:cli"]},
+)
