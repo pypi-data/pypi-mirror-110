764101c79ebd36fee174d9539724cc5d4a28ef48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:24 +0000

    Add package ti-python-module/0.8.2/ti_python_module-0.8.2-py3-none-any.whl

---
+++ packages/ti-python-module/ti_python_module-0.8.2-py3-none-any.whl/ti_python_module/__init__.py
@@ -0,0 +1,5 @@
+from ti_python_module.ti_rover import *
+from ti_python_module.ti_system import *
+from ti_python_module.ti_hub import *
+from ti_python_module.ti_draw import *
+from ti_python_module.__err import *
