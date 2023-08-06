6a4853015ac3541a48baf5b19cd23a053cceba6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:55 +0000

    Add package apysc/0.18.1/apysc-0.18.1-py3-none-any.whl

---
+++ packages/apysc/apysc-0.18.1-py3-none-any.whl/apysc/__init__.py
@@ -0,0 +1,54 @@
+from apysc.type.int import Int  # isort:skip # noqa
+from apysc.type.number import Number  # isort:skip # noqa
+from apysc.type.boolean import Boolean  # isort:skip # noqa
+from apysc.type.string import String  # isort:skip # noqa
+from apysc.type.array import Array  # isort:skip # noqa
+from apysc.type.dictionary import Dictionary  # isort:skip # noqa
+from apysc.type.any_value import AnyValue  # isort:skip # noqa
+from apysc.branch._if import If  # isort:skip # noqa
+from apysc.branch._elif import Elif  # isort:skip # noqa
+from apysc.branch._else import Else  # isort:skip # noqa
+from apysc.loop._for import For  # isort:skip # noqa
+from apysc.display.display_object import DisplayObject  # isort:skip # noqa
+from apysc.display._document import document  # isort:skip # noqa
+from apysc.display.sprite import Sprite  # isort:skip # noqa
+from apysc.display.graphics import Graphics  # isort:skip # noqa
+from apysc.display.stage import Stage  # isort:skip # noqa
+from apysc.display.rectangle import Rectangle  # isort:skip # noqa
+from apysc.display.circle import Circle  # isort:skip # noqa
+from apysc.display.ellipse import Ellipse  # isort:skip # noqa
+from apysc.display.line import Line  # isort:skip # noqa
+from apysc.display.polyline import Polyline  # isort:skip # noqa
+from apysc.display.polygon import Polygon  # isort:skip # noqa
+from apysc.display.line_caps import LineCaps  # isort:skip # noqa
+from apysc.display.line_joints import LineJoints  # isort:skip # noqa
+from apysc.display.line_dot_setting import LineDotSetting  # isort:skip # noqa
+from apysc.display.line_dash_setting import LineDashSetting  # isort:skip # noqa
+from apysc.display.line_round_dot_setting import LineRoundDotSetting  # isort:skip # noqa
+from apysc.display.line_dash_dot_setting import LineDashDotSetting  # isort:skip # noqa
+from apysc.geom.point2d import Point2D  # isort:skip # noqa
+from apysc.event.event import Event  # isort:skip # noqa
+from apysc.event.mouse_event import MouseEvent  # isort:skip # noqa
+from apysc.event.wheel_event import WheelEvent  # isort:skip # noqa
+from apysc.event.event_type import EventType  # isort:skip # noqa
+from apysc.console._trace import trace  # isort:skip # noqa
+from apysc.console.assertion import assert_equal  # isort:skip # noqa
+from apysc.console.assertion import assert_not_equal  # isort:skip # noqa
+from apysc.console.assertion import assert_true  # isort:skip # noqa
+from apysc.console.assertion import assert_false  # isort:skip # noqa
+from apysc.console.assertion import assert_arrays_equal  # isort:skip # noqa
+from apysc.console.assertion import assert_arrays_not_equal  # isort:skip # noqa
+from apysc.console.assertion import assert_dicts_equal  # isort:skip # noqa
+from apysc.console.assertion import assert_dicts_not_equal  # isort:skip # noqa
+from apysc.console.assertion import assert_defined  # isort:skip # noqa
+from apysc.console.assertion import assert_undefined  # isort:skip # noqa
+from apysc.event.document_mouse_wheel_interface import \
+    bind_wheel_event_to_document  # isort:skip # noqa
+from apysc.event.document_mouse_wheel_interface import \
+    unbind_wheel_event_all_from_document  # isort:skip # noqa
+from apysc.event.document_mouse_wheel_interface import \
+    unbind_wheel_event_from_document  # isort:skip # noqa
+
+from apysc.html.exporter import save_overall_html  # isort:skip # noqa
+
+__version__: str = '0.18.1'
