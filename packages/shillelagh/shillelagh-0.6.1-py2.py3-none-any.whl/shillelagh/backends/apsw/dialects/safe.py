3fbcbab336d5499b8b4a6d8f2693182c9a813efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:21 +0000

    Add package shillelagh/0.6.1/shillelagh-0.6.1-py2.py3-none-any.whl

---
+++ packages/shillelagh/shillelagh-0.6.1-py2.py3-none-any.whl/shillelagh/backends/apsw/dialects/safe.py
@@ -0,0 +1,50 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+
+from shillelagh.backends.apsw.dialects.base import APSWDialect
+from sqlalchemy.engine.url import URL
+
+
+class APSWSafeDialect(APSWDialect):
+    def __init__(
+        self,
+        adapters: Optional[List[str]] = None,
+        adapter_args: Optional[Dict[str, Any]] = None,
+        adapter_kwargs: Optional[Dict[str, Dict[str, Any]]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        super().__init__(*args, **kwargs)
+        self._adapters = adapters
+        self._adapter_args = adapter_args
+        self._adapter_kwargs = adapter_kwargs
+        self._safe = True
+
+    def create_connect_args(
+        self,
+        url: URL,
+    ) -> Tuple[
+        Tuple[
+            str,
+            Optional[List[str]],
+            Optional[Dict[str, Tuple[Any, ...]]],
+            Optional[Dict[str, Dict[str, Any]]],
+            bool,
+            Optional[str],
+        ],
+        Dict[str, Any],
+    ]:
+        return (
+            (
+                ":memory:",
+                self._adapters,
+                self._adapter_args,
+                self._adapter_kwargs,
+                True,
+                self.isolation_level,
+            ),
+            {},
+        )
---
+++ packages/shillelagh/shillelagh-0.6.1-py2.py3-none-any.whl/shillelagh/fields.py
@@ -0,0 +1,222 @@
+import datetime
+from distutils.util import strtobool
+from enum import Enum
+from typing import Any
+from typing import Callable
+from typing import cast
+from typing import List
+from typing import Optional
+from typing import Type
+from typing import TypeVar
+
+import dateutil.parser
+from shillelagh.filters import Filter
+
+T = TypeVar("T")
+
+
+class Order(Enum):
+    # Use ASCENDING/DESCENDING when you have static data with 1+
+    # columns pre-sorted. All other columns should have Order.NONE.
+    ASCENDING = "ascending"
+    DESCENDING = "descending"
+
+    # Use NONE when you can't or don't want to sort the data. Sqlite
+    # will then sort the provided data according to the query.
+    NONE = "none"
+
+    # Use ANY when the column can be sorted in any order. Usually
+    # all other columns will also have Order.ANY.
+    ANY = "any"
+
+
+class Field:
+
+    type = ""
+    db_api_type = "DBAPIType"
+
+    def __init__(
+        self,
+        filters: Optional[List[Type[Filter]]] = None,
+        order: Order = Order.NONE,
+        exact: bool = False,
+    ):
+        self.filters = filters or []
+        self.order = order
+        self.exact = exact
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Field):
+            return NotImplemented
+
+        return (
+            self.filters == other.filters
+            and self.order == other.order
+            and self.exact == other.exact
+        )
+
+    @staticmethod
+    def parse(value: Any) -> Any:
+        raise NotImplementedError("Subclasses must implement `parse`")
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        raise NotImplementedError("Subclasses must implement `quote`")
+
+
+class Integer(Field):
+    type = "INTEGER"
+    db_api_type = "NUMBER"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[int]:
+        if value is None:
+            return None
+
+        return int(value)
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        return str(value)
+
+
+class Float(Field):
+    type = "REAL"
+    db_api_type = "NUMBER"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[float]:
+        if value is None:
+            return None
+
+        return float(value)
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        return str(value)
+
+
+class String(Field):
+    type = "TEXT"
+    db_api_type = "STRING"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[str]:
+        if value is None:
+            return None
+
+        return str(value)
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        escaped_value = value.replace("'", "''")
+        return f"'{escaped_value}'"
+
+
+class Date(Field):
+    type = "DATE"
+    db_api_type = "DATETIME"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[datetime.date]:
+        if value is None:
+            return None
+
+        try:
+            dt = dateutil.parser.parse(value)
+        except dateutil.parser.ParserError:
+            return None
+
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=datetime.timezone.utc)
+
+        return dt.astimezone(datetime.timezone.utc).date()
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        return f"'{value.isoformat()}'"
+
+
+class Time(Field):
+    type = "TIME"
+    db_api_type = "DATETIME"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[datetime.time]:
+        if value is None:
+            return None
+
+        try:
+            dt = dateutil.parser.parse(value)
+        except dateutil.parser.ParserError:
+            return None
+
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=datetime.timezone.utc)
+
+        return dt.astimezone(datetime.timezone.utc).timetz()
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        return f"'{value.isoformat()}'"
+
+
+class DateTime(Field):
+    type = "TIMESTAMP"
+    db_api_type = "DATETIME"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[datetime.datetime]:
+        if value is None:
+            return None
+
+        try:
+            dt = dateutil.parser.parse(value)
+        except dateutil.parser.ParserError:
+            return None
+
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=datetime.timezone.utc)
+
+        return dt.astimezone(datetime.timezone.utc)
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        return f"'{value.isoformat()}'"
+
+
+class Blob(Field):
+    type = "BLOB"
+    db_api_type = "BINARY"
+
+    @staticmethod
+    def parse(value: T) -> T:
+        return value
+
+    @staticmethod
+    def quote(value: bytes) -> str:
+        return f"X'{value.hex()}'"
+
+
+class Boolean(Field):
+    type = "BOOLEAN"
+    db_api_type = "NUMBER"
+
+    @staticmethod
+    def parse(value: Any) -> Optional[bool]:
+        if value is None:
+            return None
+
+        if isinstance(value, bool):
+            return value
+        return bool(strtobool(str(value)))
+
+    @staticmethod
+    def quote(value: Any) -> str:
+        return "TRUE" if value else "FALSE"
+
+
+type_map = {
+    field.type: field
+    for field in {Integer, Float, String, Date, Time, DateTime, Blob, Boolean}
+}
