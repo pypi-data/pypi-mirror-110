72f6fa19175ae411e30bff57555360f174cb14a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package ninia/0.0.12/ninia-0.0.12.tar.gz

---
+++ packages/ninia/ninia-0.0.12.tar.gz/ninia-0.0.12/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ninia",
+    version="0.0.12",
+    author="Alex Summers",
+    author_email="ajs0201@auburn.edu",
+    description="A small Python wrapper for Quantum Espresso - still in development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ajsummers/ninia",
+    project_urls={
+        'Issues': 'https://github.com/ajsummers/ninia/issues'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    package_data={
+        'ninia': ['*.i', '*.sh']
+    },
+    python_requires=">=3.6",
+)
