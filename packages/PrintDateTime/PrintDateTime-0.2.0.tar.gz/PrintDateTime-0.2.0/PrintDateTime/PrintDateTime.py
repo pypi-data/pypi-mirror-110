46ed4a1671ab46698e0f80a7569291de1f8618a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:20 +0000

    Add package PrintDateTime/0.2.0/PrintDateTime-0.2.0.tar.gz

---
+++ packages/PrintDateTime/PrintDateTime-0.2.0.tar.gz/PrintDateTime-0.2.0/PrintDateTime/PrintDateTime.py
@@ -0,0 +1,13 @@
+"""
+Terms of Service
+Purchase
+Privacy Policy
+Copyright 2021 Gregory House Nicholas Meng
+Dogecoin: 
+Bitcoin: 
+Paypal: 
+"""
+from datetime import datetime
+print(datetime.now())
+
+
---
+++ packages/PrintDateTime/PrintDateTime-0.2.0.tar.gz/PrintDateTime-0.2.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(
+name='PrintDateTime',
+version='0.2.0',
+description='DESCRIPTION ENCOMPASSES',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+license='Python. Free to use no attribution required.',
+packages=['PrintDateTime'],
+install_requires=[])
