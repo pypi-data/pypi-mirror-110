6c8ae80f58ef9969945042531df739f20d8e4e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:57 +0000

    Add package BetterText/1.0.6/BetterText-1.0.6-py3-none-any.whl

---
+++ packages/BetterText/BetterText-1.0.6-py3-none-any.whl/BetterText/__init__.py
@@ -0,0 +1,5 @@
+from BetterText.BetterText import Color
+from BetterText.BetterText import Style
+from BetterText.BetterText import Background
+from BetterText.BetterText import RgbColor
+from BetterText.BetterText import RgbColorText
