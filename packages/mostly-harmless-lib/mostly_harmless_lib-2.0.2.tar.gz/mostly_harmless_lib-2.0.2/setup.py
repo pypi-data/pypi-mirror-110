3adb56c294430ffda31b1027ae816f89d45c7edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:26 +0000

    Add package mostly-harmless-lib/2.0.2/mostly_harmless_lib-2.0.2.tar.gz

---
+++ packages/mostly-harmless-lib/mostly_harmless_lib-2.0.2.tar.gz/mostly_harmless_lib-2.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mostly_harmless_lib',
+    version='2.0.2',
+    description='Mostly Harmless Library for Python',
+    long_description='This is a harmless or after some experience with it mostly harmless library for Python',
+    author='Wolfgang Kühn',
+    author_email=None,
+    maintainer=None,
+    maintainer_email=None,
+    packages=['mostly-harmless-lib']
+)
