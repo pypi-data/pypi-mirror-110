fd8379f2bbb18e259d60fc65e17dac30d5f4358a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:37 +0000

    Add package dpy-docs/0.1.1/dpy-docs-0.1.1.tar.gz

---
+++ packages/dpy-docs/dpy-docs-0.1.1.tar.gz/dpy-docs-0.1.1/dpy_docs/main.py
@@ -0,0 +1,25 @@
+import discord
+import aiofiles
+import asyncio
+
+from textwrap import dedent
+
+
+class DocGen:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def generate_docs(self,  command) -> None:
+        async with aiofiles.open("docs.md", "a") as file:
+            await file.write(dedent(f"""
+                **Command Name: {command.name}**
+                **Command Description: {command.help}
+                <br>
+                <br>
+                """))
+
+    def document_code(self):
+        def wrapper(func):
+            asyncio.run(self.generate_docs(func))
+
+        return wrapper
