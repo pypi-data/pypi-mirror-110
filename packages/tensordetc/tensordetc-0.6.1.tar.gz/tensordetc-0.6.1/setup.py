c5b4fc0d0eccfe9b9bff1bed2fbf2dfc8333b435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:22 +0000

    Add package tensordetc/0.6.1/tensordetc-0.6.1.tar.gz

---
+++ packages/tensordetc/tensordetc-0.6.1.tar.gz/tensordetc-0.6.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="tensordetc",
+  version="0.6.1",
+  author="Jeff Dean",
+  description="A small detc package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
