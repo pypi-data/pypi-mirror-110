7670d9eaf791b4df4b7569f626b510e381f390a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:21 +0000

    Add package swahilinlp/0.1.3/swahilinlp-0.1.3.tar.gz

---
+++ packages/swahilinlp/swahilinlp-0.1.3.tar.gz/swahilinlp-0.1.3/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+setup(
+    name='swahilinlp',
+    version='0.1.3',
+    description='Swahili Natural Language Processing (NLP) Library',
+    url="https://github.com/shebogholo/swahilinlp",
+    author='Waziri Shebogholo',
+    author_email="shebogholo@gmail.com",
+    license="MIT",
+    packages=["swahilinlp"],
+    install_requires=[
+        'requests'
+    ],
+    keywords = ['Swahili', 'Tanzania']
+)
---
+++ packages/swahilinlp/swahilinlp-0.1.3.tar.gz/swahilinlp-0.1.3/swahilinlp/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import requests
+
+# text = "Waziri anatoka Tanga"
+
+class SwahiliNLP(object):
+    def __init__(self):
+        self.NER_URL = "https://belltro.xyz/api/v1/ner-swahili"
+        self.SENTIMENT_URL = "https://belltro.xyz:81/analyse"
+        self.LANGID_URL = "https://belltro.xyz:124/v1/langid"
+
+    @staticmethod
+    def get_headers():
+        return {"Content-Type": "application/json"}
+
+    # NER
+    def ner(self, sentence, json: bool = True):
+        try:
+            response = self._get_response(url=self.NER_URL, sentence=sentence)
+            return response
+        except:
+            return None
+
+    def sentiment(self, sentence, json: bool = True):
+        try:
+            response = self._get_response(url=self.SENTIMENT_URL, sentence=sentence)
+            return response
+        except:
+            return None
+    
+    # African Languages Identification
+    def langid(self, sentence, json: bool = True):
+        try:
+            response = self._get_response(url=self.LANGID_URL, sentence=sentence)
+            return response
+        except:
+            return None
+
+    def _get_response(self, url, sentence):
+        try:
+            response = requests.get(
+                url, json={"text": sentence}, headers=self.get_headers()
+            ).json()
+            return response
+        except (requests.ConnectionError, requests.ConnectTimeout):
+            raise ConnectionError("Ooops..! Something wrong happened.")
+
+
+sys.modules[__name__] = SwahiliNLP()
