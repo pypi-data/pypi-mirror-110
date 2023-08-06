21ff7de0f1501b59c1ae183634dab6cfba95f9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:10 +0000

    Add package checkdigit/0.3.0/checkdigit-0.3.0.tar.gz

---
+++ packages/checkdigit/checkdigit-0.3.0.tar.gz/checkdigit-0.3.0/checkdigit/_data.py
@@ -0,0 +1,37 @@
+# /usr/bin/env python
+
+# This file is part of checkdigit.
+
+# checkdigit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# checkdigit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with checkdigit.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Data cleansing.
+
+This module contains functions that help to format data so that it can be interpreted easier.
+
+"""
+
+
+def cleanse(data: str) -> str:
+    """Removes Hyphens and Spaces so that data can be parsed."""
+    return data.replace("-", "").replace(" ", "")
+
+
+def convert(digit: int, isbn: bool = True) -> str:
+    """Converts digits to strings and replaces 10 and 11.
+
+    Args:
+        digit: The code to be modified.
+        isbn: Whether the code is ISBN or not.
+    """
+    if digit == 10:
+        return "X" if isbn else "0"
+    return "0" if digit == 11 else str(digit)
