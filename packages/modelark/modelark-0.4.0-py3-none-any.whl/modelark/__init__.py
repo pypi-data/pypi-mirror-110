d51adda1dc955c367b19b17c839161af9b8e57bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:31 +0000

    Add package modelark/0.4.0/modelark-0.4.0-py3-none-any.whl

---
+++ packages/modelark/modelark-0.4.0-py3-none-any.whl/modelark/__init__.py
@@ -0,0 +1,10 @@
+from .common import (
+    Entity, DataDict, RecordList)
+from .filterer import Domain
+from .repository import (
+    Repository, MemoryRepository,
+    JsonRepository, SqlRepository, RestRepository)
+
+__author__ = 'Knowark'
+__email__ = 'info@knowark.com'
+__version__ = '0.4.0'
