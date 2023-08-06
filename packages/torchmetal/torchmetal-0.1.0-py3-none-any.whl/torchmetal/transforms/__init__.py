43d8ea7e5cabe4bcc157b51936f1c263de3c2444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:48 +0000

    Add package torchmetal/0.1.0/torchmetal-0.1.0-py3-none-any.whl

---
+++ packages/torchmetal/torchmetal-0.1.0-py3-none-any.whl/torchmetal/transforms/__init__.py
@@ -0,0 +1,8 @@
+from torchmetal.transforms.augmentations import (HorizontalFlip, Rotation,
+                                                 VerticalFlip)
+from torchmetal.transforms.categorical import Categorical, FixedCategory
+from torchmetal.transforms.splitters import (ClassSplitter, Splitter,
+                                             WeightedClassSplitter)
+from torchmetal.transforms.target_transforms import (DefaultTargetTransform,
+                                                     SegmentationPairTransform,
+                                                     TargetTransform)
