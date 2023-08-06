6d8023231c0c7bf8e45ed046f2f043c839b639b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:53 +0000

    Add package pystmm/0.1.0/pystmm-0.1.0.tar.gz

---
+++ packages/pystmm/pystmm-0.1.0.tar.gz/pystmm-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pystmm',
+      version='0.1.0',
+      description='Support Tensor Machine Multiclassifier',
+      py_modules=["classifier"],
+      url='https://github.com/cdfbdex/pySTMM',
+      author='Carlos Ferrin-Bolaños, Wilfredo Alfonso-Morales and Humberto Loaiza-Correa',
+      author_email='cdfbdex@gmail.com',
+      license='MIT',
+      extras_require = {
+            "dev": [
+                "pytest >= 3.2",
+                "check-manifest",
+                "twine",
+            ],
+      },
+      packages=find_packages(),
+      install_requires=['mne', 'wget', 'plotly', 'numpy', 'scipy', 'scikit-learn',  'pandas'],
+      zip_safe=False
+)
