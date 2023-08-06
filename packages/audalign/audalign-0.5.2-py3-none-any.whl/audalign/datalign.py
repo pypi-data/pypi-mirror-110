113ddd4fb31b8b73253848e69a545a5c5d834c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:21 +0000

    Add package audalign/0.5.2/audalign-0.5.2-py3-none-any.whl

---
+++ packages/audalign/audalign-0.5.2-py3-none-any.whl/audalign/datalign.py
@@ -0,0 +1,28 @@
+import audalign
+
+"""Alignment isn't quite accurate enough for this yet"""
+
+# def _create_audainstance()
+
+
+def speed_of_sound(degrees: int) -> int:
+    return 331.3 + (degrees * 0.606)
+
+
+def event_a_is_closer(offset_a: int, offset_b: int) -> bool:
+    return offset_a > offset_b
+
+
+def distance_from_event():
+    ...
+
+
+def angle_two_events():
+    ...
+
+
+def which_is_first():
+    ...
+
+
+# def distance_to_event_given
