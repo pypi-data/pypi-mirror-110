1192ea3ae3e5904f7329cba94009b69498793cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:33 +0000

    Add package firesql/0.1.1/firesql-0.1.1.tar.gz

---
+++ packages/firesql/firesql-0.1.1.tar.gz/firesql-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name='firesql',
+    packages=['firesql'],  # this must be the same as the name above
+    version='0.1.1',
+    include_package_data=True,
+    description='Library to use databases sql or not-sql',
+    author='Andres Gonzalez',
+    author_email='andres@4coders.co',
+    license="GPLv3",
+    # use the URL to the github repo
+    url='https://github.com/4CodersColombia/firesql',
+    download_url='https://github.com/4CodersColombia/firesql/tarball/0.1',
+    keywords=['sql', 'no-sql'],
+    classifiers=['Programming Language :: Python :: 3.9',],
+    install_requires=[
+          'SQLAlchemy==1.4.18',
+      ],
+)
