7a30293d386e292d326089e19398d179402783b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:43 +0000

    Add package json-server/0.1.0/json_server-0.1.0.tar.gz

---
+++ packages/json-server/json_server-0.1.0.tar.gz/json_server-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='json_server',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'click',
+        'flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'json-server = json_server:cli_command',
+        ],
+    },
+    url="https://github.com/YuvrajGeek/json-server"
+)
