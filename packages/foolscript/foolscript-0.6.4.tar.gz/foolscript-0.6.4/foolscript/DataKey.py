01ffd8b0b6a20968b172e68538844d3d107aed2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package foolscript/0.6.4/foolscript-0.6.4.tar.gz

---
+++ packages/foolscript/foolscript-0.6.4.tar.gz/foolscript-0.6.4/foolscript/DataKey.py
@@ -0,0 +1,8 @@
+# Don't edit/delete the content in the file, 
+#It may corrupt Bird Script, 
+# And you may need to delete and reinstall Bird Script.
+# Content Keys ['not to be modified'].
+
+# Projects Path token
+project_dir = ''
+spec_class = '15oth0m4j'
