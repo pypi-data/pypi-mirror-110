8ce00b59a10c404143a428deb38d16eb6ebe7d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:58 +0000

    Add package soumayan4/0.0.1/soumayan4-0.0.1-py3-none-any.whl

---
+++ packages/soumayan4/soumayan4-0.0.1-py3-none-any.whl/soumayan4/__init__.py
@@ -0,0 +1,6 @@
+import os
+from soumayan4.fake import english_fake
+from soumayan4.fake import spanish_fake
+from soumayan4.fake import italian_fake
+from soumayan4.fake import french_fake
+from soumayan4.fake import german_fake
