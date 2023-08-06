1e37bd6aa00eea19cc173202b34c05d85f80543b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:17 +0000

    Add package pykale/0.1.0rc2/pykale-0.1.0rc2.tar.gz

---
+++ packages/pykale/pykale-0.1.0rc2.tar.gz/pykale-0.1.0rc2/kale/loaddata/dataset_access.py
@@ -0,0 +1,60 @@
+"""
+Dataset Access API adapted from https://github.com/criteo-research/pytorch-ada/blob/master/adalib/ada/datasets/dataset_access.py
+"""
+
+import torch
+import torch.utils.data
+
+
+class DatasetAccess:
+    """
+    This class ensures a unique API is used to access training, validation and test splits
+    of any dataset.
+
+    Args:
+        n_classes (int): the number of classes.
+    """
+
+    def __init__(self, n_classes):
+        self._n_classes = n_classes
+
+    def n_classes(self):
+        return self._n_classes
+
+    def get_train(self):
+        """
+        Returns: a torch.utils.data.Dataset
+            Dataset: a torch.utils.data.Dataset
+        """
+        raise NotImplementedError()
+
+    def get_train_val(self, val_ratio):
+        """
+        Randomly split a dataset into non-overlapping training and validation datasets.
+
+        Args:
+            val_ratio (float): the ratio for validation set
+
+        Returns:
+            Dataset: a torch.utils.data.Dataset
+        """
+        train_dataset = self.get_train()
+        ntotal = len(train_dataset)
+        ntrain = int((1 - val_ratio) * ntotal)
+
+        return torch.utils.data.random_split(train_dataset, [ntrain, ntotal - ntrain])
+
+    def get_test(self):
+        raise NotImplementedError()
+
+
+def get_class_subset(dataset, class_ids):
+    """
+    Args:
+        dataset: a torch.utils.data.Dataset
+        class_ids (list, optional): List of chosen subset of class ids.
+    Returns: a torch.utils.data.Dataset
+        Dataset: a torch.utils.data.Dataset with only classes in class_ids
+    """
+    sub_indices = [i for i in range(0, len(dataset)) if dataset[i][1] in class_ids]
+    return torch.utils.data.Subset(dataset, sub_indices)
