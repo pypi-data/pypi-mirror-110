2a52666f6e24f4a59929eae26a87906010abcf79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:22 +0000

    Add package django-elastic-filter/1.0.9/django-elastic-filter-1.0.9.tar.gz

---
+++ packages/django-elastic-filter/django-elastic-filter-1.0.9.tar.gz/django-elastic-filter-1.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(name='django-elastic-filter',
+      license="MIT",
+      author='Saeed Hassani Borzadaran',
+      author_email='hassanisaeed19@gmail.com',
+      url="https://github.com/realsaeedhassani/django-elastic-filter",
+      packages=['django-elastic-filter'])
