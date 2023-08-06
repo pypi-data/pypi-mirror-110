ba2f9034151b763e466114f5c39c6eacad0f4225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:57 +0000

    Add package wpdatautil/0.7.7/wpdatautil-0.7.7.tar.gz

---
+++ packages/wpdatautil/wpdatautil-0.7.7.tar.gz/wpdatautil-0.7.7/wpdatautil/s3path/__init__.py
@@ -0,0 +1,23 @@
+"""s3path utilities."""
+import logging
+import shutil
+from pathlib import Path
+from typing import Union
+
+from s3path import S3Path
+
+AnyPath = Union[Path, S3Path]
+
+log = logging.getLogger(__name__)
+
+
+def rmdirtree(path: AnyPath) -> None:
+    """Remove the local or S3 path."""
+    log.info(f"Removing path {path}.")
+    if isinstance(path, S3Path):
+        if path.exists():
+            path.rmdir()
+    else:
+        shutil.rmtree(path, ignore_errors=True)
+    assert list(path.rglob("*")) == []
+    log.info(f"Removed path {path}.")
