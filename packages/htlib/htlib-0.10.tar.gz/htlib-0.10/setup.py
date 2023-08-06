a4aa91a5c3ad6db0f3c4cbf7b1666b6dbeb92755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package htlib/0.10/htlib-0.10.tar.gz

---
+++ packages/htlib/htlib-0.10.tar.gz/htlib-0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='htlib',
+   version='0.10',
+   description='',
+   long_description=open("README.md").read(),
+   download_url="https://github.com/harol1997/htlib/archive/refs/heads/main.zip",
+   install_requires=[
+       "htlib",
+        "certifi==2020.12.5",
+        "chardet==4.0.0",
+        "idna==2.10",
+        "pyserial==3.5",
+        "requests==2.25.1",
+        "urllib3==1.26.2"
+   ],
+   author='Harol Alvardo',
+   author_email='harolav3@gmail.com',
+   long_description_content_type="text/markdown",
+   url="https://github.com/harol1997/htlib",
+   packages=find_packages(),
+   license="MIT",
+   include_package_data=True
+)
