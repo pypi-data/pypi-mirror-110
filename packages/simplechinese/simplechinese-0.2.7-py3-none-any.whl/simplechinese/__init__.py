71124d537c6cc8de842f340ee1355016d9a228cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:54 +0000

    Add package simplechinese/0.2.7/simplechinese-0.2.7-py3-none-any.whl

---
+++ packages/simplechinese/simplechinese-0.2.7-py3-none-any.whl/simplechinese/__init__.py
@@ -0,0 +1,19 @@
+from . import preprocessing
+from .preprocessing import *
+
+from . import representation
+from .representation import *
+
+from . import visualization
+from .visualization import *
+
+from . import nlp
+from .nlp import *
+
+from . import conversion
+from .conversion import *
+
+# from . import synonyms
+# from .synonyms import *
+
+__version__ = "0.2.0"
