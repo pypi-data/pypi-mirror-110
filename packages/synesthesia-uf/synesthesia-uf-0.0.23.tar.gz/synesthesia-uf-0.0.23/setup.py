3ac4fada718d7296e0e24276957934ccfe9088f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:25 +0000

    Add package synesthesia-uf/0.0.23/synesthesia-uf-0.0.23.tar.gz

---
+++ packages/synesthesia-uf/synesthesia-uf-0.0.23.tar.gz/synesthesia-uf-0.0.23/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.23'
+DESCRIPTION = 'A Python audio image creation tool'
+LONG_DESCRIPTION = 'A Python audio image creation tool that takes audio and creates images from them.'
+
+# Setting up
+setup(
+    name="synesthesia-uf",
+    version=VERSION,
+    author="Super Fun Adventure Club Dude Man Squad",
+    author_email="<georgekolasa@ufl.edu",
+    description=DESCRIPTION,
+    packages=['synesthesia'],
+    include_package_data = True,
+    package_data = {
+        # If any package contains *.txt files, include them:
+        '': ['*.svg'],
+        # And include any files found in the 'data' subdirectory
+        # of the 'rawdata' package, also:
+        'synesthesia': ['images/*.*'],
+    },
+    url="https://github.com/cbaddeley/Synesthesia",
+    license="GPL 3",
+    install_requires=['PyQt5', 'librosa', 'essentia',
+                      'pillow'],
+    keywords=['audio', 'visualizer', "image"],
+
+    entry_points =
+    {   "console_scripts":
+        [
+            "syne = synesthesia:main_func"
+        ]
+    }
+
+)
