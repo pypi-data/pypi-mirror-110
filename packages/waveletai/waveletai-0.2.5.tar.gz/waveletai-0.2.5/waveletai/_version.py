8a754c9adeaada8fd3ea7d1312ba8c6ca4b0f285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:02 +0000

    Add package waveletai/0.2.5/waveletai-0.2.5.tar.gz

---
+++ packages/waveletai/waveletai-0.2.5.tar.gz/waveletai-0.2.5/waveletai/_version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/4/6 6:10
+@Author  : WaveletAI-Product-Team Janus
+@license : (C) Copyright 2019-2022, Visionet(Tianjin)Information Technology Co.,Ltd.
+@Site    : plus.xiaobodata.com
+@File    : _version.py
+@Desc    : 
+"""
+
+
+def get_versions():
+    return {"version": "0.1.15", "full-revisionid": None, "date": "2021-04-21"}
