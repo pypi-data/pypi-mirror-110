80cbd8a892466f5c7f9f3e9b0a353d93656da9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:53 +0000

    Add package audiomentations/0.17.0/audiomentations-0.17.0.tar.gz

---
+++ packages/audiomentations/audiomentations-0.17.0.tar.gz/audiomentations-0.17.0/audiomentations/__init__.py
@@ -0,0 +1,26 @@
+from .augmentations.spectrogram_transforms import SpecFrequencyMask, SpecChannelShuffle
+from .augmentations.transforms import (
+    AddBackgroundNoise,
+    AddGaussianNoise,
+    AddGaussianSNR,
+    AddImpulseResponse,
+    ApplyImpulseResponse,
+    AddShortNoises,
+    Clip,
+    ClippingDistortion,
+    FrequencyMask,
+    Gain,
+    LoudnessNormalization,
+    Mp3Compression,
+    Normalize,
+    PitchShift,
+    PolarityInversion,
+    Resample,
+    Shift,
+    TimeMask,
+    TimeStretch,
+    Trim,
+)
+from .core.composition import Compose, SpecCompose
+
+__version__ = "0.17.0"
