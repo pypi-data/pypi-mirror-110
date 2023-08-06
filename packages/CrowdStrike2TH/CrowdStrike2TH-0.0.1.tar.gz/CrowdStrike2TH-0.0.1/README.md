65c12333293456b4b5271454d38ef08f41dee74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package CrowdStrike2TH/0.0.1/CrowdStrike2TH-0.0.1.tar.gz

---
+++ packages/CrowdStrike2TH/CrowdStrike2TH-0.0.1.tar.gz/CrowdStrike2TH-0.0.1/README.md
@@ -0,0 +1,2 @@
+# CrowdStrike2TH: Falcon CrowdStrike Alert Feeder for TheHive
+[Falcon CrowdStrike](https://www.crowdstrike.com/) provides endpoint protection, threat intelligence and attribution.
---
+++ packages/CrowdStrike2TH/CrowdStrike2TH-0.0.1.tar.gz/CrowdStrike2TH-0.0.1/src/config/config.py
@@ -0,0 +1,18 @@
+CrowdStrike = {
+    'url': '',
+    'token_endpoint': '',
+    'stream_endpoint': '',
+    'app_id': '',
+    'cid': '',
+    'secret': '',
+}
+
+CrowdStrike['url_token'] = CrowdStrike['url'] + CrowdStrike['token_endpoint']
+CrowdStrike['url_stream'] = CrowdStrike['url'] + CrowdStrike['stream_endpoint'] + f'?appId={CrowdStrike["app_id"]}'
+
+TheHive = {
+    'url': '',
+    'key': '',
+    'template': '',
+}
+
