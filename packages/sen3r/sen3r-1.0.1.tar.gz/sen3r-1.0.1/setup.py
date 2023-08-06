a803789d3ffe733e67bbd0cc72104e7504856661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:34 +0000

    Add package sen3r/1.0.1/sen3r-1.0.1.tar.gz

---
+++ packages/sen3r/sen3r-1.0.1.tar.gz/sen3r-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import sen3r
+import os
+
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+
+setup(name='sen3r',
+      version=sen3r.__version__,
+      description='SEN3R (Sentinel-3 Reflectance Retrieval over Rivers) enables extraction of reflectance time series from images over water bodies.',
+      long_description=open(README).read(),
+      long_description_content_type='text/markdown',
+      author="David Guimarães",
+      author_email="dvdgmf@gmail.com",
+      license="MIT",
+      platforms='any',
+      include_package_data=True,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+      ],
+      python_requires='>=3.6',
+      url='http://github.com/hybam-dev/sen3r/',)
