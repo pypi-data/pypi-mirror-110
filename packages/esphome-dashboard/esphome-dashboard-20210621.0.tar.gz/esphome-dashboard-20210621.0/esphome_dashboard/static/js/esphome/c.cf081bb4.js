38fad5e1180b4b3daf1ad212a13d4e2405d4a317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:41 +0000

    Add package esphome-dashboard/20210621.0/esphome-dashboard-20210621.0.tar.gz

---
+++ packages/esphome-dashboard/esphome-dashboard-20210621.0.tar.gz/esphome-dashboard-20210621.0/esphome_dashboard/static/js/esphome/c.cf081bb4.js
@@ -0,0 +1,9 @@
+import{_ as e,v as o,n as s,h as i,T as t}from"./c.3b87a103.js";import"./c.d1d27df5.js";import"./index-805914be.js";let a=class extends i{render(){return t`
+      <esphome-process-dialog
+        .heading=${`Clean MQTT discovery topics for ${this.configuration}`}
+        .type=${"clean-mqtt"}
+        .spawnParams=${{configuration:this.configuration}}
+        @closed=${this._handleClose}
+      >
+      </esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};e([o()],a.prototype,"configuration",void 0),a=e([s("esphome-clean-mqtt-dialog")],a);
