0f4a949bd3371e7ed08adaaff67d6cfeee696fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package accapi/0.0.3/accapi-0.0.3.tar.gz

---
+++ packages/accapi/accapi-0.0.3.tar.gz/accapi-0.0.3/README.md
@@ -0,0 +1,24 @@
+# Avigilon Control Center API for Python
+
+[![PyPI version](https://img.shields.io/pypi/v/accapi.svg)](https://pypi.org/project/accapi)
+
+## About
+
+This package alows you to communicate with Avigilon Control Center (ACC) API. Before you can start using it, you need to send and email to integrations@avigilon.com and ask for your unique pair of user nonce and user key values. Only having those will you be able to communicate with ACC server instance.
+
+## Features
+
+* Login and get session
+* Get camera list
+
+Currently limited functionality is available but it's easy to extend (contributions welcome!)
+
+## Usage
+
+```python
+from accapi.client import AccClientFactory
+
+factory = AccClientFactory("user_nonce", "user_key")
+client = factory.login("http://acc_address", "username", "password")
+cameras = client.get_cameras()
+```
---
+++ packages/accapi/accapi-0.0.3.tar.gz/accapi-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'Avigilon Control Center API for Python'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+        name="accapi", 
+        version=VERSION,
+        author="Jakub Bartkowiak",
+        author_email="gralinpl@gmail.com",
+        project_urls={
+            "Source": "https://github.com/gralin/acc-api-python",
+        },
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        packages=find_packages(),
+        install_requires=[],      
+        keywords=['python', 'acc', 'avigilon'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ]
+)
