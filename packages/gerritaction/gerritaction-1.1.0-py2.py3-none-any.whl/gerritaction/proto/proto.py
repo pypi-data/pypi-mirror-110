82cdf91b45e7c0a8c18ef4aaecd01c27423c3abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:59 +0000

    Add package gerritaction/1.1.0/gerritaction-1.1.0-py2.py3-none-any.whl

---
+++ packages/gerritaction/gerritaction-1.1.0-py2.py3-none-any.whl/gerritaction/proto/proto.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Proto:
+    ADD_ATTENTION = "add-attention"
+    ADD_REVIEWER = "add-reviewer"
+    DELETE_REVIEWER = "delete-reviewer"
+    REMOVE_ATTENTION = "remove-attention"
+
+
+class Separator:
+    ACCOUNT = ","
+    ACTION = ":"
+    GROUP = " "
