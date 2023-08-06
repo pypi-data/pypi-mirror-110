b99856aa7c16548c3e9119aa53768b32995b1f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:10 +0000

    Add package circuit-maintenance-parser/1.2.0/circuit-maintenance-parser-1.2.0.tar.gz

---
+++ packages/circuit-maintenance-parser/circuit-maintenance-parser-1.2.0.tar.gz/circuit-maintenance-parser-1.2.0/circuit_maintenance_parser/__init__.py
@@ -0,0 +1,98 @@
+"""Notifications parser init."""
+
+from typing import Type, Optional, Iterable
+
+from .errors import NonexistentParserError, ParsingError
+
+from .providers import (
+    GenericProvider,
+    Cogent,
+    EUNetworks,
+    Lumen,
+    Megaport,
+    NTT,
+    PacketFabric,
+    Telstra,
+    Zayo,
+)
+
+SUPPORTED_PROVIDERS = (
+    GenericProvider,
+    Cogent,
+    EUNetworks,
+    Lumen,
+    Megaport,
+    NTT,
+    PacketFabric,
+    Telstra,
+    Zayo,
+)
+
+SUPPORTED_PROVIDER_NAMES = [provider.get_provider_type() for provider in SUPPORTED_PROVIDERS]
+SUPPORTED_ORGANIZER_EMAILS = [provider.get_default_organizer() for provider in SUPPORTED_PROVIDERS]
+
+
+def init_parser(**kwargs) -> Optional[GenericProvider]:
+    """Returns an instance of the corresponding Notification Parser."""
+    try:
+        provider_type = kwargs.get("provider_type")
+        if not provider_type:
+            provider_type = GenericProvider.get_provider_type()
+        provider_parser_class = get_provider_class(provider_type)
+        return provider_parser_class(**kwargs)
+
+    except NonexistentParserError:
+        return None
+
+
+def get_provider_class(provider_name: str) -> Type[GenericProvider]:
+    """Returns the Provider parser class for a specific provider_type."""
+    provider_name = provider_name.lower()
+
+    for provider_parser in SUPPORTED_PROVIDERS:
+        if provider_parser.get_provider_type() == provider_name:
+            break
+    else:
+
+        raise NonexistentParserError(
+            f"{provider_name} is not a currently supported parser. Only {', '.join(SUPPORTED_PROVIDER_NAMES)}"
+        )
+
+    return provider_parser
+
+
+def get_provider_class_from_sender(email_sender: str) -> Type[GenericProvider]:
+    """Returns the notification parser class for an email sender address."""
+
+    for provider_parser in SUPPORTED_PROVIDERS:
+        if provider_parser.get_default_organizer() == email_sender:
+            break
+    else:
+        raise NonexistentParserError(
+            f"{email_sender} is not a currently supported provider parser. Only {', '.join(SUPPORTED_ORGANIZER_EMAILS)}"
+        )
+
+    return provider_parser
+
+
+def get_provider_data_types(provider_name: str) -> Iterable[str]:
+    """Returns the expected data types for each provider."""
+    provider_name = provider_name.lower()
+    for provider in SUPPORTED_PROVIDERS:
+        if provider.get_provider_type() == provider_name:
+            break
+    else:
+        raise NonexistentParserError(
+            f"{provider_name} is not a currently supported provider. Only {', '.join(SUPPORTED_PROVIDER_NAMES)}"
+        )
+
+    return provider.get_data_types()
+
+
+__all__ = [
+    "init_parser",
+    "get_provider_class",
+    "get_provider_class_from_sender",
+    "get_provider_data_types",
+    "ParsingError",
+]
