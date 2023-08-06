9d104577a0e059d7c58d112157eb0981e042a23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:29 +0000

    Add package qianyan-test5/0.0.2/qianyan_test5-0.0.2.tar.gz

---
+++ packages/qianyan-test5/qianyan_test5-0.0.2.tar.gz/qianyan_test5-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="qianyan_test5",
+    version="0.0.2",
+    license="MIT Licence",
+
+    url="https://github.com/DecstionBack/test",
+    author="lsw",
+    author_email="lvshangwen1011@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
