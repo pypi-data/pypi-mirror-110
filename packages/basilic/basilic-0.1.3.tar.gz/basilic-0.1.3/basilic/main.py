1aaee95fccc5449375c469162ced726a769ff9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:06 +0000

    Add package basilic/0.1.3/basilic-0.1.3.tar.gz

---
+++ packages/basilic/basilic-0.1.3.tar.gz/basilic-0.1.3/basilic/main.py
@@ -0,0 +1,12 @@
+from japronto import Application
+
+class Basilic(Application):
+    
+    def __init__(self, name):
+        self.name = name
+        super(Basilic, self).__init__()
+
+    def route(self, route, methods = ['GET']):
+        def inner(func):
+            self.router.add_route(route, func, methods = methods)
+        return inner
