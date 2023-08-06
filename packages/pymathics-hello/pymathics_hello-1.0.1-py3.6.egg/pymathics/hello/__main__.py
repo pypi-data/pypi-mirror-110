aaeefa1ce79c56ef1199bdeffb8067d901a426bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package pymathics-hello/1.0.1/pymathics_hello-1.0.1-py3.6.egg

---
+++ packages/pymathics-hello/pymathics_hello-1.0.1-py3.6.egg/pymathics/hello/__main__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from mathics.builtin.base import Builtin, String
+
+class Hello(Builtin):
+    """
+    <dl>
+      <dt>'Hello'[$person$]
+      <dd>An example function in a Python-importable Mathics module.
+    </dl>
+    >> Hello["World"]
+     = Hello, World!
+    """
+
+    # The function below should start with "apply"
+    def apply_with_name(self, person, evaluation):
+        "%(name)s[person_String]"
+        # %(name)s is just a more flexible way of writing "Hello".
+        # If the class name changes, so will the above pattern.
+        # The above pattern matches Hello with a single string argument.
+        # See https://reference.wolfram.com/language/tutorial/Patterns.html#7301
+        # and https://reference.wolfram.com/language/ref/Cases.html
+        return String("Hello, %s!" % person.get_string_value())
