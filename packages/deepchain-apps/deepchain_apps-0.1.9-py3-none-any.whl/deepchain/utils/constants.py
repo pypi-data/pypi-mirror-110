4c8371e61deef727e9d0b70bb13547c5082acecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:37 +0000

    Add package deepchain-apps/0.1.9/deepchain_apps-0.1.9-py3-none-any.whl

---
+++ packages/deepchain-apps/deepchain_apps-0.1.9-py3-none-any.whl/deepchain/utils/constants.py
@@ -0,0 +1,13 @@
+EXTENSIONS = [
+    ".h5",
+    ".hdf5",
+    ".yaml",
+    ".yml",
+    ".pt",
+    ".pb",
+    ".pkl",
+    ".ckpt",
+    ".pickle",
+]
+
+TAGS_KEY = ["tasks", "libraries", "embeddings", "datasets", "device"]
