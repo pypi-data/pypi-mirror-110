9753ee3abe905c0bdc656e725e667d5f9b3ad53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:27 +0000

    Add package dsFramework/0.1.23/dsFramework-0.1.23.tar.gz

---
+++ packages/dsFramework/dsFramework-0.1.23.tar.gz/dsFramework-0.1.23/cli/test_pipeline_template.py
@@ -0,0 +1,6 @@
+from generatedDirectory.pipeline import generatedProjectNamePipeline
+
+if __name__ == '__main__':
+    p = generatedProjectNamePipeline()
+    output = p.execute()
+    print(output)
