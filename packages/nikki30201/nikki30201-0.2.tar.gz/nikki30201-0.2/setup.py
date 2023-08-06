f5ce7663abd3f5488fcb80ce7a5507950954627d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:44 +0000

    Add package nikki30201/0.2/nikki30201-0.2.tar.gz

---
+++ packages/nikki30201/nikki30201-0.2.tar.gz/nikki30201-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='nikki30201',
+      version='0.2',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      description='Taiwan No. 1',
+      url='http://github.com/storborg/nikki30201',
+      author='Michael Chen',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['nikki30201'],
+      zip_safe=False)
