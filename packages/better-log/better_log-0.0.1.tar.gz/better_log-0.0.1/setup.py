4410ad408f2e6519670f0c862959655bf789db32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:58 +0000

    Add package better-log/0.0.1/better_log-0.0.1.tar.gz

---
+++ packages/better-log/better_log-0.0.1.tar.gz/better_log-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='better_log',
+    version='0.0.1',
+    description='a better log',
+    url='https://github.com/iammyz/better_log',
+    author='mengyunzhuo',
+    author_email='282053798@qq.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    keywords='log python',
+    packages=['better_log'],
+)
