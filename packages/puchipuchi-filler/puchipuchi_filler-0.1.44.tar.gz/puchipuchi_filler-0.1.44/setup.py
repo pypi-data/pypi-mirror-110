17862fe16619b2d39075b13220a3a10fbaf45afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:08 +0000

    Add package puchipuchi-filler/0.1.44/puchipuchi_filler-0.1.44.tar.gz

---
+++ packages/puchipuchi-filler/puchipuchi_filler-0.1.44.tar.gz/puchipuchi_filler-0.1.44/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['puchipuchi_filler']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['puchipuchi_filler = puchipuchi_filler.main:main']}
+
+setup_kwargs = {
+    'name': 'puchipuchi-filler',
+    'version': '0.1.44',
+    'description': 'no description provided',
+    'long_description': '# puchipuchi-filler\nwe do nothing\n何もしないです\n\n',
+    'author': 'puchipuchi team',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
