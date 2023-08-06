df8257f47a0440dcd2302172cd66facbd37700f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:50 +0000

    Add package pycg/0.0.4/pycg-0.0.4-py3-none-any.whl

---
+++ packages/pycg/pycg-0.0.4-py3-none-any.whl/micro-benchmark-key-errs/assignments_test.py
@@ -0,0 +1,16 @@
+
+import os
+
+from base import TestBase
+
+class AssignmentsTest(TestBase):
+    snippet_dir = "assignments"
+
+    def test_chained(self):
+        self.validate_snippet(self.get_snippet_path("chained"))
+
+    def test_recursive_tuple(self):
+        self.validate_snippet(self.get_snippet_path("recursive_tuple"))
+
+    def test_starred(self):
+        self.validate_snippet(self.get_snippet_path("starred"))
