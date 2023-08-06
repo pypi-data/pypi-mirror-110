50807ccf96ca50200101a16e62b50228ae6b9a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:23 +0000

    Add package yoojampdf/1.1/yoojampdf-1.1.tar.gz

---
+++ packages/yoojampdf/yoojampdf-1.1.tar.gz/yoojampdf-1.1/yoojampdf/pdf2text.py
@@ -0,0 +1,16 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+def convert(path):
+    """ 
+    Convert the given PDF to text. 
+    
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
+
+
