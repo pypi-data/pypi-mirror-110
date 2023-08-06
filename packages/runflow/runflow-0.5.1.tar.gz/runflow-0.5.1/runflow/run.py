b07ff5eb811335c9672c11d4ecb545a1a4ad9e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:48 +0000

    Add package runflow/0.5.1/runflow-0.5.1.tar.gz

---
+++ packages/runflow/runflow-0.5.1.tar.gz/runflow-0.5.1/runflow/run.py
@@ -0,0 +1,27 @@
+"""Run a flow spec."""
+
+from .core import Flow
+from .utils import import_module, run_async
+
+
+def load_flow(path=None, source=None, module=None, flow=None):
+    """Load a flow object."""
+    if path:
+        _flow = Flow.from_specfile(path)
+    elif source:
+        _flow = Flow.from_spec(source)
+    elif module:
+        _flow = import_module(module)
+    elif flow:
+        _flow = flow
+    else:
+        raise ValueError("Must provide one of path, source, module, flow")
+    return _flow
+
+
+def runflow(path=None, source=None, module=None, flow=None, vars=None):
+    """Run a flow object."""
+    _flow = load_flow(path=path, source=source, module=module, flow=flow)
+    assert _flow and isinstance(_flow, Flow)
+    coro = _flow.run(vars or {})
+    run_async(coro)
