1f24d0c8f2497f37945041d9f52273680a5b72e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:11 +0000

    Add package hyperdata/8.3.6/hyperdata-8.3.6.tar.gz

---
+++ packages/hyperdata/hyperdata-8.3.6.tar.gz/hyperdata-8.3.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperdata",
+    version="8.3.6",
+    description="hyperdata",
+    author="hyperdata",
+    author_email="mansu_kim@tmax.co.kr",
+    url="",
+    download_url="",
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=["hyperdata"],
+    python_requires=">=3.7",
+    package_data={},
+    zip_safe=False,
+    classifiers=["Programming Language :: Python :: 3.7"],
+)
