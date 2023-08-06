37bf2629b403ed9bcae14aa8008e643e5a16a813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:03 +0000

    Add package cloudage/0.1.0/cloudage-0.1.0-py3-none-any.whl

---
+++ packages/cloudage/cloudage-0.1.0-py3-none-any.whl/cloudage/cloudage.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+
+def find_refs(resource_name, resource_object, context=None):
+    if not resource_object or type(resource_object) in [
+        str,
+        int,
+        bool,
+        type(""),
+    ]:
+        return
+    elif type(resource_object) == list:
+        for sub_object in resource_object:
+            find_refs(
+                resource_name=resource_name,
+                resource_object=sub_object,
+                context=context,
+            )
+    else:
+        if context == "Tags" and "Key" in resource_object:
+            context = "Tags:" + resource_object["Key"]
+
+        for key, value in resource_object.items():
+            if key == "Ref":
+                print(
+                    '{resource} -> {target} [label="{context}"];'.format(
+                        resource=resource_name, target=value, context=context
+                    )
+                )
+            else:
+                if key not in ["Fn::Join", "Value"]:
+                    if context:
+                        new_context = context + ":" + key
+                    else:
+                        new_context = key
+                else:
+                    new_context = context
+                find_refs(
+                    resource_name=resource_name,
+                    resource_object=value,
+                    context=new_context,
+                )
+
+
+def main():
+    template = json.load(sys.stdin)
+
+    print("digraph g {")
+    print("rankdir=LR;")
+
+    for param in template["Parameters"]:
+        print('{param} [label="{param}",shape=oval];'.format(param=param))
+
+    print("\n")
+
+    for resource, details in template["Resources"].items():
+        print(
+            r'"{resource}" [label="{resource}\n{type}",shape=box];'.format(
+                resource=resource, type=details["Type"]
+            )
+        )
+
+    for resource, details in template["Resources"].items():
+        find_refs(
+            resource_name=resource, resource_object=details, context=None
+        )
+
+    print("}")
+
+
+if __name__ == "__main__":
+    main()
