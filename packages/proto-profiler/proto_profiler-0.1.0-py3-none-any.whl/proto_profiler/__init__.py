37d1b3eecd2dd3784eb44113d0c974c1eff41521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:16 +0000

    Add package proto-profiler/0.1.0/proto_profiler-0.1.0-py3-none-any.whl

---
+++ packages/proto-profiler/proto_profiler-0.1.0-py3-none-any.whl/proto_profiler/__init__.py
@@ -0,0 +1,14 @@
+from profiler import (  # noqa
+    ProfilerError,
+    init_trace,
+    init_msg_trace,
+    touch_trace,
+    touch_msg,
+    pass_trace,
+    copy_trace,
+    merge_traces,
+    time_mapping,
+    Timer,
+    ProtoTimer,
+    StackTimer,
+)
