40633099a4f2f83332f46b0f07ccfe40796b5838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package pyhf/0.6.2/pyhf-0.6.2-py3-none-any.whl

---
+++ packages/pyhf/pyhf-0.6.2-py3-none-any.whl/pyhf/parameters/__init__.py
@@ -0,0 +1,21 @@
+from .paramsets import (
+    paramset,
+    unconstrained,
+    constrained_by_normal,
+    constrained_by_poisson,
+)
+from .utils import reduce_paramsets_requirements
+from .paramview import ParamViewer
+
+__all__ = [
+    'paramset',
+    'unconstrained',
+    'constrained_by_normal',
+    'constrained_by_poisson',
+    'reduce_paramsets_requirements',
+    'ParamViewer',
+]
+
+
+def __dir__():
+    return __all__
