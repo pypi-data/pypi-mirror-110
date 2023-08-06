eef1de96787f667633916f8f9647c1d8fe50a5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package redfish-advantech/0.2.7/redfish_advantech-0.2.7.tar.gz

---
+++ packages/redfish-advantech/redfish_advantech-0.2.7.tar.gz/redfish_advantech-0.2.7/src/redfish_advantech/__init__.py
@@ -0,0 +1,22 @@
+# Copyright Notice:
+# Copyright 2016-2021 DMTF. All rights reserved.
+# License: BSD 3-Clause License. For full text see link:
+# https://github.com/DMTF/python-redfish-library/blob/master/LICENSE.md
+
+""" Advantech Redfish restful library """
+
+__all__ = ['restful', 'discovery']
+__version__ = "0.2.7"
+
+from redfish_advantech.restful.v1api import redfish_advantech
+from redfish_advantech.discovery.discovery import discover_ssdp
+import logging
+
+def redfish_logger(file_name, log_format, log_level=logging.ERROR):
+    formatter = logging.Formatter(log_format)
+    fh = logging.FileHandler(file_name)
+    fh.setFormatter(formatter)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(fh)
+    logger.setLevel(log_level)
+    return logger
