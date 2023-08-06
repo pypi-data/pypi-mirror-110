3dc2eb361992a048ce25c1ab4c042db4a09f4d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package healthinsurance/0.1/healthinsurance-0.1.tar.gz

---
+++ packages/healthinsurance/healthinsurance-0.1.tar.gz/healthinsurance-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='healthinsurance',
+version='0.1',
+description='Project Package',
+url='https://www.linkedin.com/in/brunovn/',
+author='bruno vinicius nonato',
+author_email='brunovinicius154@gmail.com',
+license='MIT',
+packages=['healthinsurance'],
+zip_safe=False)
