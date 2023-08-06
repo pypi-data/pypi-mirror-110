283f42d20ccea58efa285ce087960355459f8bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:58 +0000

    Add package helloworld-phil-davies/0.0.1/helloworld-phil_davies-0.0.1.tar.gz

---
+++ packages/helloworld-phil-davies/helloworld-phil_davies-0.0.1.tar.gz/helloworld-phil_davies-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='helloworld-phil_davies',
+    version='0.0.1',
+    author='Phil Davies',
+    author_email='phildavies265@gmail.com',
+    description='Say Hello', 
+    py_modules=["helloworld"],
+    package_dir={'': 'src'},
+)
