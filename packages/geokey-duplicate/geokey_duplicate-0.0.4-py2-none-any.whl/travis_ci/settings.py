9578d4fa0c30f0bfd9c5c0c602e237cfee533af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:38 +0000

    Add package geokey-duplicate/0.0.4/geokey_duplicate-0.0.4-py2-none-any.whl

---
+++ packages/geokey-duplicate/geokey_duplicate-0.0.4-py2-none-any.whl/travis_ci/settings.py
@@ -0,0 +1,33 @@
+"""GeoKey settings."""
+
+from geokey.core.settings.dev import *
+
+
+DEFAULT_FROM_EMAIL = 'no-reply@travis-ci.org'
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geokey',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+ALLOWED_HOSTS = ['localhost']
+
+INSTALLED_APPS += (
+    'geokey_duplicate',
+)
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = normpath(join(dirname(dirname(abspath(__file__))), 'assets'))
+MEDIA_URL = '/assets/'
+
+WSGI_APPLICATION = 'wsgi.application'
