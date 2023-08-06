512f65b9e269ee3564b6bfb0090df885ed2225ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:07 +0000

    Add package pytorch-gan-metrics/0.1.0/pytorch_gan_metrics-0.1.0-py3.6.egg

---
+++ packages/pytorch-gan-metrics/pytorch_gan_metrics-0.1.0-py3.6.egg/pytorch_gan_metrics/calc_fid_stats.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from .core import get_inception_feature
+from .utils import ImageDataset
+
+
+DIM = 2048
+device = torch.device('cuda:0')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Pre-calculate statistics of images")
+    parser.add_argument("--path", type=str, required=True,
+                        help='path to image directory')
+    parser.add_argument("--output", type=str, required=True,
+                        help="output path")
+    parser.add_argument("--batch_size", type=int, default=50,
+                        help="batch size (default=50)")
+    parser.add_argument("--inception_dir", type=str, default='/tmp',
+                        help='path to inception model dir')
+    args = parser.parse_args()
+
+    dataset = ImageDataset(args.path, exts=['png', 'jpg'])
+    loader = DataLoader(dataset, batch_size=50)
+    acts, = get_inception_feature(loader, dims=[2048], verbose=True)
+
+    mu = np.mean(acts, axis=0)
+    sigma = np.cov(acts, rowvar=False)
+
+    if os.path.dirname(args.output) != "":
+        os.makedirs(os.path.dirname(args.output), exist_ok=True)
+    np.savez_compressed(args.output, mu=mu, sigma=sigma)
