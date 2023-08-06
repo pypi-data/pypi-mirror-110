87cc199d232e15329c3032b33f9cb9733cd7ddcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:41 +0000

    Add package sensai/0.1.3/sensai-0.1.3.tar.gz

---
+++ packages/sensai/sensai-0.1.3.tar.gz/sensai-0.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_namespace_packages
+
+tf_requirements = ['tensorflow==1.15.0']
+torch_requirements = ['torch==1.4.0', 'torchtext==0.5.0']
+lightgbm_requirements = ['lightgbm==2.3.0']
+geoanalytics_requirements = ['networkx==2.4', 'Shapely~=1.7.0', 'geopandas==0.7.0']
+
+setup(
+    name='sensai',
+    package_dir={"": "src"},
+    license="MIT",
+    url="https://github.com/jambit/sensAI",
+    packages=find_namespace_packages(where="src"),
+    include_package_data=True,
+    version='0.1.3',
+    description='Library for sensible AI',
+    install_requires=[
+        line
+        for line in open("requirements.txt").readlines()
+        if not line.startswith("--")
+    ],
+    dependency_links=["https://download.pytorch.org/whl/torch_stable.html"],
+    setup_requires=["wheel"],
+    extras_require={
+        "torch": torch_requirements,
+        "tensorflow": tf_requirements,
+        "lightgbm": lightgbm_requirements,
+        "geoanalytics": geoanalytics_requirements,
+        "full": tf_requirements + torch_requirements + lightgbm_requirements + geoanalytics_requirements
+    },
+    author='jambit GmbH'
+)
