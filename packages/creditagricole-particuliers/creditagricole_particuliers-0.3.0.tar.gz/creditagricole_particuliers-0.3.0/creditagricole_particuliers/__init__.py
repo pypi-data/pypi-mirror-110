1c114c460fe5109c4ca038424c138518914bae77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:16 +0000

    Add package creditagricole-particuliers/0.3.0/creditagricole_particuliers-0.3.0.tar.gz

---
+++ packages/creditagricole-particuliers/creditagricole_particuliers-0.3.0.tar.gz/creditagricole_particuliers-0.3.0/creditagricole_particuliers/__init__.py
@@ -0,0 +1,4 @@
+from creditagricole_particuliers.accounts import Accounts
+from creditagricole_particuliers.operations import Operations
+from creditagricole_particuliers.authenticator import Authenticator
+from creditagricole_particuliers.logout import Logout
