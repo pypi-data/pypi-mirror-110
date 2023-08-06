6990567307950ecbce6a94391c99879cd6678ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:05 +0000

    Add package ia32-python/1.0.2/ia32_python-1.0.2.tar.gz

---
+++ packages/ia32-python/ia32_python-1.0.2.tar.gz/ia32_python-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ia32_python",
+    version="1.0.2",
+    description="x86-64 definitions for Python 2/3",
+    author="guye1296",
+    packages=find_packages(),
+    url="https://github.com/guye1296/ia32-python",
+    install_requires=["future"],
+)
