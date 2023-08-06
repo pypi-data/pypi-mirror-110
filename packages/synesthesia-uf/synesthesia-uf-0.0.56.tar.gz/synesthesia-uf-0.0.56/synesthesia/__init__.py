68074bd373fdb8a1c76bb2dfb78d50aaa84bbd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:27 +0000

    Add package synesthesia-uf/0.0.56/synesthesia-uf-0.0.56.tar.gz

---
+++ packages/synesthesia-uf/synesthesia-uf-0.0.56.tar.gz/synesthesia-uf-0.0.56/synesthesia/__init__.py
@@ -0,0 +1,12 @@
+import glob, os, shutil, os, sys
+sys.path.append("synesthesia")
+
+
+from synesthesia.gui import *
+from synesthesia import *
+from synesthesia.essentia_helper import *
+from synesthesia.image_helper import *
+from synesthesia.lib import *
+from synesthesia.wsl import *
+from synesthesia.images import *
+
