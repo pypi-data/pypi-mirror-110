2524335fd0e16c1d17b85b7fd48aa6c996776b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:38 +0000

    Add package APISpec-fromfile/1.0.3/APISpec-fromfile-1.0.3.tar.gz

---
+++ packages/APISpec-fromfile/APISpec-fromfile-1.0.3.tar.gz/APISpec-fromfile-1.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="APISpec-fromfile",
+    version="1.0.3",
+    author="OVHcloud",
+    author_email="opensource@ovhcloud.com",
+    description="APISpec plugin to import OpenAPI specifications from a file instead of putting YAML into docstrings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ovh/python-apispec-fromfile",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Environment :: Plugins",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Documentation",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="openapi apispec",
+    install_requires=[
+        "apispec[yaml]>=4.0.0",
+    ],
+    python_requires=">=3.6",
+    tests_require=["pytest"],
+    test_suite="tests",
+)
