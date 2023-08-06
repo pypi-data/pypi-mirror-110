1b2e25ed14e27c6a9e76be6009c7a6cca4c10b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:59 +0000

    Add package laputan/1.1.1/laputan-1.1.1.tar.gz

---
+++ packages/laputan/laputan-1.1.1.tar.gz/laputan-1.1.1/laputan/utilities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+Utilities
+
+    Error
+    InputError(Error)
+
+"""
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
