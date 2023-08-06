c188268e46770a6ff8874d087e253b42ec6b0e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:24 +0000

    Add package KakaoV2/1.0.9/KakaoV2-1.0.9.tar.gz

---
+++ packages/KakaoV2/KakaoV2-1.0.9.tar.gz/KakaoV2-1.0.9/KakaoV2/channel.py
@@ -0,0 +1,53 @@
+import time
+from .utils import Packet
+import bson
+
+
+class Channel:
+    def __init__(self, chatId, li, writer):
+        self.li = li
+        self.writer = writer
+        self.id = chatId
+
+    async def __sendPacket(self, command, data):
+        packet = Packet(0, 0, command, 0, bson.encode(data))
+        return (await self.writer.sendPacket(packet)).toJsonBody()
+
+    async def sendChat(self, msg, extra, t):
+        data = {
+            "chatId": self.id,
+            "extra": extra,
+            "type": t,
+            "msgId": time.time(),
+            "msg": str(msg),
+            "noSeen": False,
+        }
+        return await self.__sendPacket("WRITE", data)
+
+    async def send(self, msg):
+        return await self.sendChat(msg, "{}", 1)
+
+
+    async def kick(self, member_id):
+        if self.li:
+            data = {
+                "li": self.li,
+                "c": self.id,
+                "mid": member_id,
+            }
+            await self.__sendPacket("KICKMEM", data)
+    async def hide(self, id):
+        if self.li:
+            data = {
+                "logId": id,
+                "chatId": self.id,
+            }
+            await self.__sendPacket("DELETEMSG", data)
+    # async def get_linkinfo(self):
+    #     return await self.__sendPacket("INFOLINK", {"lis": [self.li]})
+
+    async def get_info(self):
+        return await self.__sendPacket("CHATINFO", {"chatId": self.id})
+
+    async def get_user(self, userId):
+        return await self.__sendPacket( "MEMBER", {"chatId": self.id, "memberIds": [userId]} )
---
+++ packages/KakaoV2/KakaoV2-1.0.9.tar.gz/KakaoV2-1.0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KakaoV2",
+    version="1.0.9",
+    url="https://github.com/ULTRA0221/KakaoV2",
+    author="ULTRA#0221",
+    author_email="kakao@ultra0221.me",
+    description="Kakaotalk LOCO/HTTP API protocol wrapper for python.",
+    packages=["KakaoV2"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=["pymongo", "requests", "pycryptodomex", "aiohttp"],
+    zip_safe=False,
+    classifiers=["License :: OSI Approved :: MIT License"]
+)
