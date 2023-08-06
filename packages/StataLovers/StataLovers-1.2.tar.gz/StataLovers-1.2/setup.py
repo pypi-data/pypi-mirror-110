61d4a38ff55b34f1a0b3fadceb653a6317b0d2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:02 +0000

    Add package StataLovers/1.2/StataLovers-1.2.tar.gz

---
+++ packages/StataLovers/StataLovers-1.2.tar.gz/StataLovers-1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# read the contents of your README file
+import os
+from os import path
+this_directory = os.getcwd()
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='StataLovers',
+      version ='1.2',
+      description = 'Stata-like functions tab and summarize',
+      packages =['StataLovers'],
+      author='Mila Kolpashnikova',
+      author_email='kamilakolpashnikova@gmail.com',
+	  long_description=long_description,
+	  long_description_content_type='text/markdown',
+      zip_safe=False)
