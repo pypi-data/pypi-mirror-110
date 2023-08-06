a23b9d0fa0a65df17685ac6d6e515e33db6b981f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:16 +0000

    Add package speedbotlib/0.0.1/speedbotlib-0.0.1.tar.gz

---
+++ packages/speedbotlib/speedbotlib-0.0.1.tar.gz/speedbotlib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="speedbotlib", version="0.0.1", description="", 
+    author="sl.truman", author_email='sl.truman@live.com',
+    url='https://git.speedbot.net/speedbot-lib/speedbot-lib', # 项目地址,一般是代码托管的网站
+    license='MIT',
+    packages=setuptools.find_packages()
+)
