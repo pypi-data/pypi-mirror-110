ab1ee93517645cdc9ca30adf914e10e9326ddd7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:12 +0000

    Add package tchotchke/0.1.4/tchotchke-0.1.4.tar.gz

---
+++ packages/tchotchke/tchotchke-0.1.4.tar.gz/tchotchke-0.1.4/src/tchotchke/logger.py
@@ -0,0 +1,60 @@
+import sys
+import traceback
+from contextlib import contextmanager
+
+import loguru
+import ujson
+
+
+class Logger:
+    def __init__(self, log_sink=sys.stdout):
+        self.__internal_logger = loguru.logger
+        self.__internal_logger.remove()
+        self.__internal_logger.add(log_sink, backtrace=True, diagnose=True, colorize=True)
+
+    @contextmanager
+    def log_uncaught_exceptions(self):
+        try:
+            yield
+        except Exception as error:
+            self.exception("unhandled exception occurred")
+            raise
+
+    def debug(self, message, data=None):
+        output_log = self.__format_output_log(message, data)
+        serialized_output = ujson.dumps(output_log)
+        self.__internal_logger.debug(serialized_output)
+
+    def info(self, message, data=None):
+        output_log = self.__format_output_log(message, data)
+        serialized_output = ujson.dumps(output_log)
+        self.__internal_logger.info(serialized_output)
+
+    def warning(self, message, data=None):
+        output_log = self.__format_output_log(message, data)
+        serialized_output = ujson.dumps(output_log)
+        self.__internal_logger.warning(serialized_output)
+
+    def error(self, message, data=None):
+        output_log = self.__format_output_log(message, data)
+        serialized_output = ujson.dumps(output_log)
+        self.__internal_logger.error(serialized_output)
+
+    def exception(self, message, data=None):
+        output_log = self.__format_output_log(message, data)
+        output_log["exception"] = traceback.format_exc()
+        serialized_output = ujson.dumps(output_log)
+        self.__internal_logger.error(serialized_output)
+
+    @staticmethod
+    def __format_output_log(message, data):
+        if not isinstance(message, str):
+            raise TypeError()
+        if not isinstance(data, dict) and data is not None:
+            raise TypeError()
+        output_log = {
+            "message": message,
+        }
+        if data is not None:
+            output_log.update(data)
+        return output_log
