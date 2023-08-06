e367ed4cf169a8bca77a2663186e440f360f072f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:02 +0000

    Add package enchaintesdk/0.1.3/enchaintesdk-0.1.3.tar.gz

---
+++ packages/enchaintesdk/enchaintesdk-0.1.3.tar.gz/enchaintesdk-0.1.3/tests/config/test_config_service.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest import TestCase, mock
+from enchaintesdk.config.entity.configuration_entity import Configuration
+from enchaintesdk.config.entity.config_env_entity import ConfigEnv
+from enchaintesdk.config.service.config_service import ConfigService
+
+
+class ConfigServiceTestCase(TestCase):
+
+    @mock.patch('enchaintesdk.config.repository.config_repository.ConfigRepository')
+    def test_setup_environment(self, MockConfigRepo):
+        config = ConfigService(MockConfigRepo)
+        config.setupEnvironment(ConfigEnv.TEST)
+        MockConfigRepo.fetchConfiguration.assert_called_once()
+
+    @mock.patch('enchaintesdk.config.repository.config_repository.ConfigRepository')
+    def test_get_configuration(self, MockConfigRepo):
+        config = ConfigService(MockConfigRepo)
+        config.getConfiguration()
+        MockConfigRepo.getConfiguration.assert_called_once()
+
+    @mock.patch('enchaintesdk.config.repository.config_repository.ConfigRepository')
+    def test_get_base_url(self, MockConfigRepo):
+        config = ConfigService(MockConfigRepo)
+        ret = Configuration()
+        ret.host = 'test'
+        MockConfigRepo.getConfiguration.return_value = ret
+        self.assertEqual(config.getApiBaseUrl(), 'test',
+                         'Expecting "testing", found something else.')
+        MockConfigRepo.getConfiguration.assert_called_once()
