dcc506d5c1af5b775d57368593ce57e92cdc958d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:07 +0000

    Add package utils-ag/0.0.4/utils_ag-0.0.4-py3-none-any.whl

---
+++ packages/utils-ag/utils_ag-0.0.4-py3-none-any.whl/utils_ag/first.py
@@ -0,0 +1,13 @@
+from colour_text import ColourText
+
+
+def f():
+    ct = ColourText()
+    ct.initTerminal()
+
+    print(ct.convert("The next section is in green: <>green example<>."))
+    print(ct.convert("<>red HERE! HERE! HERE! HERE! HERE! HERE! HERE! <>."))
+
+
+def fff2():
+    print(123)
