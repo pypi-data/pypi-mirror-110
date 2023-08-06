9b9805b1cba419c454a42ac1cbb764ac6496aede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:50 +0000

    Add package package-cb65/0.0.9/package_cb65-0.0.9.tar.gz

---
+++ packages/package-cb65/package_cb65-0.0.9.tar.gz/package_cb65-0.0.9/src/package_cb65/__init__.py
@@ -0,0 +1,5 @@
+"""
+__init__
+========
+"""
+from .impl import *
---
+++ packages/package-cb65/package_cb65-0.0.9.tar.gz/package_cb65-0.0.9/src/package_cb65/__main__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+from package_cb65 import build
+
+
+def main():
+    return build()
+
+
+# Execution
+
+if __name__ == "__main__":
+    print(main())
