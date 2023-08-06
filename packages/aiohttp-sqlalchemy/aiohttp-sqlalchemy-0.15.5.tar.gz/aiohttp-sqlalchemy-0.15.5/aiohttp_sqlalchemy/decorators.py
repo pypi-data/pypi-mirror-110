35f52a59420a24a2abcfe12fccc4b43f6acce660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:44 +0000

    Add package aiohttp-sqlalchemy/0.15.5/aiohttp-sqlalchemy-0.15.5.tar.gz

---
+++ packages/aiohttp-sqlalchemy/aiohttp-sqlalchemy-0.15.5.tar.gz/aiohttp-sqlalchemy-0.15.5/aiohttp_sqlalchemy/decorators.py
@@ -0,0 +1,31 @@
+from aiohttp.abc import AbstractView
+from functools import wraps
+from typing import TYPE_CHECKING
+
+from aiohttp_sqlalchemy.constants import SA_DEFAULT_KEY
+from aiohttp_sqlalchemy.exceptions import DuplicateRequestKeyError
+
+if TYPE_CHECKING:
+    from aiohttp.web import StreamResponse
+    from typing import Any
+    from aiohttp_sqlalchemy.typedefs import THandler, THandlerWrapper
+
+
+def sa_decorator(key: str = SA_DEFAULT_KEY) -> 'THandlerWrapper':
+    """ SQLAlchemy asynchronous handler decorator. """
+    def wrapper(handler: 'THandler') -> 'THandler':
+        @wraps(handler)
+        async def wrapped(*args: 'Any', **kwargs: 'Any') -> 'StreamResponse':
+            request = args[0].request \
+                      if isinstance(args[0], AbstractView) \
+                      else args[-1]
+
+            if key in request:
+                raise DuplicateRequestKeyError(key)
+
+            session_factory = request.config_dict.get(key)
+            async with session_factory() as request[key]:
+                return await handler(*args, **kwargs)
+
+        return wrapped
+    return wrapper
---
+++ packages/aiohttp-sqlalchemy/aiohttp-sqlalchemy-0.15.5.tar.gz/aiohttp-sqlalchemy-0.15.5/aiohttp_sqlalchemy/views.py
@@ -0,0 +1,51 @@
+from abc import ABCMeta
+from aiohttp.abc import AbstractView
+from aiohttp.web import View
+from typing import TYPE_CHECKING
+
+from aiohttp_sqlalchemy.constants import SA_DEFAULT_KEY
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+    from typing import Any, Optional
+
+
+class SAAbstractView(AbstractView, metaclass=ABCMeta):
+    """
+    Simple SQLAlchemy view based on aiohttp.abc.AbstractView.
+
+    The `__await__` method must be implemented in child classes.
+
+    Suitable for a specific usage with multiple models.
+    """
+    sa_session_key: 'str' = SA_DEFAULT_KEY
+
+    def sa_session(self, key: 'Optional[str]' = None) -> 'AsyncSession':
+        return self.request[key or self.sa_session_key]
+
+
+class SAOneModelMixin(SAAbstractView, metaclass=ABCMeta):
+    """
+    One model SQLAlchemy view based on aiohttp.abc.AbstractView.
+
+    The `__await__` method must be implemented in child classes.
+
+    Suitable for a usually usage with one model.
+    """
+    sa_model: 'Any'  # Not all developers use declarative mapping
+
+
+class SABaseView(View, SAAbstractView):
+    """
+    Simple SQLAlchemy view based on aiohttp.web.View.
+
+    Recomended for a specific usage with multiple models.
+    """
+
+
+class SAView(View, SAOneModelMixin):
+    """
+    One model SQLAlchemy view based on aiohttp.web.View.
+
+    Recomended for a usually usage with one model.
+    """
