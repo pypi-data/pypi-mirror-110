5e770fb6fcf34d7f89f1be306dae29c26f370c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:04 +0000

    Add package dist-gausbin/1.0/dist-gausbin-1.0.tar.gz

---
+++ packages/dist-gausbin/dist-gausbin-1.0.tar.gz/dist-gausbin-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="dist-gausbin",
+    version="1.0",
+    description="Gaussian and Binomial Distribution",
+    packages=["dist-gausbin"],
+    author="Abdul Munim Khan",
+    zip_safe=False,
+)
