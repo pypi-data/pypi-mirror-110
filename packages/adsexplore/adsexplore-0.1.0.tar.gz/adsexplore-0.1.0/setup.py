8850d7ab25e5894f5861d512b5c06e1238e0eead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:28 +0000

    Add package adsexplore/0.1.0/adsexplore-0.1.0.tar.gz

---
+++ packages/adsexplore/adsexplore-0.1.0.tar.gz/adsexplore-0.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='adsexplore',
+    version='0.1.0',
+    author='A. Camps-Fariña, C. Espinosa-Ponce, G. Magallanes-Guijón',
+    # author_email='',
+    packages=['adsexplorer'],
+    # url='http://pypi.python.org/pypi/',
+    description="Get stats on an author's papers and give useful information as well as formatting the output for use in CV or just to get some fun statistics from ADS",
+    install_requires=[
+        "ads",
+        "werkzeug == 1.0.1",
+        "metaphone"
+    ],
+)
