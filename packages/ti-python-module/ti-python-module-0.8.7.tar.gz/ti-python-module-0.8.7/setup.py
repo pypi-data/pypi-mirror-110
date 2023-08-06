4373a350383db7710a2218ca2de4e07f3219cf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:55 +0000

    Add package ti-python-module/0.8.7/ti-python-module-0.8.7.tar.gz

---
+++ packages/ti-python-module/ti-python-module-0.8.7.tar.gz/ti-python-module-0.8.7/setup.py
@@ -0,0 +1,27 @@
+from sys import version
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = "0.8.7"
+
+setup(
+    name="ti-python-module",
+    version=VERSION,
+    author="VampireTechnologyLord",
+    author_email="<elias.freund@ewe.net>",
+    description="TI-Python Debugging",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+#    install_requires=[],
+    keywords=["python", "ti", "texas instruments"],
+    # classifiers=[
+    #     "Developement Status :: 1 - Planning",
+    #     "Intended Audience :: Developers",
+    #     "Programming Language :: Python :: 3",
+    #     "Operation System :: Unix",
+    #     "Operation System :: MacOS, MacOS X",
+    #     "Operation System :: Microsoft :: Windows",
+    # ]
+)
