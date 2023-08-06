5a5d75bb3a6ca6565da00c88fb361a15cae179da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:29 +0000

    Add package gda-analysis/0.1.7/gda_analysis-0.1.7.tar.gz

---
+++ packages/gda-analysis/gda_analysis-0.1.7.tar.gz/gda_analysis-0.1.7/src/gda_analysis/compare_to_manifest/compare_to_manifest.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+PATH_OF_MERGED = '/Users/tovahallas/projects/gda_data/result_files/validated_merge_r01.xlsx'
+
+
+def extract_zeroes(manifest):
+   zero_manifest = manifest[manifest['chr'] == 0]
+
+   return zero_manifest
+
+
+def validate_manifest(manifest):
+   zeroes_df = extract_zeroes(manifest)
+   without_strand_df = manifest[(manifest['refstrand'] != '-') & (manifest['refstrand'] != '+')]
+
+   return without_strand_df
+
+
+def extract_nocalls_from_merged_data(validated_merge_r01):
+   validated_merge_r01_nocalls = validated_merge_r01[validated_merge_r01['gt'] == './.']
+
+   return validated_merge_r01_nocalls
+
+
+def extract_missing_strands(validated_merge_r01):
+   validated_merge_r01_without_strand = validated_merge_r01[validated_merge_r01['plus/minus strand'] == '?']
+
+   return validated_merge_r01_without_strand
+
+
+def cmp_missing_strands(validated_merge_r01_without_strand, manifest):
+   merged = validated_merge_r01_without_strand.merge(manifest, how='left', on='id')
+
+   return merged
+
+
+def find_missing_vcf_ids_in_merged(validated_merge_r01, manifest):
+   missing_vcf = validated_merge_r01[validated_merge_r01['chrom'].isnull()]
+   merged_missing_vcf = missing_vcf.merge(manifest, how='left', on='id')
+
+   return merged_missing_vcf
+
+
+def find_mismatches_in_manifest(validated_merge_r01, validated_merge_r01_without_strand, manifest):
+   merged_missing_strands = cmp_missing_strands(validated_merge_r01_without_strand, manifest)
+   merged_missing_vcf = find_missing_vcf_ids_in_merged(validated_merge_r01, manifest)
+
+
+def preprocess_manifest(manifest):
+   manifest.columns = [col.lower() for col in manifest.columns]
+   manifest.rename(columns={'name': 'id'}, inplace=True)
+
+
+def preprocess_vcf(vcf):
+   vcf.columns = [col.lower() for col in vcf.columns]
+   vcf.rename(columns={'ID': 'id'}, inplace=True)
+
+
+
+def read_data():
+   manifest = pd.read_excel('/Users/tovahallas/projects/GDA_project/manifest/20050209_GDA_manifest.xlsx')
+   vcf = pd.read_csv('/Users/tovahallas/projects/GDA_project/vcf_files/205364000001_R01C01.csv')
+   validated_merge_r01 = pd.read_excel(PATH_OF_MERGED)
+
+
+   # merged_vcf_gs = pd.read_excel('/Users/tovahallas/projects/gda_data/raw_data/merged_vcf_gs.xlsx')
+   #
+   # merged_on_vcf = validated_merge_r01_without_strand.merge(vcf, how='left', on='id')
+
+
+if __name__ == '__main__':
+   read_data()
+
+
+
+
---
+++ packages/gda-analysis/gda_analysis-0.1.7.tar.gz/gda_analysis-0.1.7/src/gda_analysis/display_dataset.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from src.gda_analysis.validation_tests import validation_tests as vtests
+import sys
+
+PATH_OF_MERGED_DATA = '/Users/tovahallas/projects/gda_data/raw_data/merged_vcf_gs.xlsx'
+PATH_OF_VALIDATED = '/Users/tovahallas/projects/test2/validated_merge_r01.xlsx'
+
+COLS_LIST = ['id', 'chrom', 'pos', 'ref', 'alt', 'gt', 'allele1 - top', 'allele2 - top', 'allele1 - forward',
+             'allele2 - forward', 'allele1 - ab', 'allele2 - ab', 'allele1 - plus', 'allele2 - plus',
+             'allele1 - design', 'allele2 - design', 'gc score', 'snp', 'igentify short name', 'plus/minus strand']
+
+
+def display_analyzed_data(path_of_merged_data):
+    merged_df = pd.read_excel(path_of_merged_data)
+    indels_df, merged_df = vtests.preprocess_df(merged_df, COLS_LIST)
+    merged_df = vtests.validate_wildtype(merged_df)
+    merged_df = vtests.validate_homo_to_alt(merged_df)
+    merged_df = vtests.validate_hetro(merged_df)
+    merged_df = vtests.validate_multi(merged_df)
+    merged_df = vtests.validate_missing_info(merged_df)
+    merged_df = vtests.validate_nocalls(merged_df)
+    merged_df = vtests.validate_exceptions(merged_df)
+
+    return merged_df
+
+
+def output_toexcel(merged_df, path_of_validated):
+    merged_df.to_excel(path_of_validated, encoding='utf-8')
+
+
+def main_validation():
+    merged_df = display_analyzed_data(sys.argv[1])
+    output_toexcel(merged_df, sys.argv[2])
+
+
+if __name__ == '__main__':
+    main_validation(sys.argv)
