b737d019630c2d65b7fb7f1460d5227a628467dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:38 +0000

    Add package html-index/1.0.0/html-index-1.0.0.tar.gz

---
+++ packages/html-index/html-index-1.0.0.tar.gz/html-index-1.0.0/src/html_index/util/assets.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+class Assets:
+    def __init__(self, path: Path):
+        self.path = path
+
+    def icon_folder(self):
+        return self.path.joinpath('__icons')
+
+    def icon_setting(self):
+        return self.path.joinpath('icons.yml')
+
+    def index_template(self):
+        return self.path.joinpath('__index_template.html')
+
+    def file_template(self):
+        return self.path.joinpath('__file_template.html')
