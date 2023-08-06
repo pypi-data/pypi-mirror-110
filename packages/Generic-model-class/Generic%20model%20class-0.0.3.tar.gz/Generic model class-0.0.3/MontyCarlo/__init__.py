91e7f76b9e6cff3144266065f8f517fda1482b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:21 +0000

    Add package Generic-model-class/0.0.3/Generic%20model%20class-0.0.3.tar.gz

---
+++ packages/Generic-model-class/Generic%20model%20class-0.0.3.tar.gz/Generic model class-0.0.3/MontyCarlo/__init__.py
@@ -0,0 +1,31 @@
+import sys
+print(sys.path)
+
+from ._init import eax
+
+from .plotter import *
+
+#from MontyCarlo.init import eax 
+
+
+
+
+#print("IMPORTING GEOMETRY")
+from MontyCarlo.geometry.main import *
+from .geometry.CSG import Sphere
+
+
+
+#print("IMPORTING MATERIAL")
+from .materials.materials import *
+
+
+from .sources import *
+
+
+from matplotlib.pyplot import *
+from numpy import *
+
+from .materials import database
+
+
