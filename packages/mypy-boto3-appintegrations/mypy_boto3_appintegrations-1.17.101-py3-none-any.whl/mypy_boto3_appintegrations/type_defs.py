4963ff0c1e7fc78b062940fdfb12c78be0b1d291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:51 +0000

    Add package mypy-boto3-appintegrations/1.17.101/mypy_boto3_appintegrations-1.17.101-py3-none-any.whl

---
+++ packages/mypy-boto3-appintegrations/mypy_boto3_appintegrations-1.17.101-py3-none-any.whl/mypy_boto3_appintegrations/type_defs.py
@@ -0,0 +1,112 @@
+"""
+Type annotations for appintegrations service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_appintegrations/type_defs.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_appintegrations.type_defs import CreateEventIntegrationResponseTypeDef
+
+    data: CreateEventIntegrationResponseTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "CreateEventIntegrationResponseTypeDef",
+    "EventFilterTypeDef",
+    "EventIntegrationAssociationTypeDef",
+    "EventIntegrationTypeDef",
+    "GetEventIntegrationResponseTypeDef",
+    "ListEventIntegrationAssociationsResponseTypeDef",
+    "ListEventIntegrationsResponseTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+)
+
+CreateEventIntegrationResponseTypeDef = TypedDict(
+    "CreateEventIntegrationResponseTypeDef",
+    {
+        "EventIntegrationArn": str,
+    },
+    total=False,
+)
+
+EventFilterTypeDef = TypedDict(
+    "EventFilterTypeDef",
+    {
+        "Source": str,
+    },
+)
+
+EventIntegrationAssociationTypeDef = TypedDict(
+    "EventIntegrationAssociationTypeDef",
+    {
+        "EventIntegrationAssociationArn": str,
+        "EventIntegrationAssociationId": str,
+        "EventIntegrationName": str,
+        "ClientId": str,
+        "EventBridgeRuleName": str,
+        "ClientAssociationMetadata": Dict[str, str],
+    },
+    total=False,
+)
+
+EventIntegrationTypeDef = TypedDict(
+    "EventIntegrationTypeDef",
+    {
+        "EventIntegrationArn": str,
+        "Name": str,
+        "Description": str,
+        "EventFilter": "EventFilterTypeDef",
+        "EventBridgeBus": str,
+        "Tags": Dict[str, str],
+    },
+    total=False,
+)
+
+GetEventIntegrationResponseTypeDef = TypedDict(
+    "GetEventIntegrationResponseTypeDef",
+    {
+        "Name": str,
+        "Description": str,
+        "EventIntegrationArn": str,
+        "EventBridgeBus": str,
+        "EventFilter": "EventFilterTypeDef",
+        "Tags": Dict[str, str],
+    },
+    total=False,
+)
+
+ListEventIntegrationAssociationsResponseTypeDef = TypedDict(
+    "ListEventIntegrationAssociationsResponseTypeDef",
+    {
+        "EventIntegrationAssociations": List["EventIntegrationAssociationTypeDef"],
+        "NextToken": str,
+    },
+    total=False,
+)
+
+ListEventIntegrationsResponseTypeDef = TypedDict(
+    "ListEventIntegrationsResponseTypeDef",
+    {
+        "EventIntegrations": List["EventIntegrationTypeDef"],
+        "NextToken": str,
+    },
+    total=False,
+)
+
+ListTagsForResourceResponseTypeDef = TypedDict(
+    "ListTagsForResourceResponseTypeDef",
+    {
+        "tags": Dict[str, str],
+    },
+    total=False,
+)
