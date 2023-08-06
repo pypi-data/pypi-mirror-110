4c72e7f5b8b0045ffdf85e71826f3e79a85994f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:20 +0000

    Add package ws-sdk/0.4/ws_sdk-0.4-py3-none-any.whl

---
+++ packages/ws-sdk/ws_sdk-0.4-py3-none-any.whl/ws_sdk/ws_errors.py
@@ -0,0 +1,15 @@
+class WsSdkError(Exception):
+    """Parent Exception Class"""
+
+
+class MissingTokenError(WsSdkError):
+    """Raised when token is missing"""
+    def __init__(self, token, token_type):
+        self.message = f"Token {token} does not exist in this {token_type}"
+        super().__init__(self.message)
+
+
+class TokenTypeError(WsSdkError):
+    def __init__(self, token):
+        self.message = f"Unable to discover Token Type of token {token}"
+        super().__init__(self.message)
