947fcd5aae5de0869bcb3e34f9e2c38bf09fb5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:31 +0000

    Add package django-access-control/0.4/django_access_control-0.4-py3-none-any.whl

---
+++ packages/django-access-control/django_access_control-0.4-py3-none-any.whl/django_access_control/querysets.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from typing import FrozenSet
+
+from django.contrib.auth.models import AbstractUser
+from django.db.models import QuerySet, Model
+
+from .models import all_field_names
+
+from .permissions import has_permission
+
+
+class ConfidentialQuerySet(QuerySet):
+
+    # Table wide permissions
+
+    def has_table_wide_add_permission(self, user: AbstractUser) -> bool:
+        return user.is_superuser or has_permission(user, "add", self.model)
+
+    def has_table_wide_view_permission(self, user: AbstractUser) -> bool:
+        return user.is_superuser or has_permission(user, "view", self.model)
+
+    def has_table_wide_change_permission(self, user: AbstractUser) -> bool:
+        return user.is_superuser or has_permission(user, "change", self.model)
+
+    def has_table_wide_delete_permission(self, user: AbstractUser) -> bool:
+        return user.is_superuser or has_permission(user, "delete", self.model)
+
+    # Row permissions
+
+    def rows_with_view_permission(self, user: AbstractUser) -> QuerySet:
+        return self if self.has_table_wide_view_permission(user) else self.none()
+
+    def rows_with_change_permission(self, user: AbstractUser) -> QuerySet:
+        return self if self.has_table_wide_change_permission(user) else self.none()
+
+    def rows_with_delete_permission(self, user: AbstractUser) -> QuerySet:
+        return self if self.has_table_wide_delete_permission(user) else self.none()
+
+    # Column permissions
+    @staticmethod
+    def viewable_fields(user: AbstractUser, obj) -> FrozenSet[str]:
+        return frozenset(all_field_names(obj))
+
+    @staticmethod
+    def changeable_fields(user: AbstractUser, obj) -> FrozenSet[str]:
+        return frozenset(all_field_names(obj))
+
+    def has_some_permissions(self, user: AbstractUser) -> QuerySet:
+        return (self if self.has_table_wide_add_permission(user) else self.none()) | \
+               self.rows_with_delete_permission(user) | \
+               self.rows_with_change_permission(user) | \
+               self.rows_with_view_permission(user)
+
+    def contains(self, obj: Model) -> bool:
+        return self.filter(pk=obj.pk).exists()
+
+
+def is_confidential(model: Model) -> bool:
+    return issubclass(model.objects._queryset_class, ConfidentialQuerySet)
