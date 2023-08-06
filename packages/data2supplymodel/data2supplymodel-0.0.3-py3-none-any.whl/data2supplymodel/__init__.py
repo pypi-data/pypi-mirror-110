faea8634bfafde2caf016386ba7f35164db12e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:33 +0000

    Add package data2supplymodel/0.0.3/data2supplymodel-0.0.3-py3-none-any.whl

---
+++ packages/data2supplymodel/data2supplymodel-0.0.3-py3-none-any.whl/data2supplymodel/__init__.py
@@ -0,0 +1,11 @@
+
+# import the packages
+
+from .data2supply import *
+from .field_attachment import * 
+from . period_setting import *
+from . setting import *
+
+
+
+print('data2supply, version 0.0.0')
