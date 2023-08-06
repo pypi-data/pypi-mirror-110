74bcd3802e4b26738cf03e39737e7914c12ba652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:47 +0000

    Add package synesthesia-uf/0.0.73/synesthesia-uf-0.0.73.tar.gz

---
+++ packages/synesthesia-uf/synesthesia-uf-0.0.73.tar.gz/synesthesia-uf-0.0.73/setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import codecs
+import os, sys, subprocess
+
+
+VERSION = '0.0.73'
+DESCRIPTION = 'A Python audio image creation tool'
+LONG_DESCRIPTION = 'A Python audio image creation tool that takes audio and creates images from them.'
+APT_PACKAGES = 'clementine ffmpeg gstreamer1.0-plugins-base dkms gstreamer1.0-plugins-ugly libqt5x11extras5 libcairo2-dev pkg-config'
+
+apt = "sudo apt "
+ins = "install "
+packages = APT_PACKAGES
+
+print("[+] Installation of the ubuntu packages is starting:")
+
+for items in packages.split():
+    command = str(apt) + str(ins) + str(items)
+
+    subprocess.run(command.split())
+    print("\t[+] Package [{}] Installed".format(str(items)))
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        # POST-INSTALL SCRIPT\
+        apt = "sudo apt "
+        ins = "install "
+        packages = APT_PACKAGES
+
+        print("[+] Installation of the ubuntu packages is starting:")
+
+        for items in packages.split():
+            command = str(apt) + str(ins) + str(items)
+
+            subprocess.run(command.split())
+            print("\t[+] Package [{}] Installed".format(str(items)))
+
+
+
+# Setting up
+setup(
+    name="synesthesia-uf",
+    version=VERSION,
+    author="Super Fun Adventure Club Dude Man Squad",
+    author_email="<georgekolasa@ufl.edu",
+    cmdclass={'install': PostInstallCommand},
+    description=DESCRIPTION,
+    packages=find_packages(),
+    package_data={'synesthesia': ['images/*.png', 'images/*.svg' ]},
+    url="https://github.com/cbaddeley/Synesthesia",
+    license="GPL 3",
+    install_requires=['PyQt5', 'librosa', 'essentia',
+                      'pillow', 'pycairo', 'musicnn'],
+    keywords=['audio', 'visualizer', "image"],
+
+    entry_points =
+    {   "console_scripts":
+        [
+            "syne = synesthesia:pip_main_func"
+        ]
+    }
+
+)
+
+
+
---
+++ packages/synesthesia-uf/synesthesia-uf-0.0.73.tar.gz/synesthesia-uf-0.0.73/synesthesia/__init__.py
@@ -0,0 +1,7 @@
+import glob, shutil, os, sys, subprocess, asyncio
+sys.path.append("synesthesia")
+
+
+from synesthesia.gui import *
+from synesthesia.wsl import *
+from synesthesia import *
