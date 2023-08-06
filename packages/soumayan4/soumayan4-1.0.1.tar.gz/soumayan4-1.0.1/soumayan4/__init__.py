38f093d7727eb76b0afb5a49f242a6e54550136e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:07 +0000

    Add package soumayan4/1.0.1/soumayan4-1.0.1.tar.gz

---
+++ packages/soumayan4/soumayan4-1.0.1.tar.gz/soumayan4-1.0.1/soumayan4/__init__.py
@@ -0,0 +1,8 @@
+import os
+from soumayan4.fake import english_fake
+from soumayan4.fake import spanish_fake
+from soumayan4.fake import italian_fake
+from soumayan4.fake import french_fake
+from soumayan4.fake import german_fake
+from soumayan4.bengali import bengali_fake
+from soumayan4.hindi import hindi_fake
