152fcba87141fb4e1e380d8c40eb903bfa67c4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:33 +0000

    Add package pymonstrosity/0.0.1/pymonstrosity-0.0.1.tar.gz

---
+++ packages/pymonstrosity/pymonstrosity-0.0.1.tar.gz/pymonstrosity-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymonstrosity",
+    version="0.0.1",
+    author="Fry",
+    author_email="",
+    description="A pip package that add's new If , Else , Elif , While , For statements.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fryghtened/pym",
+    project_urls={
+        "Bug Tracker": "https://github.com/Fryghtened/pym/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
