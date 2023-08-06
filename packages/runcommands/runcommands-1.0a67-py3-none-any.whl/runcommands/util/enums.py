d78b2f540c2a7b28d79e15a2cd7814ee1bd0b8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:13 +0000

    Add package runcommands/1.0a67/runcommands-1.0a67-py3-none-any.whl

---
+++ packages/runcommands/runcommands-1.0a67-py3-none-any.whl/runcommands/util/enums.py
@@ -0,0 +1,34 @@
+import enum
+import subprocess
+
+
+class Color(enum.Enum):
+
+    black = "black"
+    red = "red"
+    green = "green"
+    yellow = "yellow"
+    blue = "blue"
+    magenta = "magenta"
+    cyan = "cyan"
+    white = "white"
+
+    def __str__(self):
+        return f"[{self.value}]"
+
+
+class StreamOptions(enum.Enum):
+
+    """Choices for stream handling."""
+
+    capture = "capture"
+    hide = "hide"
+    none = "none"
+
+    @property
+    def option(self):
+        return {
+            "capture": subprocess.PIPE,
+            "hide": subprocess.DEVNULL,
+            "none": None,
+        }[self.value]
