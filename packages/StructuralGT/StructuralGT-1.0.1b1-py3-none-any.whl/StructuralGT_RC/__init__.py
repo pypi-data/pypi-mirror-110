708489b6eeaf447e725386bd5bbc7452f0ae10b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:01 +0000

    Add package StructuralGT/1.0.1b1/StructuralGT-1.0.1b1-py3-none-any.whl

---
+++ packages/StructuralGT/StructuralGT-1.0.1b1-py3-none-any.whl/StructuralGT_RC/__init__.py
@@ -0,0 +1,10 @@
+"""
+StructuralGT_RC
+
+A python package for automated graph theory analysis
+of digital structural network images.
+"""
+
+__version__ = "1.0.1a2"
+__author__ = "Drew Vecchio"
+__credits__ = "The Regents of the University of Michigan"
