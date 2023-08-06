22623fca59021d000c586f4097a0c521fdad37d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:39 +0000

    Add package synesthesia-uf/0.0.47/synesthesia_uf-0.0.47-py3-none-any.whl

---
+++ packages/synesthesia-uf/synesthesia_uf-0.0.47-py3-none-any.whl/synesthesia/__init__.py
@@ -0,0 +1,9 @@
+from synesthesia import *
+from synesthesia.gui import *
+from synesthesia.essentia_helper import *
+from synesthesia.image_helper import *
+from synesthesia.lib import *
+from synesthesia.wsl import *
+from synesthesia.images import *
+
+# os.chdir('synesthesia')
