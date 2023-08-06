583bf193c001aee3d776c566c19c69838c446920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:50 +0000

    Add package dsocli/1.0.41/dsocli-1.0.41-py3-none-any.whl

---
+++ packages/dsocli/dsocli-1.0.41-py3-none-any.whl/dsocli/version.py
@@ -0,0 +1,6 @@
+import os
+import pathlib
+print(__name__)
+with open(os.path.join(pathlib.Path(__file__).parent.resolve(), 'src', 'dsocli', '.release')) as f:
+    __release__ = f.read()
+__version__ = '1.0'
