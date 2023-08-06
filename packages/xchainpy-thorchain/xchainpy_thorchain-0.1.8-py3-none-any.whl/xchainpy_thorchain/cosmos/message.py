5e8701fc0318274c5ed1262ecca006c35d4c2e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:53 +0000

    Add package xchainpy-thorchain/0.1.8/xchainpy_thorchain-0.1.8-py3-none-any.whl

---
+++ packages/xchainpy-thorchain/xchainpy_thorchain-0.1.8-py3-none-any.whl/xchainpy_thorchain/cosmos/message.py
@@ -0,0 +1,8 @@
+from typing import List
+from .models.MsgCoin import MsgCoin
+from .models.MsgNativeTx import MsgNativeTx
+
+def msg_native_tx_from_json(coins : List[MsgCoin] , memo : str , signer : str) -> MsgNativeTx :
+    # return MsgNativeTx(coins , memo , frombech32(signer))
+    return MsgNativeTx(coins , memo , signer)
+
