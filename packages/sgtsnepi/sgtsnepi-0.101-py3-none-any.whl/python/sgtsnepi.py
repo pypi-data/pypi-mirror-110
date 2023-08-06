a0758e6254a403dff71b99709cb1a20356b42a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:42 +0000

    Add package sgtsnepi/0.101/sgtsnepi-0.101-py3-none-any.whl

---
+++ packages/sgtsnepi/sgtsnepi-0.101-py3-none-any.whl/python/sgtsnepi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from julia import Main
+from julia import Pkg
+
+
+# load packages in Julia
+Pkg.add("SparseArrays")
+Pkg.add(Pkg.PackageSpec(name="SGtSNEpi", version="0.2.1"))
+
+from julia import SGtSNEpi
+
+
+# prepare Julia wrappers
+Main.eval("""
+  using SparseArrays, SGtSNEpi
+  import SGtSNEpi.sgtsnepi
+  function scipyCSC_to_julia(A)
+    m, n = A.shape
+    colPtr = Int[i+1 for i in PyArray(A."indptr")]
+    rowVal = Int[i+1 for i in PyArray(A."indices")]
+    nzVal = Vector{Float64}(PyArray(A."data"))
+    B = SparseMatrixCSC{Float64,Int}(m, n, colPtr, rowVal, nzVal)
+  end
+  sgtsnepi( A::PyObject ; kwargs... ) = sgtsnepi( scipyCSC_to_julia(A) ; kwargs... )
+""")
+
+__all__ = ['sgtsnepi']
+
+
+# Setup Python function
+def sgtsnepi(A, **kwargs):
+    return SGtSNEpi.sgtsnepi(A, **kwargs)
