30a838703b34d21be54a4ca4cf33da677f188947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:44 +0000

    Add package memoria/2021.6.20.8/memoria-2021.6.20.8.tar.gz

---
+++ packages/memoria/memoria-2021.6.20.8.tar.gz/memoria-2021.6.20.8/memoria/__init__.py
@@ -0,0 +1,5 @@
+from .hash_object import hash_object
+from .hash_object import hash_object as hash
+from .hash_object import hash_object as smart_hash
+from .make_hashable import make_hashable
+from .to_bytes import to_bytes
