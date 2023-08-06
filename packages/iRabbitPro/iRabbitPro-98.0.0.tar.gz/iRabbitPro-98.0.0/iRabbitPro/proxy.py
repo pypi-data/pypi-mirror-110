ec25a9692fbda76e9e7fd0c09975ee916c36aa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package iRabbitPro/98.0.0/iRabbitPro-98.0.0.tar.gz

---
+++ packages/iRabbitPro/iRabbitPro-98.0.0.tar.gz/iRabbitPro-98.0.0/iRabbitPro/proxy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+import httpx
+import random
+
+
+def get_proxy():
+    url = f'http://ip.ipjldl.com/index.php/api/entry?method=proxyServer.generate_api_url&packid=0&fa=0&fetch_key=&groupid=0&qty=1&time=1&pro=&city=&port=1&format=txt&ss=1&css=&ipport=1&dt=1&specialTxt=3&specialJson=&usertype=2'
+    try:
+        resp = httpx.get(url)
+        while 'false' in resp.text:
+            time.sleep(random.randint(1, 3))
+            resp = httpx.get(url)
+        else:
+            print(f'获取到代理IP:' + resp.text)
+            return resp.text
+    except Exception as e:
+        print(e)
