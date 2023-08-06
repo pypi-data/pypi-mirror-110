c32cf0d505cd9b4adb7b7125bb62030c2390285d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:53 +0000

    Add package cubamap/0.0.1/cubamap-0.0.1-py3-none-any.whl

---
+++ packages/cubamap/cubamap-0.0.1-py3-none-any.whl/cubamap/frontend/build/precache-manifest.dbd5f4f102137286b34e2c1847566088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453951249cd049f95bc806208c125507",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6796464995e1092f18d",
+    "url": "./static/css/2.38ca0958.chunk.css"
+  },
+  {
+    "revision": "e6796464995e1092f18d",
+    "url": "./static/js/2.5dc22b8d.chunk.js"
+  },
+  {
+    "revision": "223e1043e6c92cd5b8490b603521d509",
+    "url": "./static/js/2.5dc22b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8100dbac8ce3f4255f9",
+    "url": "./static/js/main.33a19cc2.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.33a19cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
