025d7813a255f47f443f3b577e2900559719b53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:24 +0000

    Add package bluebelt/0.1.12/bluebelt-0.1.12-py3-none-any.whl

---
+++ packages/bluebelt/bluebelt-0.1.12-py3-none-any.whl/bluebelt/data/subsets.py
@@ -0,0 +1,24 @@
+
+def subset(frame, inverse=False, **kwargs):
+        
+    #check if filter values are lists and repair
+
+    # filters = {key: ([value] if not isinstance(value, list) else value) for key, value in kwargs.items()}
+    filters={}
+    for col in kwargs:
+        if col in frame.columns:
+            values = kwargs.get(col) if isinstance(kwargs.get(col), list) else [kwargs.get(col)]
+            for value in values:
+                if value not in frame[col].values:
+                    raise ValueError(f'{value} is not in {col}')
+            filters[col]=values
+        else:
+            raise ValueError(f'{col} is not in the dataframe')
+
+    #filter  
+    if inverse:
+        frame=frame[frame.isin(filters).sum(axis=1) != len(filters.keys())]
+    else:
+        frame=frame[frame.isin(filters).sum(axis=1) == len(filters.keys())]
+        
+    return frame
---
+++ packages/bluebelt/bluebelt-0.1.12-py3-none-any.whl/bluebelt/styles/paper/resolution.py
@@ -0,0 +1,15 @@
+import bluebelt.styles.defaults as defaults
+
+# main plot | pyplot.plot
+plot = {
+    'marker': None,
+    'zorder': 20,
+    'linestyle': '-',
+    'linewidth': 1,
+}
+
+# plot title | pyplot.set_title
+title = {
+    'loc': 'left',
+    'size': defaults.medium
+}
