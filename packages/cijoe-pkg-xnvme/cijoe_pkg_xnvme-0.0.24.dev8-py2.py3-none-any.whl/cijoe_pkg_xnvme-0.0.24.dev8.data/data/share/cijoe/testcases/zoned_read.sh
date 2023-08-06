584609d1aca044dc21e33b1df526ebc28b7b8f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:50 +0000

    Add package cijoe-pkg-xnvme/0.0.24.dev8/cijoe_pkg_xnvme-0.0.24.dev8-py2.py3-none-any.whl

---
+++ packages/cijoe-pkg-xnvme/cijoe_pkg_xnvme-0.0.24.dev8-py2.py3-none-any.whl/cijoe_pkg_xnvme-0.0.24.dev8.data/data/share/cijoe/testcases/zoned_read.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Verify that CLI `zoned read` runs without error
+#
+# NOTE: As is, then this test will most likely fail due to the condition of the
+# hard-coded zone address (0x0).
+#
+# shellcheck disable=SC2119
+#
+CIJ_TEST_NAME=$(basename "${BASH_SOURCE[0]}")
+export CIJ_TEST_NAME
+# shellcheck source=modules/cijoe.sh
+source "$CIJ_ROOT/modules/cijoe.sh"
+test::enter
+
+: "${XNVME_URI:?Must be set and non-empty}"
+
+: "${SLBA:=0x1}"
+: "${NLB:=0}"
+
+if ! cij::cmd "zoned read $XNVME_URI --slba $SLBA --nlb $NLB"; then
+  test::fail
+fi
+
+test::pass
