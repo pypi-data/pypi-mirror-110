8fba91185601a20f2e010a613ec7bd5412c25f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:46 +0000

    Add package mvcbotbase/0.1.5/mvcbotbase-0.1.5.tar.gz

---
+++ packages/mvcbotbase/mvcbotbase-0.1.5.tar.gz/mvcbotbase-0.1.5/mvcbotbase/__init__.py
@@ -0,0 +1,24 @@
+from mvcbotbase import social_network_provider_implementations, command_info
+from mvcbotbase.classes_for_command_arguments import (
+    BaseArg, IntArg, WordArg, StringArg, Sequence
+)
+from mvcbotbase.message_classes import (
+    AbstractIncomingMessage, OutgoingMessage, AttachmentType,
+    AbstractIncomingAttachment, OutgoingFileAttachment, Attachment,
+    ContentCantBeDownloadedError, UndownloadableAttachment,
+    AttachmentToUploadHasUnknownFileTypeError, OutgoingAttachment
+)
+from mvcbotbase.mvcbotbase import MVCBotBase
+from mvcbotbase.social_network_provider import SocialNetworkProvider
+
+__all__ = [
+    "MVCBotBase",
+    "SocialNetworkProvider", "social_network_provider", "BaseArg", "IntArg",
+    "mvcbotbase", "WordArg", "StringArg", "Sequence", "AttachmentType",
+    "OutgoingMessage", "OutgoingAttachment", "OutgoingFileAttachment",
+    "Attachment", "UndownloadableAttachment", "AbstractIncomingAttachment",
+    "AbstractIncomingMessage", "message_classes",
+    "AttachmentToUploadHasUnknownFileTypeError", "ContentCantBeDownloadedError",
+    "classes_for_command_arguments", "command_info",
+    "social_network_provider_implementations"
+]
