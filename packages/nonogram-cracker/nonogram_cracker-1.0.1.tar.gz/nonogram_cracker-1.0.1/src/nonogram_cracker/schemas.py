5feeb07b686deffcbdb653484e145bd1de095db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:06 +0000

    Add package nonogram-cracker/1.0.1/nonogram_cracker-1.0.1.tar.gz

---
+++ packages/nonogram-cracker/nonogram_cracker-1.0.1.tar.gz/nonogram_cracker-1.0.1/src/nonogram_cracker/schemas.py
@@ -0,0 +1,48 @@
+from marshmallow import fields, Schema, validate, validates_schema, ValidationError
+
+
+def is_valid_row_or_col(row_or_col):
+    return isinstance(row_or_col, list) and [isinstance(cell, int) for cell in row_or_col]
+
+
+class PuzzleSchema(Schema):
+    name = fields.String(required=True)
+
+    rows = fields.List(
+        fields.List(
+            fields.Integer(),
+            required=True
+        ),
+        required=True,
+        validate=validate.Length(min=1)
+    )
+
+    cols = fields.List(
+        fields.List(
+            fields.Integer(),
+            required=True
+        ),
+        required=True,
+        validate=validate.Length(min=1)
+    )
+
+    @validates_schema
+    def validate_array_lengths(self, data, **kwargs):
+        if (
+            'rows' in data and isinstance(data['rows'], list) and
+            'cols' in data and isinstance(data['cols'], list)
+        ):
+            rows = data['rows']
+            cols = data['cols']
+            height = len(rows)
+            width = len(cols)
+
+            # Only perform these checks if valid datatypes, all other cases are handled by schema
+            for i, row in enumerate(filter(is_valid_row_or_col, rows)):
+                min_width_for_clues = len(row) - 1 + sum(row)
+                if min_width_for_clues > width:
+                    raise ValidationError(f"Row #{i + 1} with clues {row} cannot fit in {width} columns")
+            for i, col in enumerate(filter(is_valid_row_or_col, cols)):
+                min_height_for_clues = len(col) - 1 + sum(col)
+                if min_height_for_clues > height:
+                    raise ValidationError(f"Col #{i + 1} with clues {col} cannot fit in {height} rows")
