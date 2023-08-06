f1f09eaf35cb8673a00c50da4e666fe2225b9c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:40 +0000

    Add package sym/0.3.5/sym-0.3.5.tar.gz

---
+++ packages/sym/sym-0.3.5.tar.gz/sym-0.3.5/scripts/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+if [[ "$DRONE_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
+    eval export ${1^^}_RELEASE_VERSION=\$CI_BRANCH
+fi
+
+python3 -m pip install symengine
+python3 -m pip install --user .[all]
+./scripts/run_tests.sh
+./scripts/render_notebooks.sh examples/
+./scripts/generate_docs.sh
+
+! grep "DO-NOT-MERGE!" -R . --exclude ci.sh
