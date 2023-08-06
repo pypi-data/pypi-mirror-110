5be086b011f1597b686cf02edb1cbee1dc227425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:21 +0000

    Add package hydrangea/0.1/hydrangea-0.1-py3-none-any.whl

---
+++ packages/hydrangea/hydrangea-0.1-py3-none-any.whl/hydrangea/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+//  -------------------------------------------------------------
+//  author        Giga
+//  project       qeeqbox/hydrangea
+//  email         gigaqeeq@gmail.com
+//  description   app.py (CLI)
+//  licensee      AGPL-3.0
+//  -------------------------------------------------------------
+//  contributors list qeeqbox/hydrangea/graphs/contributors
+//  -------------------------------------------------------------
+"""
+
+if __name__ == "__main__":
+	print("[!] Comming soon..")
