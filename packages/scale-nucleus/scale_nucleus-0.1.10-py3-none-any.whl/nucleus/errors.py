40c233a2b0892d9d4d9eae55ae0db45ba9937701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:25 +0000

    Add package scale-nucleus/0.1.10/scale_nucleus-0.1.10-py3-none-any.whl

---
+++ packages/scale-nucleus/scale_nucleus-0.1.10-py3-none-any.whl/nucleus/errors.py
@@ -0,0 +1,46 @@
+class ModelCreationError(Exception):
+    def __init__(self, message="Could not create the model"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ModelRunCreationError(Exception):
+    def __init__(self, message="Could not create the model run"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NotFoundError(Exception):
+    def __init__(
+        self, message="Could not open file. Check the path or if it exists."
+    ):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DatasetItemRetrievalError(Exception):
+    def __init__(self, message="Could not retrieve dataset items"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NucleusAPIError(Exception):
+    def __init__(
+        self, endpoint, command, requests_response=None, aiohttp_response=None
+    ):
+
+        if requests_response is not None:
+            message = f"Tried to {command.__name__} {endpoint}, but received {requests_response.status_code}: {requests_response.reason}."
+            if hasattr(requests_response, "text"):
+                if requests_response.text:
+                    message += (
+                        f"\nThe detailed error is:\n{requests_response.text}"
+                    )
+
+        if aiohttp_response is not None:
+            status, reason, data = aiohttp_response
+            message = f"Tried to {command.__name__} {endpoint}, but received {status}: {reason}."
+            if data:
+                message += f"\nThe detailed error is:\n{data}"
+
+        super().__init__(message)
