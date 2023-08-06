58cf070ecf5a9db061ba45bdcbff51af39b05f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:21 +0000

    Add package company-name-cleaner/1.0.3/company-name-cleaner-1.0.3.tar.gz

---
+++ packages/company-name-cleaner/company-name-cleaner-1.0.3.tar.gz/company-name-cleaner-1.0.3/fingerprints/__init__.py
@@ -0,0 +1,5 @@
+from fingerprints.generate import generate, clean_company_name
+from fingerprints.cleanup import clean_entity_name
+from fingerprints.replacers import remove_types
+
+__all__ = ["generate", "clean_entity_name", "remove_types", "clean_company_name"]
