9dfb663a010aa7b33d1d9f917c4d0391667723bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:33 +0000

    Add package slai-cli/0.6.8/slai_cli-0.6.8-py3-none-any.whl

---
+++ packages/slai-cli/slai_cli-0.6.8-py3-none-any.whl/slai_cli/__init__.py
@@ -0,0 +1,13 @@
+import click
+from slai_cli import log
+
+__version__ = "0.6.8"
+
+
+def get_version():
+    return __version__
+
+
+@click.command()
+def version():
+    log.info(f"Current CLI version: {get_version()}")
