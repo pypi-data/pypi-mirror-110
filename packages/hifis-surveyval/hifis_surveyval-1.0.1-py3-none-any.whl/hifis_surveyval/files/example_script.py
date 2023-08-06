7459277a06509805a1c1032bffd7a76734b104bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:12 +0000

    Add package hifis-surveyval/1.0.1/hifis_surveyval-1.0.1-py3-none-any.whl

---
+++ packages/hifis-surveyval/hifis_surveyval-1.0.1-py3-none-any.whl/hifis_surveyval/files/example_script.py
@@ -0,0 +1,34 @@
+# hifis-surveyval
+# Framework to help developing analysis scripts for the HIFIS Software survey.
+#
+# SPDX-FileCopyrightText: 2021 HIFIS Software <support@hifis.net>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This is an example script for an analysis.
+
+It is a file payload of the package `hifis_surveyval`.
+"""
+
+from hifis_surveyval.data_container import DataContainer
+from hifis_surveyval.hifis_surveyval import HIFISSurveyval
+
+
+def run(hifis_surveyval: HIFISSurveyval, data: DataContainer):
+    """Execute example script."""
+    frame = data.get_by_id()
+    hifis_surveyval.printer.print_dataframe(frame)
