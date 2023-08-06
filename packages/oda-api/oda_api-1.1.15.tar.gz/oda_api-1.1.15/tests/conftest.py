452c6e75476ce5acb8c80da6d0f0e54b544d243c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:24 +0000

    Add package oda-api/1.1.15/oda_api-1.1.15.tar.gz

---
+++ packages/oda-api/oda_api-1.1.15.tar.gz/oda_api-1.1.15/tests/conftest.py
@@ -0,0 +1,27 @@
+# we could make a plugin, but it's more effort
+import pytest
+
+from cdci_data_analysis.pytest_fixtures import (
+            dispatcher_live_fixture,
+            dispatcher_test_conf_fn,
+            dispatcher_debug,
+            dispatcher_nodebug,
+            dispatcher_long_living_fixture,
+        )
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "dda" in item.keywords:
+            item.add_marker(skip_slow)
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
+
