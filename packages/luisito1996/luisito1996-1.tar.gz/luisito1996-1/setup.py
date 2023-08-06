49cffb8a76eb26e2f449aafa436b1a88688d4a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:08 +0000

    Add package luisito1996/1/luisito1996-1.tar.gz

---
+++ packages/luisito1996/luisito1996-1.tar.gz/luisito1996-1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import os
+f = open ('/home/bravo/Escritorio/Prueba/requirements.txt','r')
+requerimientos=[]
+for linea in f:
+	requerimientos.append(linea) 
+f.close()
+setup(
+  name = 'luisito1996',
+  packages = ['src'], # this must be the same as the name above
+  version = '1',
+  description = 'my description',
+  author = 'Luis bravo',
+  author_email = 'luisbravocollado@gmail.com',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+  install_requires=requerimientos
+)
