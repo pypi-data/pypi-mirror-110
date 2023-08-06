276cf95422b42d1fd63c8b65f8f52403c3340f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:46 +0000

    Add package emoji-tools/0.0.1/emoji_tools-0.0.1-py3-none-any.whl

---
+++ packages/emoji-tools/emoji_tools-0.0.1-py3-none-any.whl/emoji_tools/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+
+
+from emoji_tools.core import *
+from emoji_tools.unicode_codes import *
+
+__all__ = [
+    'emojize', 'demojize', 'get_emoji_regexp', 'emoji_count', 'emoji_lis', 'replace_emoji',
+    'EMOJI_UNICODE_ENGLISH', 'EMOJI_UNICODE_SPANISH', 'EMOJI_UNICODE_PORTUGUESE',
+    'EMOJI_UNICODE_ITALIAN', 'EMOJI_UNICODE_FRENCH', 'EMOJI_UNICODE_GERMAN',
+    'UNICODE_EMOJI_ENGLISH', 'UNICODE_EMOJI_SPANISH', 'UNICODE_EMOJI_PORTUGUESE',
+    'UNICODE_EMOJI_ITALIAN', 'UNICODE_EMOJI_FRENCH', 'UNICODE_EMOJI_GERMAN',
+    'EMOJI_ALIAS_UNICODE_ENGLISH', 'UNICODE_EMOJI_ALIAS_ENGLISH',
+]
