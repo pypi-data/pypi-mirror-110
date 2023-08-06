4ad9492a52858ecac390712435e2491caf6bce65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:28 +0000

    Add package ppc-distributions/1.0/ppc_distributions-1.0.tar.gz

---
+++ packages/ppc-distributions/ppc_distributions-1.0.tar.gz/ppc_distributions-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ppc_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ppc_distributions'],
+      author = 'ppuentec',
+      author_email = 'career.development.miami@gmail.com',
+    zip_safe=False)
+                
