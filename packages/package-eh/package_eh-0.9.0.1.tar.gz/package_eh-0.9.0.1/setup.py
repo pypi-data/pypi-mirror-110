34eaa11fb68088d3206ba41a702b60ce9304a140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:57 +0000

    Add package package-eh/0.9.0.1/package_eh-0.9.0.1.tar.gz

---
+++ packages/package-eh/package_eh-0.9.0.1.tar.gz/package_eh-0.9.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='package_eh',
+    packages=['package_eh'],
+    description='Hello world enterprise edition',
+    version='0.9.0.1',
+    url=None,
+    author='eh',
+    author_email='eh@prism39.com',
+    keywords=['pip','eh','example']
+    )
+
