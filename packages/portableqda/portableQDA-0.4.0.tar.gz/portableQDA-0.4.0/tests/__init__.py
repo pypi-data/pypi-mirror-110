e8efd70d5b0d48d727c919a32c48c4a1afe6c7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:49 +0000

    Add package portableqda/0.4.0/portableQDA-0.4.0.tar.gz

---
+++ packages/portableqda/portableQDA-0.4.0.tar.gz/portableQDA-0.4.0/tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+PortableQDA tests - REFI-QDA 1.5 support package
+"""
+
+from test_portableqda import *
+from test_refi_qda import *
+
+if __name__ == '__main__':
+    print("""run tests like this, in the portableQDA directory:
+poetry shell
+python -m tests
+""")
