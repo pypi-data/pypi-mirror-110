3a3b23474b29c11095d4cb6ba18780b863803f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:22 +0000

    Add package AnueCrawler/1.0.1/AnueCrawler-1.0.1.tar.gz

---
+++ packages/AnueCrawler/AnueCrawler-1.0.1.tar.gz/AnueCrawler-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'AnueCrawler',
+    packages = ['anuecrawler.news'],
+    scripts = [],
+    version = '1.0.1',
+    description = 'Crawler for CNYes ',
+    long_description='README',
+    long_description_content_type='text/markdown',
+    author = 'LanDeveloper',
+    author_email = 'nilm987521@gmail.com',
+    url = 'https://github.com/LanDeveloper/AnueCrawler',
+    download_url = '',
+    keywords = ['pypi','cnyes','crawler'],
+    classifiers = []
+)
