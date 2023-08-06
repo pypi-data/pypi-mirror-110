6ca5c518bd5aaf9e2c262c33bc870a32932f7d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:46 +0000

    Add package cosapp-lab/0.15.4/cosapp_lab-0.15.4-py3-none-any.whl

---
+++ packages/cosapp-lab/cosapp_lab-0.15.4-py3-none-any.whl/cosapp_lab/_version.py
@@ -0,0 +1,8 @@
+"""
+Information about the package version.
+
+Until decided otherwise, the backend version is synchronized with the frontend.
+"""
+from cosapp_lab._frontend import module_version
+
+__version__ = module_version
