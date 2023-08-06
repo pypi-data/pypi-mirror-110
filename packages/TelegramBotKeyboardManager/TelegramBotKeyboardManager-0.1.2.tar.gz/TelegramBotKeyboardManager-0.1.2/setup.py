f96ddf14b8fb36f16a941334e3d96e27db89c721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:00 +0000

    Add package TelegramBotKeyboardManager/0.1.2/TelegramBotKeyboardManager-0.1.2.tar.gz

---
+++ packages/TelegramBotKeyboardManager/TelegramBotKeyboardManager-0.1.2.tar.gz/TelegramBotKeyboardManager-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TelegramBotKeyboardManager",
+    packages=["TelegramBotKeyboardManager"],
+    version="0.1.2",
+    author="Radin Gleb",
+    author_email="gleb.rad+pip@gmail.com",
+    description="This package help you to organise I/O In pyTelegramBotAPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Goradii/TelebotKeyboardManager",
+    project_urls={
+        "Bug Tracker": "https://github.com/Goradii/TelebotKeyboardManager/issues",
+    },
+    install_requires=['pyTelegramBotAPI'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.6",
+)
