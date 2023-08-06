cc98a951d45f82685fc4c1438efcf73028d27459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:14 +0000

    Add package hdext/0.0.1/hdext-0.0.1.tar.gz

---
+++ packages/hdext/hdext-0.0.1.tar.gz/hdext-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="hdext",
+    version="0.0.1",
+    description="Securing a private package.",
+    long_description="Securing a private package",
+    long_description_content_type="text/markdown",
+    packages=['hdext'],
+)
