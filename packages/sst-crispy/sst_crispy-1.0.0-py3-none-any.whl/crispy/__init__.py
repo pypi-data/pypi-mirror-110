dccd242fd3bbdb320689ce4ab62907f6104717a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:47 +0000

    Add package sst-crispy/1.0.0/sst_crispy-1.0.0-py3-none-any.whl

---
+++ packages/sst-crispy/sst_crispy-1.0.0-py3-none-any.whl/crispy/__init__.py
@@ -0,0 +1,9 @@
+from .crisp import *
+from .utils import *
+from .inversions import *
+from .io import *
+from .mixin import *
+from .neural_network import *
+from .spectral import *
+from .visualisation import *
+__version__ = "1.0.0"
