78998fb99baf8c73510f3c6bd59241d6990e2334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:47 +0000

    Add package fake-news-detector/0.0.1/fake_news_detector-0.0.1.tar.gz

---
+++ packages/fake-news-detector/fake_news_detector-0.0.1.tar.gz/fake_news_detector-0.0.1/fake_news_detector/__init__.py
@@ -0,0 +1,6 @@
+import os
+from fake_news_detector.fake import english_fake
+from fake_news_detector.fake import spanish_fake
+from fake_news_detector.fake import italian_fake
+from fake_news_detector.fake import french_fake
+from fake_news_detector.fake import german_fake
