17917b99a3380d98d2251e80a74d5681ef6c4d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:30 +0000

    Add package python-app/0.1/python_app-0.1.tar.gz

---
+++ packages/python-app/python_app-0.1.tar.gz/python_app-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python_app",
+    version="0.1",
+    author="vverholyak",
+    description="air connection app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://gitlab-1914910442.us-west-2.elb.amazonaws.com/vverkholyak/python_app",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[        
+        'flask==1.1.2',
+        'flask_restful==0.3.9',
+        'flask_sqlalchemy==2.5.1',
+        'mysql-connector-python==8.0.25',
+        'marshmallow==3.12.1',
+        'SQLAlchemy==1.4.15'
+    ]
+)
