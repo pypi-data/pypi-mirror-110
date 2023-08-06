277421083fb93939e47a8f5b4c0334a0de1ded7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:23 +0000

    Add package earth-vision/0.0.2/earth-vision-0.0.2.tar.gz

---
+++ packages/earth-vision/earth-vision-0.0.2.tar.gz/earth-vision-0.0.2/earthvision/datasets/utils.py
@@ -0,0 +1,19 @@
+import urllib
+import numpy as np
+from tqdm import tqdm
+from PIL import Image
+
+
+def _urlretrieve(url: str, filename: str, chunk_size: int = 1024) -> None:
+    with open(filename, "wb") as fh:
+        with urllib.request.urlopen(urllib.request.Request(url)) as response:
+            with tqdm(total=response.length) as pbar:
+                for chunk in iter(lambda: response.read(chunk_size), ""):
+                    if not chunk:
+                        break
+                    pbar.update(chunk_size)
+                    fh.write(chunk)
+
+
+def _load_img(fname):
+    return Image.open(fname)
