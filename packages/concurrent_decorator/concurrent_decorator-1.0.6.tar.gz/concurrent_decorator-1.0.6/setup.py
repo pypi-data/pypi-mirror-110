d6802fe68323a658cb3120fa377a2b11d2f1bfe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:17 +0000

    Add package concurrent_decorator/1.0.6/concurrent_decorator-1.0.6.tar.gz

---
+++ packages/concurrent_decorator/concurrent_decorator-1.0.6.tar.gz/concurrent_decorator-1.0.6/setup.py
@@ -0,0 +1,75 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README2.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    
+    name='concurrent_decorator',  # Required
+
+    version='1.0.6',  # Required
+
+    
+    description='A sample Python lib by easy use multi-thread',  # Optional
+
+   
+    long_description=long_description,  # Optional
+
+   
+    long_description_content_type='text/markdown',  # Optional (see note above)
+
+   
+    url='https://github.com/onlysleep/concurrent_decorator',  # Optional
+
+    author='xyzz',  # Optional
+
+   
+    author_email='xyzzpwn@foxmail.com',  # Optional
+
+  
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
+    
+    keywords='sample, setuptools, development',  # Optional
+ 
+    package_dir={'': 'src'},  # Optional
+
+    packages=find_packages(where='src'),  # Required
+
+    python_requires='>=3.6, <4',
+
+)
