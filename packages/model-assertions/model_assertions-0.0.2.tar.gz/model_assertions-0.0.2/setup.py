861f2597e844efddc1d8151416caacd52efb2361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:33 +0000

    Add package model-assertions/0.0.2/model_assertions-0.0.2.tar.gz

---
+++ packages/model-assertions/model_assertions-0.0.2.tar.gz/model_assertions-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='model_assertions',
+        version='0.0.2',
+        packages=['omg'],
+        author='Daniel Kang',
+        author_email='daniel.d.kang@gmail.com'
+)
