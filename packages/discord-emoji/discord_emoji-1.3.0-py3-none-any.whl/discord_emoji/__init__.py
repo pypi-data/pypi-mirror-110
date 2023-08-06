c09c83498584a6977d104f1c3640f8a1cf90a53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:48 +0000

    Add package discord-emoji/1.3.0/discord_emoji-1.3.0-py3-none-any.whl

---
+++ packages/discord-emoji/discord_emoji-1.3.0-py3-none-any.whl/discord_emoji/__init__.py
@@ -0,0 +1,7 @@
+from .methods import to_unicode, to_discord, to_unicode_multi, to_discord_multi  # noqa
+discord_to_unicode = to_unicode
+unicode_to_discord = to_discord
+discord_to_uni = to_unicode
+unicode_to_dis = to_discord
+
+__version__ = "1.3.0"
