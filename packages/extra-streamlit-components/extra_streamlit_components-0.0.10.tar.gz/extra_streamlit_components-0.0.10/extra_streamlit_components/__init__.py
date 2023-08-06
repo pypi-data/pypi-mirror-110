7d5ccbc2acd80a4356872479eae2f946707910f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:06 +0000

    Add package extra-streamlit-components/0.0.10/extra_streamlit_components-0.0.10.tar.gz

---
+++ packages/extra-streamlit-components/extra_streamlit_components-0.0.10.tar.gz/extra_streamlit_components-0.0.10/extra_streamlit_components/__init__.py
@@ -0,0 +1,8 @@
+IS_RELEASE = True
+
+from extra_streamlit_components.TabBar import tab_bar
+from extra_streamlit_components.TabBar import TabBarItemData
+
+from extra_streamlit_components.BouncingImage import bouncing_image
+
+from extra_streamlit_components.StepperBar import stepper_bar
