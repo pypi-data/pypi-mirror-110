d6be00a74cd67698597415cc1e41db6a80945e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:52 +0000

    Add package bordercrop/1.0.0/bordercrop-1.0.0-py3-none-any.whl

---
+++ packages/bordercrop/bordercrop-1.0.0-py3-none-any.whl/bordercrop/__init__.py
@@ -0,0 +1,18 @@
+"""
+bordercrop\n
+Crop the black borders from any image in a single line of code!
+
+© Anime no Sekai — 2021
+"""
+
+from bordercrop.bordercrop import *
+from bordercrop import exceptions
+
+__author__      = 'Anime no Sekai'
+__copyright__   = 'Copyright 2021, bordercrop'
+__credits__     = ['animenosekai']
+__license__     = 'MIT License'
+__version__     = 'bordercrop v1.0.0'
+__maintainer__  = 'Anime no Sekai'
+__email__       = 'niichannomail@gmail.com'
+__status__      = 'Stable'
