4a0e660f367944d37f28ce55544480a1a22d6fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:30 +0000

    Add package psnprofiles_scraper/1.3.2/psnprofiles_scraper-1.3.2-py3-none-any.whl

---
+++ packages/psnprofiles_scraper/psnprofiles_scraper-1.3.2-py3-none-any.whl/psnprofiles_scraper/src/PsnProfiles/Profile.py
@@ -0,0 +1,69 @@
+import json
+from typing import List
+
+from psnprofiles_scraper.src.PsnProfiles.ProfileSummary import ProfileSummary
+from psnprofiles_scraper.src.PsnProfiles.Trophy import Trophy
+from psnprofiles_scraper.src.PsnProfiles.Milestone import Milestone
+from psnprofiles_scraper.src.PsnProfiles.Game import Game
+from psnprofiles_scraper.src.PsnProfiles.Level import Level
+
+
+class Profile:
+    def __init__(self, name: str, country: str, avatar: str, summary: ProfileSummary, recent_trophies: List[Trophy],
+                 rarest_trophies: List[Trophy], milestones: List[Milestone], games: List[Game],
+                 trophy_cabinet: List[Trophy], level_history: List[Level]):
+        self.__name = name
+        self.__country = country
+        self.__avatar = avatar
+        self.__summary = summary
+        self.__recent_trophies = recent_trophies
+        self.__rarest_trophies = rarest_trophies
+        self.__milestones = milestones
+        self.__games = games
+        self.__trophy_cabinet = trophy_cabinet
+        self.__level_history = level_history
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_country(self) -> str:
+        return self.__country
+
+    def get_avatar(self) -> str:
+        return self.__avatar
+
+    def get_summary(self) -> ProfileSummary:
+        return self.__summary
+
+    def get_recent_trophies(self) -> List[Trophy]:
+        return self.__recent_trophies
+
+    def get_rarest_trophies(self) -> List[Trophy]:
+        return self.__rarest_trophies
+
+    def get_milestones(self) -> List[Milestone]:
+        return self.__milestones
+
+    def get_games(self) -> List[Game]:
+        return self.__games
+
+    def get_trophy_cabinet(self) -> List[Trophy]:
+        return self.__trophy_cabinet
+
+    def get_level_history(self) -> List[Level]:
+        return self.__level_history
+
+    def to_json(self):
+        return json.dumps({
+            "name": self.get_name(),
+            "country": self.get_country(),
+            "avatar": self.get_avatar(),
+            "summary": self.get_summary(),
+            "recent_trophies": self.get_recent_trophies(),
+            "rarest_trophies": self.get_rarest_trophies(),
+            "milestones": self.get_milestones(),
+            "games": self.get_games(),
+            "trophy_cabinet": self.get_trophy_cabinet(),
+            "level_history": self.get_level_history(),
+        }, default=lambda o: o.__dict__,
+            sort_keys=False, indent=2)
---
+++ packages/psnprofiles_scraper/psnprofiles_scraper-1.3.2-py3-none-any.whl/psnprofiles_scraper/src/PsnProfilesScraper.py
@@ -0,0 +1,176 @@
+from typing import List
+
+from psnprofiles_scraper.src.ProgressBar import ProgressBar
+from psnprofiles_scraper.src.BeautifulSoupFactory import BeautifulSoupFactory
+from psnprofiles_scraper.src.PsnProfiles.Profile import Profile
+from psnprofiles_scraper.src.PsnProfiles.ProfileSummary import ProfileSummary
+from psnprofiles_scraper.src.PsnProfiles.Trophy import Trophy
+from psnprofiles_scraper.src.PsnProfiles.Milestone import Milestone
+from psnprofiles_scraper.src.PsnProfiles.Game import Game
+from psnprofiles_scraper.src.PsnProfiles.Level import Level
+
+
+class PsnProfilesScraper:
+    def __init__(self):
+        self.__progress_bar = ProgressBar()
+
+        self.__psn_name = None
+        self.__soup_profile = None
+        self.__soup_games = None
+        self.__soup_level_history = None
+        self.__soup_stats = None
+
+    # Returns a full PSN profile object.
+    def get_profile(self, psn_name: str, detailed: bool) -> Profile:
+        self.__progress_bar.update_message("Initializing")
+        self.__psn_name = psn_name
+
+        self.__soup_games = BeautifulSoupFactory.create_for_games(self.__psn_name)
+        games = self.__get_games()
+
+        if detailed:
+            # Update max of progress bar and fetch each game separately.
+            self.__progress_bar.max = self.__progress_bar.max + len(games)
+            for game in games:
+                self.__progress_bar.update_message("Fetching " + game.title)
+                if not game.uri:
+                    self.__progress_bar.next()
+                    continue
+
+                # Update game instance and populate with details.
+                game.populate_details_from_soup(BeautifulSoupFactory.create_from_link(game.uri))
+                self.__progress_bar.next()
+
+        # Fetch profile data.
+        self.__progress_bar.update_message("Fetching profile")
+        self.__soup_profile = BeautifulSoupFactory.create_for_profile(self.__psn_name)
+        self.__progress_bar.next()
+
+        self.__soup_level_history = BeautifulSoupFactory.create_for_level_history(self.__psn_name)
+        self.__progress_bar.next()
+
+        self.__soup_stats = BeautifulSoupFactory.create_for_stats(self.__psn_name)
+        self.__progress_bar.next()
+
+        country = ""
+        if self.__soup_profile.select("img#bar-country"):
+            country = BeautifulSoupFactory.create_from_string(
+                self.__soup_profile.select("img#bar-country")[0]["title"]).text
+
+        # Extract profile summary.
+        profile_summary = ProfileSummary.create_from_soup(
+            BeautifulSoupFactory.create_from_string(str(self.__soup_profile) + str(self.__soup_stats)))
+        self.__progress_bar.next()
+
+        # Extract recent trophies.
+        recent_trophies = self.__get_recent_trophies()
+        self.__progress_bar.next()
+
+        # Extract rarest trophies.
+        rarest_trophies = self.__get_rarest_trophies()
+        self.__progress_bar.next()
+
+        # Extract milestones.
+        milestones = self.__get_milestones()
+        self.__progress_bar.next()
+
+        # Extract trophy cabinet.
+        trophy_cabinet = self.__get_trophy_cabinet()
+        self.__progress_bar.next()
+
+        # Extract level history.
+        level_history = self.__get_level_history()
+        self.__progress_bar.next()
+
+        profile = Profile(
+            self.__psn_name,
+            country,
+            self.__soup_profile.select("div.avatar img")[0]["src"] if self.__soup_profile.select(
+                "div.avatar img") else "",
+            profile_summary,
+            recent_trophies,
+            rarest_trophies,
+            milestones,
+            games,
+            trophy_cabinet,
+            level_history
+        )
+
+        self.__progress_bar.update_message("Complete")
+        self.__progress_bar.finish()
+        return profile
+
+    # Scrapes and returns the recent trophies.
+    def __get_recent_trophies(self) -> List[Trophy]:
+        if not self.__soup_profile.select("ul#recent-trophies"):
+            return []
+
+        return list(map(lambda trophy: Trophy.create_from_alternative_soup(
+            BeautifulSoupFactory.create_from_string(str(trophy))),
+                        self.__soup_profile.select("ul#recent-trophies")[0].find_all('li')))
+
+    # Scrapes and returns trophy cabinet
+    def __get_rarest_trophies(self) -> List[Trophy]:
+        if not self.__soup_profile.find("h3", text="Rarest Trophies"):
+            self.__progress_bar.next()
+            return []
+
+        if not self.__soup_profile.select('div.sidebar div.box.no-top-border table'):
+            self.__progress_bar.next()
+            return []
+
+        return list(
+            map(lambda trophy: Trophy.create_from_soup(BeautifulSoupFactory.create_from_string(str(trophy))),
+                self.__soup_profile.select('div.sidebar div.box.no-top-border table')[0].find_all("tr")))
+
+    # Scrapes and returns trophy cabinet
+    def __get_trophy_cabinet(self) -> List[Trophy]:
+        if not self.__soup_profile.find("h3", text="Trophy Cabinet"):
+            self.__progress_bar.next()
+            return []
+
+        if not self.__soup_profile.select("div.sidebar table.box.zebra"):
+            self.__progress_bar.next()
+            return []
+
+        return list(
+            map(lambda trophy: Trophy.create_from_soup(BeautifulSoupFactory.create_from_string(str(trophy))),
+                self.__soup_profile.select("div.sidebar table.box.zebra")[0].find_all("tr")))
+
+    # Scrapes and returns milestones.
+    def __get_milestones(self) -> List[Milestone]:
+        if not self.__soup_profile.find("h3", text="Trophy Milestones"):
+            self.__progress_bar.next()
+            return []
+
+        milestones_table = self.__soup_profile.find("h3", text="Trophy Milestones").find_next()
+
+        return list(
+            map(lambda milestone: Milestone.create_from_soup(BeautifulSoupFactory.create_from_string(str(milestone))),
+                milestones_table.find_all("tr")))
+
+    # Scrapes and returns games.
+    def __get_games(self) -> List[Game]:
+        if not self.__soup_games.find("tr"):
+            return []
+
+        games = []
+        for game in self.__soup_games.find_all("tr"):
+            if not game.find("div", class_="small-info"):
+                continue
+
+            if len(game.find_all("div", class_="small-info")) != 2:
+                continue
+
+            games.append(Game.create_from_soup(BeautifulSoupFactory.create_from_string(str(game))))
+
+        return games
+
+    # Scrapes and returns level history.
+    def __get_level_history(self) -> List[Level]:
+        if not self.__soup_level_history.select("table.box.zebra"):
+            self.__progress_bar.next()
+            return []
+
+        return list(map(lambda level: Level.create_from_soup(BeautifulSoupFactory.create_from_string(str(level))),
+                        self.__soup_level_history.select("table.box.zebra")[0].find_all("tr")))
