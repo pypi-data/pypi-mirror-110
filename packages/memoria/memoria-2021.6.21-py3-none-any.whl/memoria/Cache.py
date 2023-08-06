8596372603b051560b2dede5d86bcd241f731065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:48 +0000

    Add package memoria/2021.6.21/memoria-2021.6.21-py3-none-any.whl

---
+++ packages/memoria/memoria-2021.6.21-py3-none-any.whl/memoria/Cache.py
@@ -0,0 +1,79 @@
+from disk import Path
+from .hash_object import smart_hash
+
+
+class Cache:
+	def __init__(self, path, spark=None):
+		"""
+		:type path: amazonian.S3.S3Path or Path or str
+		:type spark: pyspark.sql.session.SparkSession
+		"""
+		if isinstance(path, str):
+			path = Path(path)
+
+		self._path = path
+		self._spark = spark
+		self._items = set()
+
+	@staticmethod
+	def get_key(item):
+		return smart_hash(item, base=32)
+
+	def get_path(self, key):
+		"""
+		:type key: str
+		:rtype: amazonian.S3.S3Path or Path
+		"""
+		return self._path / key
+
+	def set_item(self, item, value):
+		key = self.get_key(item)
+		path = self.get_path(key)
+		result = path.save(obj=value, mode='overwrite')
+		self._items.add(key)
+		return result
+
+	def get_item(self, item):
+		key = self.get_key(item)
+		path = self.get_path(key)
+		return path.load(spark=self._spark)
+
+	def contains(self, item):
+		key = self.get_key(item)
+		if key in self._items:
+			return True
+		else:
+			path = self.get_path(key)
+			if path.exists():
+				self._items.add(key)
+				return True
+			else:
+				return False
+
+	def delete_item(self, item):
+		key = self.get_key(item)
+		if key in self._items:
+			self._items.remove(key)
+			path = self.get_path(key)
+			if path.exists():
+				path.delete()
+			else:
+				raise FileNotFoundError(f'key "{key}" not found!')
+		else:
+			path = self.get_path(key)
+			if path.exists():
+				path.delete()
+			else:
+				raise KeyError(f'key "{key}" not found and path "{path}" not found!')
+
+	def __setitem__(self, item, value):
+		return self.set_item(item=item, value=value)
+
+	def __getitem__(self, item):
+		return self.get_item(item)
+
+	def __contains__(self, item):
+		return self.contains(item)
+
+	def __delitem__(self, item):
+		return self.delete_item(item)
