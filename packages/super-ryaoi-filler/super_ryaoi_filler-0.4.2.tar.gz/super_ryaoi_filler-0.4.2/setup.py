ef6c863926e5725bb967f7911c6bb371c0f1fd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:52 +0000

    Add package super-ryaoi-filler/0.4.2/super_ryaoi_filler-0.4.2.tar.gz

---
+++ packages/super-ryaoi-filler/super_ryaoi_filler-0.4.2.tar.gz/super_ryaoi_filler-0.4.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['super_ryaoi_filler']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['super_ryaoi_filler = '
+                     'super_ryaoi_filler.super_ryaoi_filler:main']}
+
+setup_kwargs = {
+    'name': 'super-ryaoi-filler',
+    'version': '0.4.2',
+    'description': "Don't download",
+    'long_description': None,
+    'author': 'ntoshihi',
+    'author_email': 'ntoshihi@student.42tokyo.jp',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
