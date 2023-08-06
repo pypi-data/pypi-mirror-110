40c6f7483ada0b5e3304a914696ada48c4e24094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:41 +0000

    Add package df3dbehav/0.22/df3dbehav-0.22.tar.gz

---
+++ packages/df3dbehav/df3dbehav-0.22.tar.gz/df3dbehav-0.22/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="df3dbehav",
+    version="0.22",
+    author="Semih Gunel",
+    packages=["df3dbehav", "df3dbehav.data"],
+    entry_points={"console_scripts": ["df3dbehav = df3dbehav.df3dbehav:main"]},
+    description="Behavior Estimation on DeepFly3D annotations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/semihgunel/Df3dBehav",
+    install_requires=[
+          'numpy',
+          'matplotlib',
+          'pandas',
+          'pytorch-lightning',
+          'torch',
+          'torchvision',
+          'torchaudio',
+          'einops',
+          'opencv-python',
+          'scipy',
+          'pickle5',
+          'tsaug'
+      ],
+)
