bd5f82c5955c235b166a3ca6552fe68d62a09347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:02 +0000

    Add package unicms-editorial-board/0.18.0/unicms-editorial-board-0.18.0.tar.gz

---
+++ packages/unicms-editorial-board/unicms-editorial-board-0.18.0.tar.gz/unicms-editorial-board-0.18.0/src/unicms_editorial_board/unicms_editorial_board_vue/src/pages/TemplateBlocks.vue
@@ -0,0 +1,36 @@
+<template>
+    <CompleteTable
+        :api_source="api_source"
+        :fields="fields"
+        :files="files"
+        :ordering_list="ordering_list"
+        :page_title="page_title"
+        :showCreatedBy="false" >
+    </CompleteTable>
+</template>
+<script>
+import CompleteTable from '../layout/CompleteTable.vue'
+
+export default {
+    components: {
+        CompleteTable
+    },
+    data () {
+        return {
+            api_source: '/api/editorial-board/templates/blocks/',
+            fields: ['id',
+                     'name',
+                     {key: 'type_friendly', label: 'Type'},
+                     'image',
+                     'description',
+            ],
+            files: ['image'],
+            ordering_list: [{ text: 'Id', value: 'id' },
+                            { text: 'Name', value: 'name' },
+                            { text: 'Type', value: 'type' },
+                            { text: 'Description', value: 'description' }],
+            page_title: 'Template blocks'
+        }
+    }
+}
+</script>
