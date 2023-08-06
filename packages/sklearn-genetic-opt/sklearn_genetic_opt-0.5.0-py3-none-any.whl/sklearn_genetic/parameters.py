b2b8e3542576b5008e09885d912a723c62a644de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:08 +0000

    Add package sklearn-genetic-opt/0.5.0/sklearn_genetic_opt-0.5.0-py3-none-any.whl

---
+++ packages/sklearn-genetic-opt/sklearn_genetic_opt-0.5.0-py3-none-any.whl/sklearn_genetic/parameters.py
@@ -0,0 +1,25 @@
+import enum
+
+
+class ExtendedEnum(enum.Enum):
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+class Algorithms(ExtendedEnum):
+    eaSimple = "eaSimple"
+    eaMuPlusLambda = "eaMuPlusLambda"
+    eaMuCommaLambda = "eaMuCommaLambda"
+
+
+class Criteria(ExtendedEnum):
+    max = "max"
+    min = "min"
+
+
+class Metrics(ExtendedEnum):
+    fitness = "fitness"
+    fitness_std = "fitness_std"
+    fitness_max = "fitness_max"
+    fitness_min = "fitness_min"
