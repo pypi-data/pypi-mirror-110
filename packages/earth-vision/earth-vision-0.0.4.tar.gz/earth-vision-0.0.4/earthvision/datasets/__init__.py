4a7d558b040b0f4e00fdd716357c7493885df21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package earth-vision/0.0.4/earth-vision-0.0.4.tar.gz

---
+++ packages/earth-vision/earth-vision-0.0.4.tar.gz/earth-vision-0.0.4/earthvision/datasets/__init__.py
@@ -0,0 +1,13 @@
+"""Import specific class that will use."""
+from .drone_deploy import DroneDeploy
+from .aerialcactus import AerialCactus
+from .eurosat import EuroSat
+from .resisc45 import RESISC45
+from .ucmercedland import UCMercedLand
+from .l8sparcs import L8SPARCS
+from .deepsat import DeepSat
+from .landcover import LandCover
+from .cowc import COWC
+
+__all__ = ['DroneDeploy', 'AerialCactus', 'RESISC45',
+           'UCMercedLand', 'EuroSat', 'L8SPARCS', 'DeepSat', 'LandCover', 'COWC']
