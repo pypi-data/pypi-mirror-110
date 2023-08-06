63111eec7f4c89981b3d041dc085336fd401c8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:42 +0000

    Add package sretools/0.1.101/sretools-0.1.101.tar.gz

---
+++ packages/sretools/sretools-0.1.101.tar.gz/sretools-0.1.101/sretools/__init__.py
@@ -0,0 +1,20 @@
+import sys
+
+from .ltexpect import LE
+from .tblfmt import SimpleTable
+from .json2html import JsonConverter
+from .json2html import jsonize
+from .commandline import commandline
+from .json2table import prepare_table
+if sys.platform in ['darwin','linux'] :
+    from .console_input import Input
+    from .dbx import DBX
+if sys.platform == "cygwin" :
+    from .console_input import Input
+if sys.platform in ['darwin','linux'] :
+    __all__ = [LE, SimpleTable, JsonConverter, commandline,Input,prepare_table,jsonize,DBX]
+elif sys.platform in ['cygwin'] :
+    __all__ = [LE, SimpleTable, JsonConverter, commandline,Input,prepare_table,jsonize]
+else :
+    __all__ = [LE, SimpleTable, JsonConverter, commandline,prepare_table,jsonize]
+
