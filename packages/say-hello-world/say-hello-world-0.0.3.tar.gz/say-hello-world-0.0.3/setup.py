74c36ed57bf9df4312cff5498a4b94be3d89534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:25 +0000

    Add package say-hello-world/0.0.3/say-hello-world-0.0.3.tar.gz

---
+++ packages/say-hello-world/say-hello-world-0.0.3.tar.gz/say-hello-world-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# here = os.path.abspath(os.path.dirname(__file__))
+#
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+VERSION = '0.0.03'
+DESCRIPTION = 'Just print sayhello world'
+LONG_DESCRIPTION = 'Just print sayhello world....'
+
+# Setting up
+setup(
+    name="say-hello-world",
+    version=VERSION,
+    author="Rashed",
+    author_email="<rashedhsn16@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sayhello', 'world', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
