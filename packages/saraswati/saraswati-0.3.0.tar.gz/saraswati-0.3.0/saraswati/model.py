a33d82de14b70f699d5c64fc8cd5bc0856d37f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:06 +0000

    Add package saraswati/0.3.0/saraswati-0.3.0.tar.gz

---
+++ packages/saraswati/saraswati-0.3.0.tar.gz/saraswati-0.3.0/saraswati/model.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List, Optional
+
+from appdirs import user_data_dir
+
+
+@dataclass
+class Channel:
+    name: str
+    source: str
+    options: Dict[str, str] = None
+
+
+@dataclass
+class SaraswatiSettings:
+
+    # Channel definition.
+    channels: List[Channel]
+
+    # Chunking options.
+    chunk_duration: Optional[int] = 5 * 60
+    chunk_max_files: Optional[int] = 9999
+
+    # Where to store the recordings.
+    spool_path: Optional[str] = None
+    spool_filename_pattern: Optional[
+        str
+    ] = "recording_{channel}_{timestamp}_{fragment:04d}.mka"
+
+    # Where and how often to upload recordings.
+    upload_target: Optional[str] = None
+    upload_interval: Optional[int] = 5 * 60
+
+    def __post_init__(self):
+        if self.spool_path is not None:
+            self.spool_path = Path(self.spool_path)
+        else:
+            from saraswati import __appname__
+
+            self.spool_path = Path(user_data_dir(__appname__, "hiveeyes")) / "spool"
+
+
+@dataclass
+class Pipeline:
+    expression: str
+    gst: object = None
