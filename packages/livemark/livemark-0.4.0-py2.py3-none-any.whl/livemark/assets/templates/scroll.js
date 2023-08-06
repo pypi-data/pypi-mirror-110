0d42a66f3c67142f40cc75a7ad56bd6f353df5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:48 +0000

    Add package livemark/0.4.0/livemark-0.4.0-py2.py3-none-any.whl

---
+++ packages/livemark/livemark-0.4.0-py2.py3-none-any.whl/livemark/assets/templates/scroll.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {
+  UeScroll.init({ element: ".ue-scroll", scrollSpeed: 10 });
+});
---
+++ packages/livemark/livemark-0.4.0-py2.py3-none-any.whl/livemark/document.py
@@ -0,0 +1,62 @@
+import yaml
+import marko
+import subprocess
+import frictionless
+from marko.ext.gfm import GFM
+from jinja2 import Environment, FileSystemLoader
+from .renderer import LivemarkExtension, LivemarkRendererMixin
+from . import config
+
+
+class Document:
+    def __init__(self, path, *, layout_path=None):
+        self.__path = path
+
+    # Process
+
+    def process(self):
+        markdown = marko.Markdown()
+        markdown.use(GFM)
+        markdown.use(LivemarkExtension)
+        templating = Environment(
+            loader=FileSystemLoader(config.TEMPLATES),
+            trim_blocks=True,
+        )
+
+        # Source document
+        with open(self.__path) as file:
+            source = file.read()
+            target = source
+
+        # Parse document
+        metadata = {}
+        if target.startswith("---"):
+            frontmatter, target = target.split("---", maxsplit=2)[1:]
+            metadata = yaml.safe_load(frontmatter)
+            # TODO: it's a hack as marko doesn't have context
+            LivemarkRendererMixin.metadata = metadata
+
+        # Prepare document
+        for code in metadata.get("prepare", []):
+            subprocess.run(code, shell=True)
+
+        # Preprocess document
+        template = templating.from_string(target)
+        target = template.render(frictionless=frictionless)
+
+        # Convert document
+        target = markdown.convert(target).strip()
+
+        # Postprocess document
+        layout = config.LAYOUT
+        if metadata.get("layout"):
+            with open(metadata["layout"]) as file:
+                layout = file.read()
+        template = templating.from_string(layout)
+        target = template.render(metadata=metadata, content=target)
+
+        # Cleanup document
+        for code in metadata.get("cleanup", []):
+            subprocess.run(code, shell=True)
+
+        return source, target
