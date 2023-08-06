04d6c18903dcae6084718bd30d8ee51767aa2c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:04 +0000

    Add package jina/2.0.0rc9.dev22/jina-2.0.0rc9.dev22.tar.gz

---
+++ packages/jina/jina-2.0.0rc9.dev22.tar.gz/jina-2.0.0rc9.dev22/jina/parsers/client.py
@@ -0,0 +1,34 @@
+"""Module for argparse for Client"""
+from .helper import add_arg_group
+
+
+def mixin_comm_protocol_parser(parser):
+    """Add the arguments for the protocol to the parser
+
+    :param parser: the parser configure
+    """
+
+    from ..enums import GatewayProtocolType
+
+    parser.add_argument(
+        '--protocol',
+        type=GatewayProtocolType.from_string,
+        choices=list(GatewayProtocolType),
+        default=GatewayProtocolType.GRPC,
+        help='Communication protocol between server and client.',
+    )
+
+
+def mixin_client_features_parser(parser):
+    """Add the arguments for the client to the parser
+
+    :param parser: the parser configure
+    """
+    gp = add_arg_group(parser, title='Client Features')
+
+    parser.add_argument(
+        '--asyncio',
+        action='store_true',
+        default=False,
+        help='If set, then the input and output of this Client work in an asynchronous manner. ',
+    )
