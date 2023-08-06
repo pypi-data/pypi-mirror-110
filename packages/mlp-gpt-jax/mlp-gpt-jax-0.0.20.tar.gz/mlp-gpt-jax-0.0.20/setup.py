9dea58092fe4c3e51d8e696dc29eb5ed92295811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:36 +0000

    Add package mlp-gpt-jax/0.0.20/mlp-gpt-jax-0.0.20.tar.gz

---
+++ packages/mlp-gpt-jax/mlp-gpt-jax-0.0.20.tar.gz/mlp-gpt-jax-0.0.20/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlp-gpt-jax",
+    packages=find_packages(),
+    version="0.0.20",
+    license="MIT",
+    description="MLP GPT - Jax",
+    author="Phil Wang",
+    author_email="",
+    url="https://github.com/lucidrains/mlp-gpt-jax",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "language model",
+        "multilayered-perceptron",
+        "jax"
+    ],
+    install_requires=[
+        "click",
+        "click-option-group",
+        "einops>=0.3",
+        "dm-haiku",
+        "jax",
+        "jaxlib",
+        "optax",
+        "torch",
+        "tqdm"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
