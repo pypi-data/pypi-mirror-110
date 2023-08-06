d061cfedc21b1aabcc2bdbb8e537a4607cd5baa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:09 +0000

    Add package appteka/0.16.0/appteka-0.16.0.tar.gz

---
+++ packages/appteka/appteka-0.16.0.tar.gz/appteka-0.16.0/appteka/pyqt/testing.py
@@ -0,0 +1,9 @@
+# pylint: skip-file
+# flake8: noqa
+
+"""The tool for visual testing of widgets."""
+
+from warnings import warn
+from pyqtest import TestApp
+
+warn("module testing is deprecated, use pyqtest", FutureWarning)
