1e06cd6096b5da5cba2c22976d428fe45ddf8156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:02 +0000

    Add package cmake/3.20.4/cmake-3.20.4.tar.gz

---
+++ packages/cmake/cmake-3.20.4.tar.gz/cmake-3.20.4/CMakeUrls.cmake
@@ -0,0 +1,26 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.20.4/cmake-3.20.4.tar.gz")
+set(unix_source_sha256       "87a4060298f2c6bb09d479de1400bc78195a5b55a65622a7dceeb3d1090a1b16")
+
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.20.4/cmake-3.20.4.zip")
+set(windows_source_sha256    "943a80de2520c5c053bcca7c93d4613e1f74e9faa95d3cb0429ce785e9d0b382")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256    "NA")
+
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.20.4/cmake-3.20.4-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "067feed25b76b3adf5863f5a5f7e2b8cafb2dcd6feeaac39a713372ef2c3584c")
+
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.20.4/cmake-3.20.4-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "368bf3bedac38a931a334b9b515d83879c82dacd55eed2adb1ef2725b9038607")
+
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.20.4/cmake-3.20.4-windows-i386.zip")
+set(win32_binary_sha256      "e571c0cebb237da800198ef89d1c874d15f04dfe13dc44d85c2a56e90a4a5209")
+
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.20.4/cmake-3.20.4-windows-x86_64.zip")
+set(win64_binary_sha256      "965d2f001c3ca807d288f2b6b15c42b25579a0e73ef12c2a72c95f4c69123638")
