a0ea825b127aa85b94ac90753d7c9428f7a3ef52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:40 +0000

    Add package todo-database/0.0.4/todo_database-0.0.4.tar.gz

---
+++ packages/todo-database/todo_database-0.0.4.tar.gz/todo_database-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="todo_database",
+    version="0.0.4",
+    author="Roman Esin",
+    author_email="esinromanswift@gmail.com",
+    description="A todo database wrapper for DPL course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/romanesin/todo_database",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['todo'],
+    python_requires=">=3.7",
+)
