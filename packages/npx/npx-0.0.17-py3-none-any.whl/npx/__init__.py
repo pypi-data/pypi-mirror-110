d68a05d84b31110206ad8e49fa3bc7afdc546006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:20 +0000

    Add package npx/0.0.17/npx-0.0.17-py3-none-any.whl

---
+++ packages/npx/npx-0.0.17-py3-none-any.whl/npx/__init__.py
@@ -0,0 +1,14 @@
+from ._isin import isin_rows
+from ._main import add_at, dot, solve, subtract_at, sum_at, unique_rows
+from ._mean import mean
+
+__all__ = [
+    "dot",
+    "solve",
+    "sum_at",
+    "add_at",
+    "subtract_at",
+    "unique_rows",
+    "isin_rows",
+    "mean",
+]
