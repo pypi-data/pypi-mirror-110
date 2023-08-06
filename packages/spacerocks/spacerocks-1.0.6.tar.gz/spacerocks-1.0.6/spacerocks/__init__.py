66bed919d5d1b0821cb2a036d389478b8dec546d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:33 +0000

    Add package spacerocks/1.0.6/spacerocks-1.0.6.tar.gz

---
+++ packages/spacerocks/spacerocks-1.0.6.tar.gz/spacerocks-1.0.6/spacerocks/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.0.6'
+__author__ = 'Kevin Napier'
+
+from .spacerock import SpaceRock
+from .units import Units
+#from .propagate import Propagate
+#from .observe import Observe
+#from .neptune_resonances import *
+#from .linalg3d import *
+#from .constants import *
+#from .jacobians import *
