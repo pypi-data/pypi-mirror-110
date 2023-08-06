b8f42d0c0971c59035b0a9c7bc45b032ca677e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:55 +0000

    Add package thechef-filler/0.1.19/thechef_filler-0.1.19.tar.gz

---
+++ packages/thechef-filler/thechef_filler-0.1.19.tar.gz/thechef_filler-0.1.19/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['thechef_filler', 'thechef_filler.chef']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['thechef_filler = thechef_filler.main:main']}
+
+setup_kwargs = {
+    'name': 'thechef-filler',
+    'version': '0.1.19',
+    'description': '',
+    'long_description': None,
+    'author': 'thechef',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/thechef-filler/thechef_filler-0.1.19.tar.gz/thechef_filler-0.1.19/thechef_filler/main.py
@@ -0,0 +1,9 @@
+from john_filler.chef import filler
+
+
+def main() -> None:
+    filler.main()
+
+
+if __name__ == "__main__":
+    main()
