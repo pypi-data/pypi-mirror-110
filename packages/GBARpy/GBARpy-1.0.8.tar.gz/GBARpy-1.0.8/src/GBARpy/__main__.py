153ff6ba9ded75b3835434768ca84912900951f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:35 +0000

    Add package GBARpy/1.0.8/GBARpy-1.0.8.tar.gz

---
+++ packages/GBARpy/GBARpy-1.0.8.tar.gz/GBARpy-1.0.8/src/GBARpy/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 15:07:10 2020
+
+@author: samuel.niang@cern.ch
+"""
+
+from GBARpy.Main_window import MainWindow
+# Uncomment for edit mode
+# from Main_window import MainWindow
+
+print('  _|_|_|  _|_|_|      _|_|    _|_|_|  ', '\n',
+      '_|        _|    _|  _|    _|  _|    _|', '\n',
+      '_|  _|_|  _|_|_|    _|_|_|_|  _|_|_|  ', '\n',
+      '_|    _|  _|    _|  _|    _|  _|    _|', '\n',
+      '  _|_|_|  _|_|_|    _|    _|  _|    _|')
+
+win = MainWindow()            
+win.mainloop()
+win.quit()
