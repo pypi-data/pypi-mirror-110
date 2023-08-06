1a22689c46ced99eff126d56a9b1252d7c69babc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package resistics-readers/0.1.1/resistics-readers-0.1.1.tar.gz

---
+++ packages/resistics-readers/resistics-readers-0.1.1.tar.gz/resistics-readers-0.1.1/resistics_readers/spam/__init__.py
@@ -0,0 +1,9 @@
+"""
+Readers for SPAM data, including
+
+- Readers for time series data
+- Readers for calibration files
+"""
+from resistics_readers.spam.spam import TimeReaderXTR  # noqa
+from resistics_readers.spam.calibration import SensorCalibrationRSP  # noqa
+from resistics_readers.spam.calibration import SensorCalibrationRSPX  # noqa
