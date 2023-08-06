6d570e02db30cad250324090f26d14ab6bb176fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:08 +0000

    Add package vtda/1.0.0/vtda-1.0.0-py3-none-any.whl

---
+++ packages/vtda/vtda-1.0.0-py3-none-any.whl/vtda/__init__.py
@@ -0,0 +1,15 @@
+﻿"""
+vtda
+Vibration Test Data Analysis
+"""
+
+__version__ = '1.0.0'
+__author__ = 'zhangshenglong'
+
+'''
+read_data
+'''
+from vtda.read_data.read_dasp import (
+                                               read_dasp_data_single,
+                                               read_dasp_data
+                                            )
