002545e77febeacd0b40935ba763038a74939964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:08 +0000

    Add package textgameengine/0.1/textgameengine-0.1.tar.gz

---
+++ packages/textgameengine/textgameengine-0.1.tar.gz/textgameengine-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'textgameengine',
+  version = '0.1',
+  license='MIT',
+  description = 'A game engine made in python which uses text as its graphic interface instead of regular 2d shapes and sprites.',
+  author = 'Daniel Pagano',
+  author_email = 'danielpagano202@gmail.com',
+  url = 'https://github.com/toasterstrudelz/Text-Based-Game-Engine',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Python', 'Text', 'Game-Engine'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
