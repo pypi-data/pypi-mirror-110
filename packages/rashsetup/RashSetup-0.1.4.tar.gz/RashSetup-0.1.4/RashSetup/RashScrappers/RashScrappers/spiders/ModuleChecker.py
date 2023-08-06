489fcf3f283fa9022c2b4d36229c3e9ab3114b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:49 +0000

    Add package rashsetup/0.1.4/RashSetup-0.1.4.tar.gz

---
+++ packages/rashsetup/RashSetup-0.1.4.tar.gz/RashSetup-0.1.4/RashSetup/RashScrappers/RashScrappers/spiders/ModuleChecker.py
@@ -0,0 +1,61 @@
+import scrapy
+
+
+class ModuleCheckerSpider(scrapy.Spider):
+    name = 'ModuleChecker'
+
+    allowed_domains = [
+        'github.com'
+    ]
+
+    def __init__(
+            self,
+            url,
+            pipe,
+            err_back
+    ):
+        super().__init__()
+        self.pipe = pipe
+        self.url = url
+        self.error_back = err_back
+
+    def start_requests(self):
+        yield scrapy.Request(
+            self.start_urls,
+            errback=self.error_back
+        )
+
+    def parse(self, response, *args):
+        entities = response.xpath("//div[@class='Box-row Box-row--focus-gray py-2 d-flex position-relative "
+                                  "js-navigation-item ']")
+
+        for entity in entities:
+            entity_ = entity.xpath(".//span/a/text()").get()
+
+            if entity_ not in (
+                "settings.json",
+                "setup.py",
+                "README.md"
+            ):
+                continue
+
+            yield scrapy.Request(
+                response.urljoin(
+                    entity.xpath(".//span/a/@href").get()
+                ), callback=self.yield_raw_settings, meta={"name": entity_}, errback=self.error_back
+            )
+
+            break
+
+    def yield_raw_settings(self, response):
+        raw = response.urljoin(response.xpath("//div[@class='BtnGroup']/a/@href").get())
+
+        self.pipe(
+            {
+                response.request.meta["name"]: raw
+            }
+        )
+
+        yield {
+            "raw_link": raw
+        }
