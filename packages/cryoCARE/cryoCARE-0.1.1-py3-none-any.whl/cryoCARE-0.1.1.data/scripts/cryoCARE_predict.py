0f251401ecc04f6b3cddea08caeba67692f6f8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:25 +0000

    Add package cryoCARE/0.1.1/cryoCARE-0.1.1-py3-none-any.whl

---
+++ packages/cryoCARE/cryoCARE-0.1.1-py3-none-any.whl/cryoCARE-0.1.1.data/scripts/cryoCARE_predict.py
@@ -0,0 +1,56 @@
+#!python
+import argparse
+import json
+from os.path import join
+import datetime
+import mrcfile
+import numpy as np
+
+from cryocare.internals.CryoCARE import CryoCARE
+from cryocare.internals.CryoCAREDataModule import CryoCARE_DataModule
+
+import psutil
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run cryoCARE prediction.')
+    parser.add_argument('--conf')
+
+    args = parser.parse_args()
+    with open(args.conf, 'r') as f:
+        config = json.load(f)
+
+    dm = CryoCARE_DataModule()
+    dm.load(config['path'])
+
+    model = CryoCARE(None, config['model_name'], basedir=config['path'])
+
+    even = mrcfile.mmap(config['even'], mode='r', permissive=True)
+    odd = mrcfile.mmap(config['odd'], mode='r', permissive=True)
+    denoised = mrcfile.new_mmap(join(config['path'], config['output_name']), even.data.shape, mrc_mode=2,
+                                overwrite=True)
+
+    even.data.shape += (1,)
+    odd.data.shape += (1,)
+    denoised.data.shape += (1,)
+
+    mean, std = dm.train_dataset.mean, dm.train_dataset.std
+
+    model.predict(even.data, odd.data, denoised.data, axes='ZYXC', normalizer=None, mean=mean, std=std,
+                  n_tiles=config['n_tiles'] + [1, ])
+
+    for l in even.header.dtype.names:
+        if l == 'label':
+            new_label = np.concatenate((even.header[l][1:-1], np.array([
+                'cryoCARE                                                ' + datetime.datetime.now().strftime(
+                    "%d-%b-%y  %H:%M:%S") + "     "]),
+                                        np.array([''])))
+            print(new_label)
+            denoised.header[l] = new_label
+        else:
+            denoised.header[l] = even.header[l]
+    denoised.header['mode'] = 2
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/cryoCARE/cryoCARE-0.1.1-py3-none-any.whl/cryoCARE-0.1.1.data/scripts/cryoCARE_train.py
@@ -0,0 +1,43 @@
+#!python
+import argparse
+import json
+from cryocare.internals.CryoCARE import CryoCARE
+from csbdeep.models import Config
+import pickle
+from os.path import join
+from cryocare.internals.CryoCAREDataModule import CryoCARE_DataModule
+
+def main():
+    parser = argparse.ArgumentParser(description='Load training config.')
+    parser.add_argument('--conf')
+
+    args = parser.parse_args()
+    with open(args.conf, 'r') as f:
+        config = json.load(f)
+
+    dm = CryoCARE_DataModule()
+    dm.load(config['train_data'])
+
+    net_conf = Config(
+        axes='ZYXC',
+        train_loss='mse',
+        train_epochs=config['epochs'],
+        train_steps_per_epoch=config['steps_per_epoch'],
+        train_batch_size=config['batch_size'],
+        unet_kern_size=config['unet_kern_size'],
+        unet_n_depth=config['unet_n_depth'],
+        unet_n_first=config['unet_n_first'],
+        train_tensorboard=False,
+        train_learning_rate=config['learning_rate']
+    )
+
+    model = CryoCARE(net_conf, config['model_name'], basedir=config['path'])
+
+    history = model.train(dm.get_train_dataset(), dm.get_val_dataset())
+
+    print(list(history.history.keys()))
+    with open(join(config['path'], config['model_name'], 'history.dat'), 'wb+') as f:
+        pickle.dump(history.history, f)
+
+if __name__ == "__main__":
+    main()
