9675327afe30a13a0bb5c9c98dc4e70f5bc96cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:47 +0000

    Add package qianyantest7/0.0.1/qianyantest7-0.0.1.tar.gz

---
+++ packages/qianyantest7/qianyantest7-0.0.1.tar.gz/qianyantest7-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="qianyantest7",
+    version="0.0.1",
+    license="MIT Licence",
+
+    url="https://github.com/DecstionBack/test",
+    author="lsw",
+    author_email="lvshangwen1011@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
