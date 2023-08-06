87c2f4ea5bfff5a8702b9f9ffeb40fdc8a53ea17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:09 +0000

    Add package pymodoro-gui/1.3.1/pymodoro-gui-1.3.1.tar.gz

---
+++ packages/pymodoro-gui/pymodoro-gui-1.3.1.tar.gz/pymodoro-gui-1.3.1/README.md
@@ -0,0 +1,24 @@
+# **Pymodoro-gui, a python pomodoro app with a GUI for Windows**
+
+A simple GUI python app for using the [pomodoro technique](https://en.wikipedia.org/wiki/Pomodoro_Technique) when working.
+
+Only uses the python standard library, so no additional packages are required.
+
+Computer will beep and show a popup when every interval (work/break) is done.
+
+###### **To install:**
+
+`pip install pymodoro-gui`
+
+###### **To run the package:**
+
+`python -m pymodoro-gui`
+
+###### Screens:
+
+![Screen1](https://github.com/Lcrs123/pymodoro-gui/blob/master/pymodoro-gui/resources/Screen1.png?raw=True) 
+
+![Screen2](https://github.com/Lcrs123/pymodoro-gui/blob/master/pymodoro-gui/resources/Screen2.png?raw=True)
+
+
+
---
+++ packages/pymodoro-gui/pymodoro-gui-1.3.1.tar.gz/pymodoro-gui-1.3.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pymodoro-gui',
+    packages=setuptools.find_packages(),
+    version='1.3.1',
+    license='GPL 3.0',
+    description='A pomodoro app with a GUI for Windows',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Lucas Camillo',
+    author_email='lucascamillo333@hotmail.com',
+    url='https://github.com/Lcrs123/pymodoro-gui.git',
+    keywords=['pomodoro', 'gui', 'windows', 'tkinter'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: Microsoft :: Windows',
+    ],
+)
