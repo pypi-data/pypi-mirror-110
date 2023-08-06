096a41c0c763085bdb06bf60064ec5be8500f3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:51 +0000

    Add package django-multiforloop/0.3.0/django-multiforloop-0.3.0.tar.gz

---
+++ packages/django-multiforloop/django-multiforloop-0.3.0.tar.gz/django-multiforloop-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='django-multiforloop',
+    version='0.3.0',
+    packages=['multiforloop', 'multiforloop.templatetags'],
+    author='Gabriel Grant',
+    author_email='g@briel.ca',
+    license='LGPL',
+    long_description=open('README.markdown').read(),
+    long_description_content_type="text/markdown",
+    url='http://github.org/gabrielgrant/django-multiforloop/',
+)
+
