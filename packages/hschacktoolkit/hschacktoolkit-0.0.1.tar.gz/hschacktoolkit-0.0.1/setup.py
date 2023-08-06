90aadea5a7d995d2009f66a90ff0a506286efc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:21 +0000

    Add package hschacktoolkit/0.0.1/hschacktoolkit-0.0.1.tar.gz

---
+++ packages/hschacktoolkit/hschacktoolkit-0.0.1.tar.gz/hschacktoolkit-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='hschacktoolkit',
+    version='0.0.1',
+    description='A tool ',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Ian Moffet and Arnav Nanduri',
+    author_email='arnanduri2006@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Hack',
+    packages=find_packages(),
+    install_requires=['requests']
+)
+
