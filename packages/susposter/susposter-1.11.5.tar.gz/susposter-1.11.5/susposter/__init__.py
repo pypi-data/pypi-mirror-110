f819c44cf7d969000bf88988fd41ce98eb72e6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:43 +0000

    Add package susposter/1.11.5/susposter-1.11.5.tar.gz

---
+++ packages/susposter/susposter-1.11.5.tar.gz/susposter-1.11.5/susposter/__init__.py
@@ -0,0 +1,13 @@
+from io import BytesIO
+
+__version__ = "1.11.5"
+
+def sussy():
+  print("so sussy")
+  return "so sussy"
+
+def sus():
+  return BytesIO(open("./susposter/susposter.png", "rb").read())
+
+def amongus():
+  return BytesIO(open("./susposter/sussy.png", "rb").read())
