4d747e4447c597fb8e1a11e62448494e26d29bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:26 +0000

    Add package firstimpression/0.12.3/firstimpression-0.12.3.tar.gz

---
+++ packages/firstimpression/firstimpression-0.12.3.tar.gz/firstimpression-0.12.3/firstimpression/constants.py
@@ -0,0 +1,33 @@
+from tempfile import gettempdir
+import mimetypes
+
+mimetypes.init()
+
+PICTURE_FORMATS = {
+    'fullscreen': "http://narrowcasting.fimpweb.nl/imageresize.php?width=1920&url=",
+    'square':     "http://narrowcasting.fimpweb.nl/imageresize.php?width=512&url=",
+}
+
+TEMP_FOLDER = gettempdir()
+LOCAL_INTEGRATED_FOLDER = 'C:/Users/Public/Documents/Scala/LocalIntegratedContent'
+
+APIS = [
+    '',
+    'BBC',
+    'NS',
+    'NU',
+    'TRAFFIC',
+    'INSTAGRAM',
+    'WEATHER',
+    'AMBER_ALERT',
+    'AMS_AIRPORT',
+    'CHUCK_NORRIS',
+    'EHV_AIRPORT',
+    'FACEBOOK',
+    'GOOGLE_TRENDS',
+    'INTERIA',
+    'SONGKICK'
+    ]
+
+VIDEO_EXTENSIONS = [ext for ext in mimetypes.types_map if mimetypes.types_map[ext].split('/')[0] == 'video']
+IMG_EXTENSIONS = [ext for ext in mimetypes.types_map if mimetypes.types_map[ext].split('/')[0] == 'image']
