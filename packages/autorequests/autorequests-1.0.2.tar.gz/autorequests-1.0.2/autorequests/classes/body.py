44f71a1eb1036cfd46c446f26cc727234fa4e31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:35 +0000

    Add package autorequests/1.0.2/autorequests-1.0.2.tar.gz

---
+++ packages/autorequests/autorequests-1.0.2.tar.gz/autorequests-1.0.2/autorequests/classes/body.py
@@ -0,0 +1,146 @@
+import json
+import urllib.parse
+from typing import Optional
+
+
+class Body:
+
+    def __init__(self, body: Optional[str]):
+        if body:
+            # parse escape sequences :thumbs_up:
+            # ignore/replace are kind of just guesses at what i think would be best
+            # if there is a more logical reason to use something else LMK!
+            body = body.encode(encoding="utf8", errors="ignore")\
+                       .decode(encoding="unicode_escape", errors="replace")
+            # replace line breaks with \n(s)
+            body = "\n".join(body.splitlines())
+        self.__body = body
+        self.__data = {}
+        self.__json = {}
+        self.__files = {}
+
+        # multipart is the most broad and obvious so it goes first
+        if not body:
+            pass
+        elif self.is_multipart_form_data:
+            self.__parse_multipart_form_data()
+        elif self.is_json:
+            self.__parse_json()
+        # urlencoded is the simplest (hardest to check) so it goes last
+        elif self.is_urlencoded:
+            self.__parse_urlencoded()
+
+    def __repr__(self):
+        base = "<Body"
+        if self.body:
+            if self.data:
+                base += f" data={self.data}"
+            if self.json:
+                base += f" json={self.json}"
+            if self.files:
+                base += f" files={self.files}"
+        if base == "<Body":
+            return "<Body data=None json=None files=None>"
+        return base + ">"
+
+    @property
+    def body(self):
+        return self.__body
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def json(self):
+        return self.__json
+
+    @property
+    def files(self):
+        return self.__files
+
+    @property
+    def is_json(self):
+        try:
+            json.loads(self.body)
+            return True
+        except json.JSONDecodeError:
+            return False
+
+    @property
+    def is_urlencoded(self):
+        if "=" not in self.body:
+            return False
+        for item in self.body.split("&"):
+            if item.count("=") <= 0:
+                return False
+        return True
+
+    @property
+    def is_multipart_form_data(self):
+        return "------WebKitFormBoundary" in self.body
+
+    def __parse_json(self):
+        # sometimes body is "null" but we want our json to be a dict and not None
+        json_ = json.loads(self.body)
+        if json_ is not None:
+            self.__json = json_
+
+    def __parse_urlencoded(self):
+        # urllib.parse.parse_qs doesn't work here btw
+        # if a key doesn't have a value then it gets excluded with parse_qs
+        # not sure if unquote_plus() would be better
+        body = urllib.parse.unquote(self.body)
+        for param in body.split("&"):
+            key, value = param.split("=", maxsplit=1)
+            self.__data[key] = value
+
+    def __parse_multipart_form_data(self):
+        try:
+            boundary, body = self.body.split("\n", maxsplit=1)
+            body = body.rstrip("--")
+        except ValueError:
+            return
+        for item in body.split(boundary):
+            # remove leading & trailing /n
+            if item.endswith("\n"):
+                item = item[:-1]
+            if item.startswith("\n"):
+                item = item[1:]
+            # get two main details
+            try:
+                details, content = item.split("\n\n")
+                details = details.lstrip(boundary + "\n")
+                details_dict = {k: v for k, v in
+                                (line.split(": ", maxsplit=1) for line in details.splitlines() if ": " in line)}
+            except ValueError:
+                # bad data that wasn't caught earlier
+                # this should never be reached, it's kind of just a safeguard in case the data is corrupted asf
+                continue
+            content_disposition = details_dict.get("Content-Disposition")
+            content_type = details_dict.get("Content-Type")
+            if not content_disposition:
+                continue
+            # get filename && name
+            content_disposition_dict = {}
+            for detail in content_disposition[11:].split("; "):
+                key, value = detail.split("=", maxsplit=1)
+                value = value[1:-1]
+                content_disposition_dict[key] = value
+            name = content_disposition_dict.get("name")
+            filename = content_disposition_dict.get("filename")
+
+            if not name:
+                pass
+            elif not filename:
+                self.__data[name] = content
+            # if it has a filename it's a file
+            # tuple of four items
+            # 1. filename
+            # 2. content
+            # 3. content-type
+            # 4. extra headers
+            elif content_type:
+                self.__files[name] = (filename, content, content_type)
+            else:
+                self.__files[name] = (filename, content)
---
+++ packages/autorequests/autorequests-1.0.2.tar.gz/autorequests-1.0.2/autorequests/classes/file.py
@@ -0,0 +1,131 @@
+import json
+from pathlib import Path
+from typing import Match
+
+from .. import regexp
+from ..utils import extract_cookies
+from ..classes import URL, Body, Method
+
+# Path() returns type WindowsPath or PosixPath based on os
+# I could replicate their os check, but this is safer in case they change it in the future.
+superclass = type(Path())
+
+
+class File(superclass):
+    """ handles files and the parsing of files """
+
+    def __new__(cls, *args):
+        return super().__new__(cls, *args)
+
+    def __init__(self, *args):
+        super().__init__()
+        # *args is needed but not used
+        self.__text = self.read_text(encoding="utf8", errors="ignore")
+        self.__method = self._compute_method()
+
+    @property
+    def text(self):
+        return self.__text
+
+    @property
+    def method(self):
+        return self.__method
+
+    def _compute_method(self):
+        fetch = self.fetch_match
+        if fetch:
+            return self._method_from_fetch(fetch)
+        powershell = self.powershell_match
+        if powershell:
+            return self._method_from_powershell(powershell)
+
+    @property
+    def fetch_match(self):
+        return regexp.fetch_regexp.search(self.text)
+
+    @property
+    def powershell_match(self):
+        return regexp.powershell_regexp.search(self.text)
+
+    # static methods
+    # (for parsing)
+
+    @staticmethod
+    def _method_from_fetch(fetch: Match) -> Method:
+        """
+        Parses a file that follows this format:
+
+        fetch(<URL>, {
+          "headers": <HEADERS>,
+          "referrer": <REFERRER>,  # optional
+          "referrerPolicy": <REFERRER-POLICY>,  # might be optional
+          "body": <BODY>,
+          "method": <METHOD>,
+          "mode": <MODE>
+        });
+        """
+        headers = json.loads(fetch["headers"])
+        # referer is spelled wrong in the HTTP header
+        # referrer policy is not
+        referrer = fetch["referrer"]
+        referrer_policy = fetch["referrer_policy"]
+        if referrer:
+            headers["referer"] = referrer
+        if referrer_policy:
+            headers["referrer-policy"] = referrer_policy
+
+        cookies = extract_cookies(headers)
+
+        method = fetch["method"]
+        url = URL(fetch["url"])
+        body = Body(fetch["body"])
+
+        return Method(method=method,
+                      url=url,
+                      body=body,
+                      headers=headers,
+                      cookies=cookies,
+                      )
+
+    @staticmethod
+    def _method_from_powershell(powershell: Match) -> Method:
+        """
+        Parses a file that follows this format:
+
+        Invoke-WebRequest -Uri <URL> `
+        -Method <METHOD> `   # optional; defaults to GET if not set
+        -Headers <HEADERS> `
+        -ContentType <CONTENT-TYPE> `   # optional; only exists w/ body
+        -Body <BODY>     # optional; only exists if a body is present
+        """
+        headers = {}
+
+        for header in powershell["headers"].splitlines():
+            if "=" in header:
+                header = header.lstrip("  ")
+                key, value = header.split("=", maxsplit=1)
+                # remove leading and trailing "s that always exist
+                key = key[1:-1]
+                value = value[1:-1]
+                headers[key] = value
+
+        cookies = extract_cookies(headers)
+
+        # ` is the escape character in powershell
+        # replace two `s with a singular `
+        # replace singular `s with nothing
+
+        raw_body = powershell["body"]
+        raw_body = raw_body.split("`")
+        raw_body = "".join((e if e != "" else "`") for e in raw_body)
+
+        method = powershell["method"] or "GET"
+        url = URL(powershell["url"])
+        body = Body(raw_body)
+
+        return Method(method=method,
+                      url=url,
+                      body=body,
+                      headers=headers,
+                      cookies=cookies,
+                      )
