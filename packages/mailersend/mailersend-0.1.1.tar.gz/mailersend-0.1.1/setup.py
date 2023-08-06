6af10733ad8345cc2f6b58d03e2772dfdf544ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:36 +0000

    Add package mailersend/0.1.1/mailersend-0.1.1.tar.gz

---
+++ packages/mailersend/mailersend-0.1.1.tar.gz/mailersend-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mailersend']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pre-commit>=2.12.1,<3.0.0', 'requests>=2.25.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'mailersend',
+    'version': '0.1.1',
+    'description': 'The official MailerSend Python SDK',
+    'long_description': None,
+    'author': 'Alex Orfanos',
+    'author_email': 'alexandros@mailerlite.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
