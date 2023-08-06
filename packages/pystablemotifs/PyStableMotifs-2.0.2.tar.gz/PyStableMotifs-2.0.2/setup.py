9d25ad18d1e7403ce16979233a262bf421053c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:46 +0000

    Add package pystablemotifs/2.0.2/PyStableMotifs-2.0.2.tar.gz

---
+++ packages/pystablemotifs/PyStableMotifs-2.0.2.tar.gz/PyStableMotifs-2.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="PyStableMotifs",
+    version='2.0.2',
+    author="Jordan Rozum",
+    author_email="jcr52@psu.edu",
+    description="Python package for analyzing Boolean Netowrk",
+    url="https://github.com/jcrozum/PyStableMotifs",
+    license='MIT',
+    python_requires='>=3.5',
+    packages=['PyStableMotifs'],
+    install_requires=[
+    "PyBoolNet >= 2.3.0",
+    "networkx >= 2.4.0",
+    "sympy >= 1.5.1",
+    "pandas >= 1.0.0",
+    "numpy >= 1.19.2",
+    "matplotlib >= 3.2.1"
+    ],
+    dependency_links=[
+    'PyBoolNet @ git+https://github.com/hklarner/PyBoolNet@=2.3.0'
+    ]
+)
