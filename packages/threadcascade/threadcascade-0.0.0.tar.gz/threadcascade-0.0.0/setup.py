055d3f9541f4f45a9556ceb8d564e68eb12a1d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:41 +0000

    Add package threadcascade/0.0.0/threadcascade-0.0.0.tar.gz

---
+++ packages/threadcascade/threadcascade-0.0.0.tar.gz/threadcascade-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='threadcascade',
+    packages=["threadcascade"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
