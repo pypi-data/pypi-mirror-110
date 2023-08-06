374e29bddebc2be4b466f5d49621f6e7ff0d2722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:12 +0000

    Add package tehom/0.0.0/tehom-0.0.0.tar.gz

---
+++ packages/tehom/tehom-0.0.0.tar.gz/tehom-0.0.0/README.md
@@ -0,0 +1,2 @@
+# Tehom - the primordial deep of chaos
+Machine Learning on Underwater Acoustics
---
+++ packages/tehom/tehom-0.0.0.tar.gz/tehom-0.0.0/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import versioneer
+
+
+setup(
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+)
