971e9707f676fcf2da1fb804108ea58963e04bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package pyhf/0.6.2/pyhf-0.6.2.tar.gz

---
+++ packages/pyhf/pyhf-0.6.2.tar.gz/pyhf-0.6.2/src/pyhf/cli/__init__.py
@@ -0,0 +1,13 @@
+"""The pyhf command line interface."""
+from .cli import pyhf as cli
+from .rootio import cli as rootio
+from .spec import cli as spec
+from .infer import cli as infer
+from .complete import cli as complete
+from ..contrib import cli as contrib
+
+__all__ = ['cli', 'rootio', 'spec', 'infer', 'complete', 'contrib']
+
+
+def __dir__():
+    return __all__
