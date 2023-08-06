bf20325ae71e808db1a4fc471c924bf1f7d39302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:36 +0000

    Add package qaeval/0.0.9/qaeval-0.0.9.tar.gz

---
+++ packages/qaeval/qaeval-0.0.9.tar.gz/qaeval-0.0.9/qaeval/__init__.py
@@ -0,0 +1,4 @@
+from qaeval.answer_selection import AnswerSelector
+from qaeval.answering.model import QuestionAnsweringModel
+from qaeval.generation.model import QuestionGenerationModel
+from qaeval.version import VERSION as __version__
