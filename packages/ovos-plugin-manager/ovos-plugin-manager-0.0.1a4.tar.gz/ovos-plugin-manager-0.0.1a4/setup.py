5731a3046814e6732fd2a2cfa529b73173666c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:09 +0000

    Add package ovos-plugin-manager/0.0.1a4/ovos-plugin-manager-0.0.1a4.tar.gz

---
+++ packages/ovos-plugin-manager/ovos-plugin-manager-0.0.1a4.tar.gz/ovos-plugin-manager-0.0.1a4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ovos-plugin-manager',
+    version='0.0.1a4',
+    packages=['ovos_plugin_manager', 'ovos_plugin_manager.templates'],
+    url='https://github.com/OpenVoiceOS/OVOS-plugin-manager',
+    license='Apache-2.0',
+    author='jarbasAi',
+    install_requires=["ovos_utils>=0.0.7", "requests"],
+    author_email='jarbasai@mailfence.com',
+    description='OpenVoiceOS plugin manager'
+)
