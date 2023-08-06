c8938a217c6fbd4bace95da64dc6fdca2e513edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:05 +0000

    Add package Python-Peatio/1.4.0/Python_Peatio-1.4.0-py3-none-any.whl

---
+++ packages/Python-Peatio/Python_Peatio-1.4.0-py3-none-any.whl/Python_Peatio/Accounts.py
@@ -0,0 +1,30 @@
+import hmac
+import hashlib 
+import binascii
+import time
+import json
+import http.client
+from Python_Peatio.Auth import Auth
+
+class Accounts(Auth):
+
+    def account_balances_currency(self, URL : str, currency : str)  -> dict:
+        "Get list of user accounts balances of selected currency"
+        payload = {}
+        header = Auth.header(self)
+        conn = http.client.HTTPConnection(URL)
+        conn.request("GET", "/api/v2/peatio/account/balances/{currency}".format(currency = currency), payload, header)
+        currency_balance = json.loads(conn.getresponse().read().decode("utf-8"))
+
+        return currency_balance
+
+
+    def all_currency_balance(self, URL : str)  -> list:
+        "Get list of user accounts balances of all currency"
+        payload = {}
+        header = Auth.header(self)
+        conn = http.client.HTTPConnection(URL)
+        conn.request("GET", "/api/v2/peatio/account/balances", payload, header)
+        all_currency_balance = json.loads(conn.getresponse().read().decode("utf-8"))
+
+        return all_currency_balance
