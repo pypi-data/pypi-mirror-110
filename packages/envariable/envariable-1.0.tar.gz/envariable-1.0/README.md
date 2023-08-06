6dca2f65582f0c6943bd5335b5766f335318f508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:59 +0000

    Add package envariable/1.0/envariable-1.0.tar.gz

---
+++ packages/envariable/envariable-1.0.tar.gz/envariable-1.0/README.md
@@ -0,0 +1,30 @@
+
+Change System Environment Variable permanently
+
+## Example
+```python
+from envarible import setenv, unsetenv, getenv, getenvall
+# The variable TEST is stored permanently with the VALUE value
+setenv('TEST', 'VALUE') 
+
+print(getenv('TEST'))
+>>> VALUE
+
+# The TEST variable is permanently deleted
+unsetenv('TEST')
+
+
+print(getenv('TEST'))
+>>> None
+
+# returned all Environment Variable
+all_env = getenvall()
+```
+
+## Example 2
+```python
+from envariable import setenv, getenv
+
+path = getenv('PATH')
+setenv('PATH', f'{path}:/my/dir')
+```
---
+++ packages/envariable/envariable-1.0.tar.gz/envariable-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, version
+
+
+setup(
+    name='envariable',
+    packages=['envariable'],
+    version='1.0',
+    license='MIT',
+    description='Change System Environment Variable permanently',
+    author='Arshia Ihammi',
+    author_email='arshiaihammi@gmail.com',
+    url='https://github.com/blueboy-tm/python-envariable/',
+    download_url='https://github.com/blueboy-tm/python-envariable/blob/main/dist/envariable-1.0.tar.gz',
+    keywords=['env', 'variable', 'environment', 'os', 'system', 'Environment Variable', 'environ'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: System',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only'
+    ]
+)
