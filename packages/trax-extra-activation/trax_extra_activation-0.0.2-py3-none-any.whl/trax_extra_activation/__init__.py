6f94fc4e8cc3ac21cfd751e84a9f46cf85f7267a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:32 +0000

    Add package trax-extra-activation/0.0.2/trax_extra_activation-0.0.2-py3-none-any.whl

---
+++ packages/trax-extra-activation/trax_extra_activation-0.0.2-py3-none-any.whl/trax_extra_activation/__init__.py
@@ -0,0 +1,27 @@
+from trax_extra_activation.fns import (
+    Celu,
+    HardShrink,
+    HardSwish,
+    LogSigmoid,
+    Mish,
+    Relu6,
+    Silu,
+    SoftShrink,
+    SoftSign,
+    TanhExp,
+    TanhShrink,
+)
+
+__all__ = [
+    "HardShrink",
+    "HardSwish",
+    "LogSigmoid",
+    "Relu6",
+    "Celu",
+    "Silu",
+    "Mish",
+    "SoftShrink",
+    "SoftSign",
+    "TanhShrink",
+    "TanhExp",
+]
