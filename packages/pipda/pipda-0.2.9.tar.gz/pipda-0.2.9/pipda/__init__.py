3c4c4a33f8d4d37a1cec21015ef9fd8c627b5aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:06 +0000

    Add package pipda/0.2.9/pipda-0.2.9.tar.gz

---
+++ packages/pipda/pipda-0.2.9.tar.gz/pipda-0.2.9/pipda/__init__.py
@@ -0,0 +1,13 @@
+"""A framework for data piping in python"""
+# pylint: disable=unused-import
+from .symbolic import Symbolic
+from .verb import register_verb, register_piping_sign
+from .function import register_func
+from .utils import (
+    evaluate_args, evaluate_kwargs, evaluate_expr,
+    DataEnv, functype, unregister, logger
+)
+from .context import Context, ContextBase
+from .operator import Operator, register_operator
+
+__version__ = '0.2.9'
