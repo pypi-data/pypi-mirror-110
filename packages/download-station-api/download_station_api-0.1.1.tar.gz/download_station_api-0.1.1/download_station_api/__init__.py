e469f98a162ae2d518635093c3ba0f2ddea42d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:42 +0000

    Add package download-station-api/0.1.1/download_station_api-0.1.1.tar.gz

---
+++ packages/download-station-api/download_station_api-0.1.1.tar.gz/download_station_api-0.1.1/download_station_api/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Download Station API."""
+
+__author__ = """Paul Armstrong"""
+__email__ = 'paul.armstrong211@gmail.com'
+__version__ = '0.1.1'
+
+from . import download_station_api
