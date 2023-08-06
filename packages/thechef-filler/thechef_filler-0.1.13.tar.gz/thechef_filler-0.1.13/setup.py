fe81d1252c0ce6e0e1e0df619ea372dcd6520da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:08 +0000

    Add package thechef-filler/0.1.13/thechef_filler-0.1.13.tar.gz

---
+++ packages/thechef-filler/thechef_filler-0.1.13.tar.gz/thechef_filler-0.1.13/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['thechef_filler', 'thechef_filler.src']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['thechef_filler = thechef_filler.cli:main']}
+
+setup_kwargs = {
+    'name': 'thechef-filler',
+    'version': '0.1.13',
+    'description': '',
+    'long_description': None,
+    'author': 'thechef',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
