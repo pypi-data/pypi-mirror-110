a193b7c885b4ed67bd3c7fbf696ced57ba15defb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package Bakle-Helpers/0.1.1/Bakle%20Helpers-0.1.1.tar.gz

---
+++ packages/Bakle-Helpers/Bakle%20Helpers-0.1.1.tar.gz/Bakle Helpers-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+    file.close()
+
+setup(
+    name='Bakle Helpers',
+    version='0.1.1',
+    description='Package that provides multiple helpers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Bashir Akle',
+    author_email='bashir.akle@gmail.com',
+    url='https://github.com/bakle/Py-Validator',
+    packages=['bakle_helpers'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'flake8'],
+    package_data={'bakle_helpers': ['support/*.txt']}
+)
