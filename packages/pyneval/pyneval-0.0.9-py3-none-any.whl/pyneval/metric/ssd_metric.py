fc8ef69b251285072eac428dfb9e75eeb635ba0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:19 +0000

    Add package pyneval/0.0.9/pyneval-0.0.9-py3-none-any.whl

---
+++ packages/pyneval/pyneval-0.0.9-py3-none-any.whl/pyneval/metric/ssd_metric.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""a geometry metric method, SSD metric
+
+This module implement a geometry metric method called Substantial Spatial Distance.
+relative paper: https://doi.org/10.1038/nbt.1612
+title, auther
+
+Example:
+    In command line:
+    $ pyneval --gold ./data\\test_data\\ssd_data\\gold\\c.swc
+              --test .\\data\\test_data\\ssd_data\\test\\c.swc
+              --metric ssd_metric
+Attributes:
+    None
+
+Todos:
+    None
+"""
+import sys
+
+import jsonschema
+
+from pyneval.io import swc_writer
+from pyneval.model import swc_node
+from pyneval.io import read_json
+from pyneval.tools import re_sample
+from pyneval.metric.utils import point_match_utils
+from pyneval.io import read_config
+
+
+def get_mse(src_tree, tar_tree, ssd_threshold=2.0, mode=1):
+    """ calculate the minimum square error of two trees
+    find the closest node on the tar_tree for each node on the src tree, calculate the average
+    distance of these node pairs.
+
+    Args:
+        src_tree(SwcTree):
+        tar_tree(SwcTree):
+        ssd_threshold(float): distance will be count into the res
+            only if two nodes' distance is larger than this threshold.
+        mode(1 or 2):
+            1 means static threshold, equal to ssd_threshold.
+            2 means dynamic threshold, equal to ssd_threshold * src_node.threshold
+
+    Returns:
+        dis(float): average minimum distance of node, distance must be larger than ssd_threshold
+        num(float): The number of pairs of nodes that are counted when calculating distance
+
+    Raise:
+        None
+    """
+    dis, num = 0, 0
+    kdtree, pos_node_dict = point_match_utils.create_kdtree(tar_tree.get_node_list())
+    for node in src_tree.get_node_list():
+        if node.is_virtual():
+            continue
+        target_pos = kdtree.search_knn(list(node.get_center_as_tuple()), k=1)[0]
+        target_node = pos_node_dict[tuple(target_pos[0].data)]
+
+        cur_dis = target_node.distance(node)
+
+        if mode == 1:
+            threshold = ssd_threshold
+        else:
+            threshold = ssd_threshold * node.radius()
+
+        if cur_dis >= threshold:
+            node._type = 9
+            dis += cur_dis
+            num += 1
+    try:
+        dis /= num
+    except ZeroDivisionError:
+        dis = num = 0
+    return dis, num
+
+
+def ssd_metric(gold_swc_tree: swc_node.SwcTree, test_swc_tree: swc_node.SwcTree, config: dict):
+    """Main function of SSD metric.
+    Args:
+        gold_swc_tree(SwcTree):
+        test_swc_tree(SwcTree):
+        config(Dict):
+            The keys of 'config' is the name of configs, and the items are config values
+    Example:
+        test_tree = swc_node.SwcTree()
+        gold_tree = swc_node.SwcTree()
+        gold_tree.load("..\\..\\data\\test_data\\ssd_data\\gold\\c.swc")
+        test_tree.load("..\\..\\data\\test_data\\ssd_data\\test\\c.swc")
+        score, recall, precision = ssd_metric(gold_swc_tree=gold_tree,
+                                              test_swc_tree=test_tree,
+                                              config=config)
+    Returns:
+        tuple: contain three values to demonstrate metric result
+            avg_score(float): average distance of nodes between gold and test swc trees.
+            precision(float): percentage of nodes that are matched compared to test tree
+            recall(float): percentage of nodes that are matched compared to gold tree
+
+    Raises:
+        None
+    """
+    debug = config["debug"]
+    threshold_mode = config["threshold_mode"]
+    ssd_threshold = config["ssd_threshold"]
+    up_sample_threshold = config["up_sample_threshold"]
+    scale = config["scale"]
+
+    gold_swc_tree.rescale(scale)
+    test_swc_tree.rescale(scale)
+    u_gold_swc_tree = re_sample.up_sample_swc_tree(swc_tree=gold_swc_tree,
+                                                   length_threshold=up_sample_threshold)
+    u_test_swc_tree = re_sample.up_sample_swc_tree(swc_tree=test_swc_tree,
+                                                   length_threshold=up_sample_threshold)
+    u_gold_swc_tree.set_node_type_by_topo(root_id=1)
+    u_test_swc_tree.set_node_type_by_topo(root_id=5)
+
+    g2t_score, g2t_num = get_mse(src_tree=u_gold_swc_tree, tar_tree=u_test_swc_tree,
+                                 ssd_threshold=ssd_threshold, mode=threshold_mode)
+    t2g_score, t2g_num = get_mse(src_tree=u_test_swc_tree, tar_tree=u_gold_swc_tree,
+                                 ssd_threshold=ssd_threshold, mode=threshold_mode)
+
+    if debug:
+        print("recall_num = {}, pre_num = {}, gold_tot_num = {}, test_tot_num = {} {} {}".format(
+            g2t_num, t2g_num, u_gold_swc_tree.size(), u_test_swc_tree.size(), gold_swc_tree.length(), test_swc_tree.length()
+        ))
+
+    res = {
+        "avg_score": (g2t_score + t2g_score) / 2,
+        "recall": 1 - g2t_num/u_gold_swc_tree.size(),
+        "precision": 1 - t2g_num/u_test_swc_tree.size()
+    }
+
+    return res, u_gold_swc_tree, u_test_swc_tree
+
+
+if __name__ == "__main__":
+    test_tree = swc_node.SwcTree()
+    gold_tree = swc_node.SwcTree()
+
+    sys.setrecursionlimit(10000000)
+    gold_tree.load("../../data/test_data/geo_metric_data/gold_fake_data1.swc")
+    test_tree.load("../../data/test_data/geo_metric_data/test_fake_data1.swc")
+
+    from pyneval.metric.utils import config_utils
+    config = config_utils.get_default_configs("ssd_metric")
+    config_schema = config_utils.get_config_schema("ssd_metric")
+
+    try:
+        jsonschema.validate(config, config_schema)
+    except Exception as e:
+        raise Exception("[Error: ]Error in analyzing config json file")
+    config["detail_path"] = "..//..//output//ssd_output//ssd_detail.swc"
+
+    ssd_res = ssd_metric(gold_swc_tree=gold_tree,
+                         test_swc_tree=test_tree,
+                         config=config)
+
+    print("ssd score = {}\n"
+          "recall    = {}%\n"
+          "precision = {}%".
+          format(round(ssd_res["avg_score"], 2),
+                 round(ssd_res["recall"]*100, 2),
+                 round(ssd_res["precision"]*100, 2)))
+
---
+++ packages/pyneval/pyneval-0.0.9-py3-none-any.whl/pyneval/tools/optimize/optimize.py
@@ -0,0 +1,119 @@
+import os
+
+from pyneval.tools.optimize.SA import SAFast
+from pyneval.metric import ssd_metric
+from pyneval.metric.utils import config_utils
+from pyneval.model import swc_node
+from pyneval.io import read_json
+from scipy import stats as st
+
+import copy
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+g_score = -1
+g_gold_tree = None
+g_tar_tree = None
+g_rcn_method = None
+g_rcn_config = None
+g_metric_method = None
+g_metric_configs = None
+
+# Gold file and Test tif need to placed in ../../../data/optimation
+# and named after FILE_ID_test.tif, FILE_ID_gold.swc
+FILE_ID = "test1"
+NEUTU_PATH = "neutu"
+ORIGIN_PATH = "../../../data/optimation/{}/{}_test.tif".format(FILE_ID, FILE_ID)
+GOLD_PATH = "../../../data/optimation/{}/{}_gold.swc".format(FILE_ID, FILE_ID)
+LOG_PATH = "../../../output/optimization/neutu_log.txt"
+METRIC = "ssd_metric"
+# specific test name is given in SA.py
+TEST_PATH = "../../../data/optimation/output/"
+CONFIG_PATH = "../../../config/fake_reconstruction_configs/"
+
+
+def SA_optimize(configs=None, test_name=None, lock=None):
+    global g_metric_method
+    global g_metric_configs
+    global g_rcn_config
+    # identify specific TEST output path and CONFIG input PATH
+    LOC_TEST_PATH = os.path.join(TEST_PATH, test_name+"_test.swc")
+    rec_config = copy.deepcopy(g_rcn_config)
+
+    if configs is not None:
+        LOC_CONFIG_PATH = os.path.join(CONFIG_PATH, test_name+".json")
+        rec_config["trace"]["default"]["minimalScoreAuto"] = configs[0]
+        rec_config["trace"]["default"]["minimalScoreSeed"] = configs[1]
+
+        read_json.save_json(LOC_CONFIG_PATH, rec_config)
+    else:
+        LOC_CONFIG_PATH = os.path.join(CONFIG_PATH, "best_x_{}.json".format(test_name))
+
+    REC_CMD = "{} --command --trace {} -o {} --config {} > {}".format(
+        NEUTU_PATH, ORIGIN_PATH, LOC_TEST_PATH, LOC_CONFIG_PATH, LOG_PATH
+    )
+    try:
+        os.system(REC_CMD)
+    except:
+        raise Exception("[Error: ] error executing reconstruction")
+
+    res_tree = swc_node.SwcTree()
+    gold_tree = swc_node.SwcTree()
+    res_tree.load(LOC_TEST_PATH)
+    gold_tree.load(GOLD_PATH)
+
+    if lock is not None:
+        lock.acquire()
+    try:
+        main_score = g_metric_method(gold_tree, res_tree, g_metric_configs)
+    finally:
+        if lock is not None:
+            lock.release()
+    score = (main_score["recall"] + main_score["precision"])/2
+    print("[Info: ] ssd loss = {}".format(score))
+
+    return configs, -score
+
+
+def main():
+    global g_metric_configs
+    global g_metric_method
+    global g_rcn_config
+    g_metric_method = ssd_metric.ssd_metric
+    g_metric_configs = config_utils.get_default_configs(METRIC)
+    g_rcn_config = read_json.read_json(os.path.join(CONFIG_PATH, "default.json"))
+
+    # optimize with SA
+    # configs here is the config of the reconstruction
+    configs = (0.3, 0.35)
+    start = time.time()
+    sa_fast = SAFast(func=SA_optimize,
+                     x0=configs, T_max=0.01, T_min=1e-5, q=0.96, L=25, max_stay_counter=15, upper=1, lower=0)
+    best_configs, best_value = sa_fast.run()
+    print("[Info: ]best configs:\n"
+          "        origin minimalScoreAuto = {}\n"
+          "        minimalScoreSeed = {}\n"
+          "        best value = {}\n"
+          "        time = {}\n" .format(
+        best_configs[0], best_configs[1], best_value, time.time() - start
+    ))
+    # save best json file
+    g_rcn_config["trace"]["default"]["minimalScoreAuto"] = best_configs[0]
+    g_rcn_config["trace"]["default"]["minimalScoreSeed"] = best_configs[1]
+    read_json.save_json(os.path.join(CONFIG_PATH, "best_x_{}.json".format(FILE_ID)), g_rcn_config)
+    # get and save best reconstruct swc
+    print("[Info: ]Exam test score with best configs: ")
+    cfg, score = SA_optimize(test_name = FILE_ID)
+    # plot the result.
+    plt.plot(pd.DataFrame(sa_fast.best_y_history).cummin(axis=0))
+    plt.xlabel("iterations")
+    plt.ylabel("score(opposite value)")
+    plt.show()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
