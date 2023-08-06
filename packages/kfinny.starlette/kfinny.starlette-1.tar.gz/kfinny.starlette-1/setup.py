fe398ad788ea2e12b61ae71b25d64e75d37bf16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:27 +0000

    Add package kfinny.starlette/1/kfinny.starlette-1.tar.gz

---
+++ packages/kfinny.starlette/kfinny.starlette-1.tar.gz/kfinny.starlette-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='kfinny.starlette',
+      version='1',
+      description='A pip-installable of starlette-example',
+      url='https://github.com/kfinny/starlette-pip-example',
+      author='Kevin Finnigin',
+      author_email='kevin@finnigin.net',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
