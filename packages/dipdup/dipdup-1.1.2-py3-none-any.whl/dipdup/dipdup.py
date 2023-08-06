4004c1fd42bfd67081b2e9665cecd0f9d988c227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:38 +0000

    Add package dipdup/1.1.2/dipdup-1.1.2-py3-none-any.whl

---
+++ packages/dipdup/dipdup-1.1.2-py3-none-any.whl/dipdup/dipdup.py
@@ -0,0 +1,300 @@
+import asyncio
+import hashlib
+import logging
+from os.path import join
+from posix import listdir
+from typing import Dict, List, cast
+
+from genericpath import exists
+from tortoise import Tortoise
+from tortoise.exceptions import OperationalError
+from tortoise.transactions import get_connection
+from tortoise.utils import get_schema_sql
+
+import dipdup.utils as utils
+from dipdup.codegen import DipDupCodeGenerator
+from dipdup.config import (
+    ROLLBACK_HANDLER,
+    BcdDatasourceConfig,
+    BigMapIndexConfig,
+    DatasourceConfigT,
+    DipDupConfig,
+    IndexConfigTemplateT,
+    OperationIndexConfig,
+    PostgresDatabaseConfig,
+    StaticTemplateConfig,
+    TzktDatasourceConfig,
+)
+from dipdup.context import DipDupContext, RollbackHandlerContext
+from dipdup.datasources import DatasourceT
+from dipdup.datasources.bcd.datasource import BcdDatasource
+from dipdup.datasources.datasource import IndexDatasource
+from dipdup.datasources.tzkt.datasource import TzktDatasource
+from dipdup.exceptions import ConfigurationError, HandlerImportError
+from dipdup.hasura import configure_hasura
+from dipdup.index import BigMapIndex, Index, OperationIndex
+from dipdup.models import BigMapData, IndexType, OperationData, State
+
+
+class IndexDispatcher:
+    def __init__(self, ctx: DipDupContext) -> None:
+        self._ctx = ctx
+
+        self._logger = logging.getLogger(__name__)
+        self._indexes: Dict[str, Index] = {}
+
+    async def add_index(self, index_config: IndexConfigTemplateT) -> None:
+        if index_config.name in self._indexes:
+            return
+        self._logger.info('Adding index `%s` to dispatcher', index_config.name)
+        if isinstance(index_config, OperationIndexConfig):
+            datasource_name = cast(TzktDatasourceConfig, index_config.datasource).name
+            datasource = self._ctx.datasources[datasource_name]
+            if not isinstance(datasource, TzktDatasource):
+                raise RuntimeError
+            operation_index = OperationIndex(self._ctx, index_config, datasource)
+            self._indexes[index_config.name] = operation_index
+            await datasource.add_index(index_config)
+
+        elif isinstance(index_config, BigMapIndexConfig):
+            datasource_name = cast(TzktDatasourceConfig, index_config.datasource).name
+            datasource = self._ctx.datasources[datasource_name]
+            if not isinstance(datasource, TzktDatasource):
+                raise RuntimeError
+            big_map_index = BigMapIndex(self._ctx, index_config, datasource)
+            self._indexes[index_config.name] = big_map_index
+            await datasource.add_index(index_config)
+
+        else:
+            raise NotImplementedError
+
+    async def reload_config(self) -> None:
+        if not self._ctx.updated:
+            return
+
+        self._logger.info('Reloading config')
+        self._ctx.config.initialize()
+
+        for index_config in self._ctx.config.indexes.values():
+            if isinstance(index_config, StaticTemplateConfig):
+                raise RuntimeError
+            await self.add_index(index_config)
+
+        self._ctx.reset()
+
+    async def dispatch_operations(self, datasource: TzktDatasource, operations: List[OperationData]) -> None:
+        assert len(set(op.level for op in operations)) == 1
+        level = operations[0].level
+        for index in self._indexes.values():
+            if isinstance(index, OperationIndex) and index.datasource == datasource:
+                index.push(level, operations)
+
+    async def dispatch_big_maps(self, datasource: TzktDatasource, big_maps: List[BigMapData]) -> None:
+        assert len(set(op.level for op in big_maps)) == 1
+        level = big_maps[0].level
+        for index in self._indexes.values():
+            if isinstance(index, BigMapIndex) and index.datasource == datasource:
+                index.push(level, big_maps)
+
+    async def _rollback(self, datasource: TzktDatasource, from_level: int, to_level: int) -> None:
+        logger = utils.FormattedLogger(ROLLBACK_HANDLER)
+        rollback_fn = self._ctx.config.get_rollback_fn()
+        ctx = RollbackHandlerContext(
+            config=self._ctx.config,
+            datasources=self._ctx.datasources,
+            logger=logger,
+            datasource=datasource,
+            from_level=from_level,
+            to_level=to_level,
+        )
+        await rollback_fn(ctx)
+
+    async def run(self, oneshot=False) -> None:
+        self._logger.info('Starting index dispatcher')
+        for datasource in self._ctx.datasources.values():
+            if not isinstance(datasource, IndexDatasource):
+                continue
+            datasource.on_operations(self.dispatch_operations)
+            datasource.on_big_maps(self.dispatch_big_maps)
+            datasource.on_rollback(self._rollback)
+
+        self._ctx.commit()
+
+        while True:
+            await self.reload_config()
+
+            # FIXME: Process all indexes in parallel, blocked by https://github.com/tortoise/tortoise-orm/issues/792
+            async with utils.slowdown(1):
+                for index in self._indexes.values():
+                    await index.process()
+
+            # TODO: Continue if new indexes are spawned from origination
+            if oneshot:
+                break
+
+
+class DipDup:
+    """Main indexer class.
+
+    Spawns datasources, registers indexes, passes handler callbacks to executor"""
+
+    def __init__(self, config: DipDupConfig) -> None:
+        self._logger = logging.getLogger(__name__)
+        self._config = config
+        self._datasources: Dict[str, DatasourceT] = {}
+        self._datasources_by_config: Dict[DatasourceConfigT, DatasourceT] = {}
+        self._ctx = DipDupContext(
+            config=self._config,
+            datasources=self._datasources,
+        )
+        self._index_dispatcher = IndexDispatcher(self._ctx)
+
+    async def init(self) -> None:
+        """Create new or update existing dipdup project"""
+        await self._create_datasources()
+
+        codegen = DipDupCodeGenerator(self._config, self._datasources_by_config)
+        await codegen.create_package()
+        await codegen.fetch_schemas()
+        await codegen.generate_types()
+        await codegen.generate_default_handlers()
+        await codegen.generate_user_handlers()
+        await codegen.cleanup()
+
+        for datasource in self._datasources.values():
+            await datasource.close_session()
+
+    async def run(self, reindex: bool, oneshot: bool) -> None:
+        """Main entrypoint"""
+
+        url = self._config.database.connection_string
+        models = f'{self._config.package}.models'
+
+        async with utils.tortoise_wrapper(url, models):
+            await self._initialize_database(reindex)
+            await self._create_datasources()
+            await self._configure()
+
+            self._logger.info('Starting datasources')
+            datasource_tasks = [] if oneshot else [asyncio.create_task(d.run()) for d in self._datasources.values()]
+            worker_tasks = []
+
+            if self._config.hasura:
+                worker_tasks.append(asyncio.create_task(configure_hasura(self._config)))
+
+            worker_tasks.append(asyncio.create_task(self._index_dispatcher.run(oneshot)))
+
+            try:
+                await asyncio.gather(*datasource_tasks, *worker_tasks)
+            except KeyboardInterrupt:
+                pass
+            finally:
+                self._logger.info('Closing datasource sessions')
+                await asyncio.gather(*[d.close_session() for d in self._datasources.values()])
+
+    async def migrate(self) -> None:
+        codegen = DipDupCodeGenerator(self._config, self._datasources_by_config)
+        await codegen.generate_default_handlers(recreate=True)
+        await codegen.migrate_user_handlers_to_v1()
+        self._logger.warning('==================== WARNING =====================')
+        self._logger.warning('Your handlers have just been migrated to v1.0.0 format.')
+        self._logger.warning('Review and commit changes before proceeding.')
+        self._logger.warning('==================== WARNING =====================')
+        quit()
+
+    async def _configure(self) -> None:
+        """Run user-defined initial configuration handler"""
+        try:
+            configure_fn = self._config.get_configure_fn()
+        except HandlerImportError:
+            await self.migrate()
+        await configure_fn(self._ctx)
+        self._config.initialize()
+
+    async def _create_datasources(self) -> None:
+        datasource: DatasourceT
+        for name, datasource_config in self._config.datasources.items():
+            if name in self._datasources:
+                continue
+
+            if isinstance(datasource_config, TzktDatasourceConfig):
+                datasource = TzktDatasource(
+                    url=datasource_config.url,
+                    cache=self._config.cache_enabled,
+                )
+                self._datasources[name] = datasource
+                self._datasources_by_config[datasource_config] = datasource
+
+            elif isinstance(datasource_config, BcdDatasourceConfig):
+                datasource = BcdDatasource(
+                    datasource_config.url,
+                    datasource_config.network,
+                    self._config.cache_enabled,
+                )
+                self._datasources[name] = datasource
+                self._datasources_by_config[datasource_config] = datasource
+            else:
+                raise NotImplementedError
+
+    async def _initialize_database(self, reindex: bool = False) -> None:
+        self._logger.info('Initializing database')
+
+        if isinstance(self._config.database, PostgresDatabaseConfig) and self._config.database.schema_name:
+            await Tortoise._connections['default'].execute_script(f"CREATE SCHEMA IF NOT EXISTS {self._config.database.schema_name}")
+            await Tortoise._connections['default'].execute_script(f"SET search_path TO {self._config.database.schema_name}")
+
+        connection_name, connection = next(iter(Tortoise._connections.items()))
+        schema_sql = get_schema_sql(connection, False)
+
+        # NOTE: Column order could differ in two generated schemas for the same models, drop commas and sort strings to eliminate this
+        processed_schema_sql = '\n'.join(sorted(schema_sql.replace(',', '').split('\n'))).encode()
+        schema_hash = hashlib.sha256(processed_schema_sql).hexdigest()
+
+        # TODO: Move higher
+        if reindex:
+            self._logger.warning('Started with `--reindex` argument, reindexing')
+            await self._ctx.reindex()
+
+        try:
+            schema_state = await State.get_or_none(index_type=IndexType.schema, index_name=connection_name)
+        except OperationalError:
+            schema_state = None
+
+        if schema_state is None:
+            await Tortoise.generate_schemas()
+            await self._execute_sql_scripts(reindex=True)
+
+            schema_state = State(index_type=IndexType.schema, index_name=connection_name, hash=schema_hash)
+            await schema_state.save()
+        elif schema_state.hash != schema_hash:
+            self._logger.warning('Schema hash mismatch, reindexing')
+            await self._ctx.reindex()
+
+        await self._execute_sql_scripts(reindex=False)
+
+    async def _execute_sql_scripts(self, reindex: bool) -> None:
+        """Execute SQL included with project"""
+        sql_path = join(self._config.package_path, 'sql')
+        if not exists(sql_path):
+            return
+        if any(map(lambda p: p not in ('on_reindex', 'on_restart'), listdir(sql_path))):
+            raise ConfigurationError(
+                f'SQL scripts must be placed either to `{self._config.package}/sql/on_restart` or to `{self._config.package}/sql/on_reindex` directory'
+            )
+        if not isinstance(self._config.database, PostgresDatabaseConfig):
+            self._logger.warning('Execution of user SQL scripts is supported on PostgreSQL only, skipping')
+            return
+
+        sql_path = join(sql_path, 'on_reindex' if reindex else 'on_restart')
+        if not exists(sql_path):
+            return
+        self._logger.info('Executing SQL scripts from `%s`', sql_path)
+        for filename in sorted(listdir(sql_path)):
+            if not filename.endswith('.sql'):
+                continue
+
+            with open(join(sql_path, filename)) as file:
+                sql = file.read()
+
+            self._logger.info('Executing `%s`', filename)
+            await get_connection(None).execute_script(sql)
---
+++ packages/dipdup/dipdup-1.1.2-py3-none-any.whl/dipdup/models.py
@@ -0,0 +1,205 @@
+import logging
+from copy import deepcopy
+from datetime import datetime
+from enum import Enum
+from typing import Any, Dict, Generic, List, Optional, Type, TypeVar
+
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
+from tortoise import Model, fields
+
+from dipdup.exceptions import ConfigurationError
+
+ParameterType = TypeVar('ParameterType', bound=BaseModel)
+StorageType = TypeVar('StorageType', bound=BaseModel)
+KeyType = TypeVar('KeyType', bound=BaseModel)
+ValueType = TypeVar('ValueType', bound=BaseModel)
+
+
+_logger = logging.getLogger(__name__)
+
+
+class IndexType(Enum):
+    operation = 'operation'
+    big_map = 'big_map'
+    block = 'block'
+    schema = 'schema'
+
+
+class State(Model):
+    """Stores current level of index and hash of it's config"""
+
+    index_name = fields.CharField(256)
+    index_type = fields.CharEnumField(IndexType)
+    hash = fields.CharField(256)
+    level = fields.IntField(default=0)
+
+    class Meta:
+        table = 'dipdup_state'
+
+
+class TemporaryState(State):
+    """Used within stateless indexes, skip saving to DB"""
+
+    async def save(self, using_db=None, update_fields=None, force_create=False, force_update=False) -> None:
+        pass
+
+    class Meta:
+        abstract = True
+
+
+@dataclass
+class OperationData:
+    """Basic structure for operations from TzKT response"""
+
+    type: str
+    id: int
+    level: int
+    timestamp: datetime
+    hash: str
+    counter: int
+    sender_address: str
+    target_address: Optional[str]
+    amount: Optional[int]
+    status: str
+    has_internals: Optional[bool]
+    storage: Dict[str, Any]
+    block: Optional[str] = None
+    sender_alias: Optional[str] = None
+    nonce: Optional[int] = None
+    target_alias: Optional[str] = None
+    entrypoint: Optional[str] = None
+    parameter_json: Optional[Any] = None
+    originated_contract_address: Optional[str] = None
+    originated_contract_type_hash: Optional[int] = None
+    originated_contract_code_hash: Optional[int] = None
+    diffs: Optional[List[Dict[str, Any]]] = None
+
+    def _merge_bigmapdiffs(self, storage_dict: Dict[str, Any], bigmap_name: str, array: bool) -> None:
+        """Apply big map diffs of specific path to storage"""
+        if self.diffs is None:
+            raise Exception('`bigmaps` field missing')
+        _logger.debug(bigmap_name)
+        bigmapdiffs = [bm for bm in self.diffs if bm['path'] == bigmap_name]
+        bigmap_key = bigmap_name.split('.')[-1]
+        for diff in bigmapdiffs:
+            _logger.debug('Applying bigmapdiff: %s', diff)
+            if diff['action'] in ('add_key', 'update_key'):
+                key = diff['content']['key']
+                if array is True:
+                    storage_dict[bigmap_key].append({'key': key, 'value': diff['content']['value']})
+                else:
+                    storage_dict[bigmap_key][key] = diff['content']['value']
+
+    def _process_storage(
+        self,
+        storage_type: Type[StorageType],
+        storage: Dict[str, Any],
+        prefix: str = None,
+    ) -> Dict[str, Any]:
+        for key, field in storage_type.__fields__.items():
+
+            if key == '__root__':
+                continue
+
+            if field.alias:
+                key = field.alias
+
+            bigmap_name = key if prefix is None else f'{prefix}.{key}'
+
+            # NOTE: TzKT could return bigmaps as object or as array of key-value objects. We need to guess this from storage.
+            # TODO: This code should be a part of datasource module.
+            try:
+                value = storage[key]
+            except KeyError as e:
+                if not field.required:
+                    continue
+                raise ConfigurationError(f'Type `{storage_type.__name__}` is invalid: `{key}` field does not exists') from e
+
+            # FIXME: Pydantic bug. `BaseModel.type_` returns incorrect value when annotation is Dict[str, bool]
+            if field.type_ != field.outer_type_ and field.type_ == bool:
+                annotation = field.outer_type_
+            else:
+                annotation = field.type_
+
+            if annotation not in (int, bool) and isinstance(value, int):
+                if hasattr(annotation, '__fields__') and 'key' in annotation.__fields__ and 'value' in annotation.__fields__:
+                    storage[key] = []
+                    if self.diffs:
+                        self._merge_bigmapdiffs(storage, bigmap_name, array=True)
+                else:
+                    storage[key] = {}
+                    if self.diffs:
+                        self._merge_bigmapdiffs(storage, bigmap_name, array=False)
+            elif hasattr(annotation, '__fields__') and isinstance(storage[key], dict):
+                storage[key] = self._process_storage(annotation, storage[key], bigmap_name)
+
+        return storage
+
+    def get_merged_storage(self, storage_type: Type[StorageType]) -> StorageType:
+        """Merge big map diffs and deserialize raw storage into typeclass"""
+        if self.storage is None:
+            raise Exception('`storage` field missing')
+
+        storage = deepcopy(self.storage)
+        _logger.debug('Merging storage')
+        _logger.debug('Before: %s', storage)
+        _logger.debug('Diffs: %s', self.diffs)
+
+        storage = self._process_storage(storage_type, storage, None)
+
+        _logger.debug('After: %s', storage)
+
+        return storage_type.parse_obj(storage)
+
+
+@dataclass
+class Transaction(Generic[ParameterType, StorageType]):
+    """Wrapper for every transaction in handler arguments"""
+
+    data: OperationData
+    parameter: ParameterType
+    storage: StorageType
+
+
+@dataclass
+class Origination(Generic[StorageType]):
+    """Wrapper for every origination in handler arguments"""
+
+    data: OperationData
+    storage: StorageType
+
+
+class BigMapAction(Enum):
+    """Mapping for action in TzKT response"""
+
+    ALLOCATE = 'allocate'
+    ADD = 'add_key'
+    UPDATE = 'update_key'
+    REMOVE = 'remove_key'
+
+
+@dataclass
+class BigMapData:
+    """Basic structure for big map diffs from TzKT response"""
+
+    id: int
+    level: int
+    operation_id: int
+    timestamp: datetime
+    bigmap: int
+    contract_address: str
+    path: str
+    action: BigMapAction
+    key: Optional[Any] = None
+    value: Optional[Any] = None
+
+
+@dataclass
+class BigMapDiff(Generic[KeyType, ValueType]):
+    """Wrapper for every big map diff in each list of handler arguments"""
+
+    action: BigMapAction
+    data: BigMapData
+    key: KeyType
+    value: Optional[ValueType]
