56a94338b26227537171d33db0451e35a7c336e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package GenDoc/0.0.32/GenDoc-0.0.32.tar.gz

---
+++ packages/GenDoc/GenDoc-0.0.32.tar.gz/GenDoc-0.0.32/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="GenDoc",
+    version="0.0.32",
+    description="Generate a Markdown Documentation file from a Python Repository with DocStrings.",
+    #py_modules=["main"],
+    #package_dir={'':'gen_doc'},
+    packages = ["gen_doc"],
+    #install_requires = [],
+    entry_points = {
+        'console_scripts': [
+            'gendoc=gen_doc.main:main'
+        ]
+    },
+    url="https://github.com/http-samc/GenDoc",
+    author="Samarth Chitgopekar",
+    author_email="sam@chitgopekar.tech",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # classifiers = [
+    #     "Topic :: Utilities",
+    #     "Intended Audience :: Developers",
+    #     "Environment :: Console",
+    #     "License :: MIT License",
+    #     "Operating System :: Windows :: OSX :: Linux",
+    #     "Programming Language :: Python :: >= 3.2",
+    #     "Natural Language :: English :: EN"
+    # ]
+)
