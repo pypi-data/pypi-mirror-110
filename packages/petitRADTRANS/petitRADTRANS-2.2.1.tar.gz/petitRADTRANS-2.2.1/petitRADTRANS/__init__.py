4c8e65c4b1ad401f53f6f53d383fc36475072515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:57 +0000

    Add package petitRADTRANS/2.2.1/petitRADTRANS-2.2.1.tar.gz

---
+++ packages/petitRADTRANS/petitRADTRANS-2.2.1.tar.gz/petitRADTRANS-2.2.1/petitRADTRANS/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+__author__ = "Paul Molliere"
+__copyright__ = "Copyright 2016-2018, Paul Molliere"
+__maintainer__ = "Paul Molliere"
+__email__ = "molliere@strw.leidenunivl.nl"
+__status__ = "Development"
+__version__ = "2.1.0"
+
+from petitRADTRANS.radtrans import Radtrans
+from petitRADTRANS.retrieval.retrieval import Retrieval
+from petitRADTRANS.retrieval.retrieval_config import RetrievalConfig
+from petitRADTRANS.retrieval.data import Data
+from petitRADTRANS.retrieval.parameter import Parameter
+
+
