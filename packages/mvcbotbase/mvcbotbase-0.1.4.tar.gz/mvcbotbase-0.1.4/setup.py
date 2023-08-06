ceacfb8011d9218c3294398f44462a99b0545494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package mvcbotbase/0.1.4/mvcbotbase-0.1.4.tar.gz

---
+++ packages/mvcbotbase/mvcbotbase-0.1.4.tar.gz/mvcbotbase-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="mvcbotbase",
+    version="0.1.4",
+    packages=["mvcbotbase"],
+    download_url=(
+        "https://github.com/megahomyak/mvcbotbase/archive/v0.1.4.tar.gz"
+    ),
+    url="https://github.com/megahomyak/mvcbotbase",
+    author="megahomyak",
+    author_email="g.megahomyak@gmail.com",
+    description="A base to make social network bots (generic)",
+    install_requires=["simple_avk"]
+)
