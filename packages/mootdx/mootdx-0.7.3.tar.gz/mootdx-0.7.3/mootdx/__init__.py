e34665ebda2d50117af2767615070be57dedea66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:29 +0000

    Add package mootdx/0.7.3/mootdx-0.7.3.tar.gz

---
+++ packages/mootdx/mootdx-0.7.3.tar.gz/mootdx-0.7.3/mootdx/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from mootdx import config
+from mootdx.consts import EX_HOSTS, GP_HOSTS, HQ_HOSTS
+
+__version__ = '0.7.3'
+__author__ = "bopo.wang<ibopo@126.com>"
+
+CONFIG = {
+    'SERVER': {
+        'HQ': HQ_HOSTS,
+        'EX': EX_HOSTS,
+        'GP': GP_HOSTS
+    },
+    'BESTIP': {
+        'HQ': '',
+        'EX': '',
+        'GP': ''
+    },
+    'TDXDIR': 'C:/new_tdx',
+}
+
+
+def bestip():
+    return
+
+
+config.setup()
