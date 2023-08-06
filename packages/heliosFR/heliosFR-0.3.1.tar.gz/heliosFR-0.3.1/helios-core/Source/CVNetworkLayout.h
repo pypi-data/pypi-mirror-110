aa0c7ce73bd619d7ffde1b8710da401c28834279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package heliosFR/0.3.1/heliosFR-0.3.1.tar.gz

---
+++ packages/heliosFR/heliosFR-0.3.1.tar.gz/heliosFR-0.3.1/helios-core/Source/CVNetworkLayout.h
@@ -0,0 +1,10 @@
+#ifndef CVNetwork_CVNetworkLayout_h
+#define CVNetwork_CVNetworkLayout_h
+
+#include "CVCommons.h"
+void CVNetworkIteratePositions(CVIndex* edges, float* R, float* dR,
+	CVSize edgesCount, CVSize verticesCount, CVSize iterations,
+	CVFloat attractiveConstant,CVFloat repulsiveConstant,CVFloat viscosityConstant,
+	CVInteger maxWorkers, CVInteger updateInterval);
+  
+#endif
