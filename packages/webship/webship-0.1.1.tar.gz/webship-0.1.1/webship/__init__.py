488b6d0018f413007d66c3973339d32ead5cf1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:01 +0000

    Add package webship/0.1.1/webship-0.1.1.tar.gz

---
+++ packages/webship/webship-0.1.1.tar.gz/webship-0.1.1/webship/__init__.py
@@ -0,0 +1,19 @@
+import os
+import configparser
+
+from invoke import Collection, Program
+from invoke.util import yaml
+from webship import tasks
+
+interpolation = configparser.ExtendedInterpolation()
+config = configparser.ConfigParser(interpolation=interpolation)
+try:
+    config.read("webship.ini")
+except Exception as e:
+    print(e)
+    config = {"fetch": {}, "build": {}, "deploy": {}}
+
+print(config)
+ns = Collection.from_module(tasks)
+ns.configure({"webship": {s:dict(config.items(s)) for s in config.sections()}})
+program = Program(namespace=ns, version='0.1.0')
