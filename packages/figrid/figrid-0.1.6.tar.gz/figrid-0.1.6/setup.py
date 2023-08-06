b1029ff9e1f829ec4ea90704e468d75c6150b8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:34 +0000

    Add package figrid/0.1.6/figrid-0.1.6.tar.gz

---
+++ packages/figrid/figrid-0.1.6.tar.gz/figrid-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+'''
+figrid provides a set of convenience functions to make it easier to 
+place axes on a grid using matplotlib
+'''
+setup(name='figrid',
+    version='0.1.6',
+    packages=['figrid'],
+    include_package_data = True,
+    description='Formats multipanel figures',
+    url='https://github.com/dougollerenshaw/figrid',
+    author='Doug Ollerenshaw',
+    author_email="dougo@alleninstitute.org",
+    license='MIT',
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "seaborn"
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+  ],
+)
