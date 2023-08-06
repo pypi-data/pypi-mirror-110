f5a91d6e8066d6304e5465705ba2f53eb9435930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:57 +0000

    Add package AreaVolume/1.0.2/AreaVolume-1.0.2.tar.gz

---
+++ packages/AreaVolume/AreaVolume-1.0.2.tar.gz/AreaVolume-1.0.2/AreaVolume/ComputeCylinderVolume.py
@@ -0,0 +1,4 @@
+import math
+
+def compute_cylinder_volume(radius, height):
+    return math.pi * radius^2 * height
---
+++ packages/AreaVolume/AreaVolume-1.0.2.tar.gz/AreaVolume-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.2' 
+DESCRIPTION = 'AreaVolume Python package'
+LONG_DESCRIPTION = 'has useful methods for area, volume computations'
+
+setup(
+       # the name must match the folder name 'AreaVolume'
+        name="AreaVolume", 
+        version=VERSION,
+        author="Ausif Mahmood",
+        author_email="ausif@xyz.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # specify any additional packages that 
+        # need to be installed along with your package. Eg: 'numpy'
+        
+        keywords=['python', 'area', 'volume'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
