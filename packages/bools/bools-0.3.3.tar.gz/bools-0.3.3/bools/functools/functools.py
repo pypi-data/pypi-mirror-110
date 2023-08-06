fa4e7b4c0d3248b29cd56c9422f73002bf9ca9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:33 +0000

    Add package bools/0.3.3/bools-0.3.3.tar.gz

---
+++ packages/bools/bools-0.3.3.tar.gz/bools-0.3.3/bools/functools/functools.py
@@ -0,0 +1,63 @@
+from functools import wraps
+from typing import Union, Tuple
+
+from bools.log import Logger
+from bools.datetime import Datetime
+
+
+def catch(_func=None, *, exception: Union[Tuple[type(Exception), ...], type(Exception)] = Exception,
+          except_func=print, except_return=None, log=''):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except exception:
+                import traceback
+                except_func()
+                Logger.error(log)
+                print(traceback.print_exc())
+                return except_return
+
+        return wrapper
+
+    return decorator(_func) if _func else decorator
+
+
+def timeit(_func=None, *, count=3, return_costs=False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*arg, **kwargs):
+            current, seconds = Datetime.now().timestamp(), []
+            for i in range(count):
+                func(*arg, **kwargs)
+                now = Datetime.now().timestamp()
+                seconds.append(now - current)
+                current = now
+            Logger.info(f'平均执行时间: {sum(seconds) / count:.3f}s')
+            if return_costs:
+                return seconds
+
+        return wrapper
+
+    return decorator(_func) if _func else decorator
+
+
+def parallel(func=None, count: int = None):
+    setattr(_outer_func, _FUNC, func)
+
+    def inner(data: list):
+        from multiprocessing import Pool
+        with Pool(count) as p:
+            result = p.map(_outer_func, data)
+        return result
+
+    return inner
+
+
+_FUNC = 'func'
+
+
+def _outer_func(*args, **kwargs):
+    func = getattr(_outer_func, _FUNC)
+    return func(*args, **kwargs) if func.__code__.co_argcount else func()
