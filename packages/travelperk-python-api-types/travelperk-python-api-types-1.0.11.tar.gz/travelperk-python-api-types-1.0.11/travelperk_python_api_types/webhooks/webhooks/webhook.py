a4a24644cf2b612567490f2f3c39b1e4e02a1eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:33 +0000

    Add package travelperk-python-api-types/1.0.11/travelperk-python-api-types-1.0.11.tar.gz

---
+++ packages/travelperk-python-api-types/travelperk-python-api-types-1.0.11.tar.gz/travelperk-python-api-types-1.0.11/travelperk_python_api_types/webhooks/webhooks/webhook.py
@@ -0,0 +1,16 @@
+from typing import List
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class Webhook:
+    id: str
+    name: str
+    url: str
+    secret: str
+    enabled: bool
+    status: str
+    events: List[str]
+    successfully_sent: int
+    failed_sent: int
+    error_rate: float
