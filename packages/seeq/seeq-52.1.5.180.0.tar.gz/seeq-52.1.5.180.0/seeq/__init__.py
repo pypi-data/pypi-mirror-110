c0de45c958f2acc765ce4b423a66a0bae6043eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:47 +0000

    Add package seeq/52.1.5.180.0/seeq-52.1.5.180.0.tar.gz

---
+++ packages/seeq/seeq-52.1.5.180.0.tar.gz/seeq-52.1.5.180.0/seeq/__init__.py
@@ -0,0 +1,3 @@
+name = "seeq"
+__version__ = '%d.%d.%d.%d.%d' % (int('52'), int('1'), int('5'),
+                                  int('180'), int('0'))
---
+++ packages/seeq/seeq-52.1.5.180.0.tar.gz/seeq-52.1.5.180.0/seeq/spy/jobs/__init__.py
@@ -0,0 +1,5 @@
+from ._pull import pull
+from ._push import push
+from ._schedule import schedule, unschedule
+
+__all__ = ['push', 'pull', 'schedule', 'unschedule']
