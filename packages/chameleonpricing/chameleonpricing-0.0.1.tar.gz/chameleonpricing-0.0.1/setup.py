8d0dab3a936626604f6e760ecd3e6a9dad8d96f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:36 +0000

    Add package chameleonpricing/0.0.1/chameleonpricing-0.0.1.tar.gz

---
+++ packages/chameleonpricing/chameleonpricing-0.0.1.tar.gz/chameleonpricing-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="chameleonpricing",
+    version="0.0.1",
+    author="mikelvallejo",
+    author_email="mikelvallejo9@gmail.com",
+    description="Price recommender system",
+    url="https://github.com/mikelvallejo/chameleon-pricing",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
