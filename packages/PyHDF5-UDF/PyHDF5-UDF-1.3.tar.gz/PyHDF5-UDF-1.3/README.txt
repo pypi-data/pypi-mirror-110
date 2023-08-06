b519d2705d9ed6da4467d7b824df254a34f629b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:16 +0000

    Add package PyHDF5-UDF/1.3/PyHDF5-UDF-1.3.tar.gz

---
+++ packages/PyHDF5-UDF/PyHDF5-UDF-1.3.tar.gz/PyHDF5-UDF-1.3/README.txt
@@ -0,0 +1,14 @@
+=================================================
+User-Defined Functions for HDF5 - Python bindings
+=================================================
+
+HDF5 is a scientific file format that enables the storage and retrieval of
+static datasets. HDF5-UDF enables the attachment of user-defined routines to
+HDF5 files so that datasets can be produced on-the-fly by executing such
+routines when the application reads the data. HDF5-UDF provides bindings for
+the HDF5-UDF C/C++ library.
+
+Please visit the `HDF5-UDF <https://hdf5-udf.readthedocs.io>`_
+page for instructions on how to use this library and its page on
+`GitHub <https://github.com/lucasvr/hdf5-udf>`_ for more details on the
+infrastructure behind it.
---
+++ packages/PyHDF5-UDF/PyHDF5-UDF-1.3.tar.gz/PyHDF5-UDF-1.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="PyHDF5-UDF",
+    version="1.3",
+    description="User-defined functions for HDF5 - Python bindings",
+    description_content_type="text/x-rst",
+    long_description=open("README.txt").read(),
+    long_description_content_type="text/x-rst",
+    author="Lucas C. Villa Real",
+    author_email="lucasvr@gobolinux.org",
+    url="https://hdf5-udf.readthedocs.io",
+
+    py_modules=["hdf5_udf"],
+    python_requires=">=3.5.10",
+    setup_requires=[
+        "wheel",
+        "cffi>=1.0.0"
+    ],
+    install_requires=[
+        "cffi>=1.0.0",
+        "jsonschema>=3.2.0"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+
+    cffi_modules=["build_ext.py:ffibuilder"],
+    zip_safe=False,
+)
