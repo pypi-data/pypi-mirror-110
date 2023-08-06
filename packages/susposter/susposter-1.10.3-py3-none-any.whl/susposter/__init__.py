c0bfe7df1c31019f601e423be9fe5588941518f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package susposter/1.10.3/susposter-1.10.3-py3-none-any.whl

---
+++ packages/susposter/susposter-1.10.3-py3-none-any.whl/susposter/__init__.py
@@ -0,0 +1,13 @@
+from io import BytesIO
+
+__version__ = "1.10.3"
+
+def sussy():
+  print("so sussy")
+  return "so sussy"
+
+def sus():
+  return BytesIO(open("susposter.png", "rb").read())
+
+def amongus():
+  return BytesIO(open("sussy.png", "rb").read())
