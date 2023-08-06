4a7b40c3c5eefe3cd9b34a898431fdf09a7aa294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:53 +0000

    Add package django-appchance-sections/0.1/django_appchance_sections-0.1-py3-none-any.whl

---
+++ packages/django-appchance-sections/django_appchance_sections-0.1-py3-none-any.whl/appchance_sections/urls.py
@@ -0,0 +1,8 @@
+from appchance_sections.views import SectionViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"sections", SectionViewSet)
+
+app_name = "appchance_sections"  # pylint: disable=C0103
+urlpatterns = router.urls
