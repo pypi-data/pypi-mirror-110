30a07913ff1c75df074dc591d737a5d735b8dacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:18 +0000

    Add package mypy-boto3-kms/1.17.98/mypy_boto3_kms-1.17.98-py3-none-any.whl

---
+++ packages/mypy-boto3-kms/mypy_boto3_kms-1.17.98-py3-none-any.whl/mypy_boto3_kms/type_defs.pyi
@@ -0,0 +1,474 @@
+"""
+Type annotations for kms service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_kms/type_defs.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_kms.type_defs import AliasListEntryTypeDef
+
+    data: AliasListEntryTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import IO, Dict, List, Union
+
+from .literals import (
+    ConnectionErrorCodeTypeType,
+    ConnectionStateTypeType,
+    CustomerMasterKeySpecType,
+    DataKeyPairSpecType,
+    EncryptionAlgorithmSpecType,
+    ExpirationModelTypeType,
+    GrantOperationType,
+    KeyManagerTypeType,
+    KeyStateType,
+    KeyUsageTypeType,
+    MultiRegionKeyTypeType,
+    OriginTypeType,
+    SigningAlgorithmSpecType,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "AliasListEntryTypeDef",
+    "CancelKeyDeletionResponseTypeDef",
+    "CreateCustomKeyStoreResponseTypeDef",
+    "CreateGrantResponseTypeDef",
+    "CreateKeyResponseTypeDef",
+    "CustomKeyStoresListEntryTypeDef",
+    "DecryptResponseTypeDef",
+    "DescribeCustomKeyStoresResponseTypeDef",
+    "DescribeKeyResponseTypeDef",
+    "EncryptResponseTypeDef",
+    "GenerateDataKeyPairResponseTypeDef",
+    "GenerateDataKeyPairWithoutPlaintextResponseTypeDef",
+    "GenerateDataKeyResponseTypeDef",
+    "GenerateDataKeyWithoutPlaintextResponseTypeDef",
+    "GenerateRandomResponseTypeDef",
+    "GetKeyPolicyResponseTypeDef",
+    "GetKeyRotationStatusResponseTypeDef",
+    "GetParametersForImportResponseTypeDef",
+    "GetPublicKeyResponseTypeDef",
+    "GrantConstraintsTypeDef",
+    "GrantListEntryTypeDef",
+    "KeyListEntryTypeDef",
+    "KeyMetadataTypeDef",
+    "ListAliasesResponseTypeDef",
+    "ListGrantsResponseTypeDef",
+    "ListKeyPoliciesResponseTypeDef",
+    "ListKeysResponseTypeDef",
+    "ListResourceTagsResponseTypeDef",
+    "MultiRegionConfigurationTypeDef",
+    "MultiRegionKeyTypeDef",
+    "PaginatorConfigTypeDef",
+    "ReEncryptResponseTypeDef",
+    "ReplicateKeyResponseTypeDef",
+    "ScheduleKeyDeletionResponseTypeDef",
+    "SignResponseTypeDef",
+    "TagTypeDef",
+    "VerifyResponseTypeDef",
+)
+
+AliasListEntryTypeDef = TypedDict(
+    "AliasListEntryTypeDef",
+    {
+        "AliasName": str,
+        "AliasArn": str,
+        "TargetKeyId": str,
+        "CreationDate": datetime,
+        "LastUpdatedDate": datetime,
+    },
+    total=False,
+)
+
+CancelKeyDeletionResponseTypeDef = TypedDict(
+    "CancelKeyDeletionResponseTypeDef",
+    {
+        "KeyId": str,
+    },
+    total=False,
+)
+
+CreateCustomKeyStoreResponseTypeDef = TypedDict(
+    "CreateCustomKeyStoreResponseTypeDef",
+    {
+        "CustomKeyStoreId": str,
+    },
+    total=False,
+)
+
+CreateGrantResponseTypeDef = TypedDict(
+    "CreateGrantResponseTypeDef",
+    {
+        "GrantToken": str,
+        "GrantId": str,
+    },
+    total=False,
+)
+
+CreateKeyResponseTypeDef = TypedDict(
+    "CreateKeyResponseTypeDef",
+    {
+        "KeyMetadata": "KeyMetadataTypeDef",
+    },
+    total=False,
+)
+
+CustomKeyStoresListEntryTypeDef = TypedDict(
+    "CustomKeyStoresListEntryTypeDef",
+    {
+        "CustomKeyStoreId": str,
+        "CustomKeyStoreName": str,
+        "CloudHsmClusterId": str,
+        "TrustAnchorCertificate": str,
+        "ConnectionState": ConnectionStateTypeType,
+        "ConnectionErrorCode": ConnectionErrorCodeTypeType,
+        "CreationDate": datetime,
+    },
+    total=False,
+)
+
+DecryptResponseTypeDef = TypedDict(
+    "DecryptResponseTypeDef",
+    {
+        "KeyId": str,
+        "Plaintext": Union[bytes, IO[bytes]],
+        "EncryptionAlgorithm": EncryptionAlgorithmSpecType,
+    },
+    total=False,
+)
+
+DescribeCustomKeyStoresResponseTypeDef = TypedDict(
+    "DescribeCustomKeyStoresResponseTypeDef",
+    {
+        "CustomKeyStores": List["CustomKeyStoresListEntryTypeDef"],
+        "NextMarker": str,
+        "Truncated": bool,
+    },
+    total=False,
+)
+
+DescribeKeyResponseTypeDef = TypedDict(
+    "DescribeKeyResponseTypeDef",
+    {
+        "KeyMetadata": "KeyMetadataTypeDef",
+    },
+    total=False,
+)
+
+EncryptResponseTypeDef = TypedDict(
+    "EncryptResponseTypeDef",
+    {
+        "CiphertextBlob": Union[bytes, IO[bytes]],
+        "KeyId": str,
+        "EncryptionAlgorithm": EncryptionAlgorithmSpecType,
+    },
+    total=False,
+)
+
+GenerateDataKeyPairResponseTypeDef = TypedDict(
+    "GenerateDataKeyPairResponseTypeDef",
+    {
+        "PrivateKeyCiphertextBlob": Union[bytes, IO[bytes]],
+        "PrivateKeyPlaintext": Union[bytes, IO[bytes]],
+        "PublicKey": Union[bytes, IO[bytes]],
+        "KeyId": str,
+        "KeyPairSpec": DataKeyPairSpecType,
+    },
+    total=False,
+)
+
+GenerateDataKeyPairWithoutPlaintextResponseTypeDef = TypedDict(
+    "GenerateDataKeyPairWithoutPlaintextResponseTypeDef",
+    {
+        "PrivateKeyCiphertextBlob": Union[bytes, IO[bytes]],
+        "PublicKey": Union[bytes, IO[bytes]],
+        "KeyId": str,
+        "KeyPairSpec": DataKeyPairSpecType,
+    },
+    total=False,
+)
+
+GenerateDataKeyResponseTypeDef = TypedDict(
+    "GenerateDataKeyResponseTypeDef",
+    {
+        "CiphertextBlob": Union[bytes, IO[bytes]],
+        "Plaintext": Union[bytes, IO[bytes]],
+        "KeyId": str,
+    },
+    total=False,
+)
+
+GenerateDataKeyWithoutPlaintextResponseTypeDef = TypedDict(
+    "GenerateDataKeyWithoutPlaintextResponseTypeDef",
+    {
+        "CiphertextBlob": Union[bytes, IO[bytes]],
+        "KeyId": str,
+    },
+    total=False,
+)
+
+GenerateRandomResponseTypeDef = TypedDict(
+    "GenerateRandomResponseTypeDef",
+    {
+        "Plaintext": Union[bytes, IO[bytes]],
+    },
+    total=False,
+)
+
+GetKeyPolicyResponseTypeDef = TypedDict(
+    "GetKeyPolicyResponseTypeDef",
+    {
+        "Policy": str,
+    },
+    total=False,
+)
+
+GetKeyRotationStatusResponseTypeDef = TypedDict(
+    "GetKeyRotationStatusResponseTypeDef",
+    {
+        "KeyRotationEnabled": bool,
+    },
+    total=False,
+)
+
+GetParametersForImportResponseTypeDef = TypedDict(
+    "GetParametersForImportResponseTypeDef",
+    {
+        "KeyId": str,
+        "ImportToken": Union[bytes, IO[bytes]],
+        "PublicKey": Union[bytes, IO[bytes]],
+        "ParametersValidTo": datetime,
+    },
+    total=False,
+)
+
+GetPublicKeyResponseTypeDef = TypedDict(
+    "GetPublicKeyResponseTypeDef",
+    {
+        "KeyId": str,
+        "PublicKey": Union[bytes, IO[bytes]],
+        "CustomerMasterKeySpec": CustomerMasterKeySpecType,
+        "KeyUsage": KeyUsageTypeType,
+        "EncryptionAlgorithms": List[EncryptionAlgorithmSpecType],
+        "SigningAlgorithms": List[SigningAlgorithmSpecType],
+    },
+    total=False,
+)
+
+GrantConstraintsTypeDef = TypedDict(
+    "GrantConstraintsTypeDef",
+    {
+        "EncryptionContextSubset": Dict[str, str],
+        "EncryptionContextEquals": Dict[str, str],
+    },
+    total=False,
+)
+
+GrantListEntryTypeDef = TypedDict(
+    "GrantListEntryTypeDef",
+    {
+        "KeyId": str,
+        "GrantId": str,
+        "Name": str,
+        "CreationDate": datetime,
+        "GranteePrincipal": str,
+        "RetiringPrincipal": str,
+        "IssuingAccount": str,
+        "Operations": List[GrantOperationType],
+        "Constraints": "GrantConstraintsTypeDef",
+    },
+    total=False,
+)
+
+KeyListEntryTypeDef = TypedDict(
+    "KeyListEntryTypeDef",
+    {
+        "KeyId": str,
+        "KeyArn": str,
+    },
+    total=False,
+)
+
+_RequiredKeyMetadataTypeDef = TypedDict(
+    "_RequiredKeyMetadataTypeDef",
+    {
+        "KeyId": str,
+    },
+)
+_OptionalKeyMetadataTypeDef = TypedDict(
+    "_OptionalKeyMetadataTypeDef",
+    {
+        "AWSAccountId": str,
+        "Arn": str,
+        "CreationDate": datetime,
+        "Enabled": bool,
+        "Description": str,
+        "KeyUsage": KeyUsageTypeType,
+        "KeyState": KeyStateType,
+        "DeletionDate": datetime,
+        "ValidTo": datetime,
+        "Origin": OriginTypeType,
+        "CustomKeyStoreId": str,
+        "CloudHsmClusterId": str,
+        "ExpirationModel": ExpirationModelTypeType,
+        "KeyManager": KeyManagerTypeType,
+        "CustomerMasterKeySpec": CustomerMasterKeySpecType,
+        "EncryptionAlgorithms": List[EncryptionAlgorithmSpecType],
+        "SigningAlgorithms": List[SigningAlgorithmSpecType],
+        "MultiRegion": bool,
+        "MultiRegionConfiguration": "MultiRegionConfigurationTypeDef",
+        "PendingDeletionWindowInDays": int,
+    },
+    total=False,
+)
+
+class KeyMetadataTypeDef(_RequiredKeyMetadataTypeDef, _OptionalKeyMetadataTypeDef):
+    pass
+
+ListAliasesResponseTypeDef = TypedDict(
+    "ListAliasesResponseTypeDef",
+    {
+        "Aliases": List["AliasListEntryTypeDef"],
+        "NextMarker": str,
+        "Truncated": bool,
+    },
+    total=False,
+)
+
+ListGrantsResponseTypeDef = TypedDict(
+    "ListGrantsResponseTypeDef",
+    {
+        "Grants": List["GrantListEntryTypeDef"],
+        "NextMarker": str,
+        "Truncated": bool,
+    },
+    total=False,
+)
+
+ListKeyPoliciesResponseTypeDef = TypedDict(
+    "ListKeyPoliciesResponseTypeDef",
+    {
+        "PolicyNames": List[str],
+        "NextMarker": str,
+        "Truncated": bool,
+    },
+    total=False,
+)
+
+ListKeysResponseTypeDef = TypedDict(
+    "ListKeysResponseTypeDef",
+    {
+        "Keys": List["KeyListEntryTypeDef"],
+        "NextMarker": str,
+        "Truncated": bool,
+    },
+    total=False,
+)
+
+ListResourceTagsResponseTypeDef = TypedDict(
+    "ListResourceTagsResponseTypeDef",
+    {
+        "Tags": List["TagTypeDef"],
+        "NextMarker": str,
+        "Truncated": bool,
+    },
+    total=False,
+)
+
+MultiRegionConfigurationTypeDef = TypedDict(
+    "MultiRegionConfigurationTypeDef",
+    {
+        "MultiRegionKeyType": MultiRegionKeyTypeType,
+        "PrimaryKey": "MultiRegionKeyTypeDef",
+        "ReplicaKeys": List["MultiRegionKeyTypeDef"],
+    },
+    total=False,
+)
+
+MultiRegionKeyTypeDef = TypedDict(
+    "MultiRegionKeyTypeDef",
+    {
+        "Arn": str,
+        "Region": str,
+    },
+    total=False,
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": int,
+        "PageSize": int,
+        "StartingToken": str,
+    },
+    total=False,
+)
+
+ReEncryptResponseTypeDef = TypedDict(
+    "ReEncryptResponseTypeDef",
+    {
+        "CiphertextBlob": Union[bytes, IO[bytes]],
+        "SourceKeyId": str,
+        "KeyId": str,
+        "SourceEncryptionAlgorithm": EncryptionAlgorithmSpecType,
+        "DestinationEncryptionAlgorithm": EncryptionAlgorithmSpecType,
+    },
+    total=False,
+)
+
+ReplicateKeyResponseTypeDef = TypedDict(
+    "ReplicateKeyResponseTypeDef",
+    {
+        "ReplicaKeyMetadata": "KeyMetadataTypeDef",
+        "ReplicaPolicy": str,
+        "ReplicaTags": List["TagTypeDef"],
+    },
+    total=False,
+)
+
+ScheduleKeyDeletionResponseTypeDef = TypedDict(
+    "ScheduleKeyDeletionResponseTypeDef",
+    {
+        "KeyId": str,
+        "DeletionDate": datetime,
+        "KeyState": KeyStateType,
+        "PendingWindowInDays": int,
+    },
+    total=False,
+)
+
+SignResponseTypeDef = TypedDict(
+    "SignResponseTypeDef",
+    {
+        "KeyId": str,
+        "Signature": Union[bytes, IO[bytes]],
+        "SigningAlgorithm": SigningAlgorithmSpecType,
+    },
+    total=False,
+)
+
+TagTypeDef = TypedDict(
+    "TagTypeDef",
+    {
+        "TagKey": str,
+        "TagValue": str,
+    },
+)
+
+VerifyResponseTypeDef = TypedDict(
+    "VerifyResponseTypeDef",
+    {
+        "KeyId": str,
+        "SignatureValid": bool,
+        "SigningAlgorithm": SigningAlgorithmSpecType,
+    },
+    total=False,
+)
