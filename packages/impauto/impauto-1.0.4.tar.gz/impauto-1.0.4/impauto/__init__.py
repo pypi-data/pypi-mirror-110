32f9dafc1022cd7410250fb87d93fcef28ec4aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package impauto/1.0.4/impauto-1.0.4.tar.gz

---
+++ packages/impauto/impauto-1.0.4.tar.gz/impauto-1.0.4/impauto/__init__.py
@@ -0,0 +1,30 @@
+import builtins
+
+author = "Sigmanificient"
+version = "1.0.1"
+
+
+class Automated:
+
+    def __init__(self, *args, show_message=True, forever=False):
+        self.values = self.__generate_values(args, forever)
+        self.show_message = show_message
+
+        builtins.input = self.__input__
+
+    @staticmethod
+    def __generate_values(values, forever):
+        values = map(str, values)
+        once = 1
+
+        while forever or once:
+            once -= 1
+
+            yield from values
+        raise StopIteration("No more input values given. Make sure to have enough values passed")
+
+    def __input__(self, __prompt=None):
+        value = next(self.values)
+        if self.show_message:
+            print(f"[Automated] {__prompt}{value}")
+        return str(value)
