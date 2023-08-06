55f8347258180a8f3c41ee30a08614e266ea5587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package treesum/0.3.2/treesum-0.3.2.tar.gz

---
+++ packages/treesum/treesum-0.3.2.tar.gz/treesum-0.3.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from pathlib import Path
+
+setup(
+    name="treesum",
+    version="0.3.2",
+    author="Azat Akhmetov",
+    author_email="azatinfo@yandex.com",
+    description="Utility and library for summarizing trees.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/metov/treesum",
+    classifiers=["Development Status :: 3 - Alpha"],
+    install_requires=[
+        "docopt",
+    ],
+    entry_points={
+        "console_scripts": ["treesum=treesum.cli:main"],
+    },
+)
