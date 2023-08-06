ba36958f2cd4baba3202d301743775e1bcd4b691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:58 +0000

    Add package hacs-frontend/20210620130608/hacs-frontend-20210620130608.tar.gz

---
+++ packages/hacs-frontend/hacs-frontend-20210620130608.tar.gz/hacs-frontend-20210620130608/hacs_frontend/c.30ed4711.js
@@ -0,0 +1,14 @@
+import{_ as i,H as e,e as t,T as s,n as o}from"./main-ef294d92.js";import"./c.773db748.js";import"./c.94568445.js";import"./c.c05acf21.js";let c=i([o("hacs-progress-dialog")],(function(i,e){return{F:class extends e{constructor(...e){super(...e),i(this)}},d:[{kind:"field",decorators:[t()],key:"title",value:void 0},{kind:"field",decorators:[t()],key:"content",value:void 0},{kind:"field",decorators:[t()],key:"confirmText",value:void 0},{kind:"field",decorators:[t()],key:"confirm",value:void 0},{kind:"field",decorators:[t({type:Boolean})],key:"_inProgress",value:()=>!1},{kind:"method",key:"shouldUpdate",value:function(i){return i.has("active")||i.has("title")||i.has("content")||i.has("confirmText")||i.has("confirm")||i.has("_inProgress")}},{kind:"method",key:"render",value:function(){return this.active?s`
+      <hacs-dialog .active=${this.active} .hass=${this.hass} title=${this.title||""}>
+        <div class="content">
+          ${this.content||""}
+        </div>
+        <mwc-button slot="secondaryaction" ?disabled=${this._inProgress} @click=${this._close}>
+          ${this.hacs.localize("common.cancel")}
+        </mwc-button>
+        <mwc-button slot="primaryaction" @click=${this._confirmed}>
+          ${this._inProgress?s`<ha-circular-progress active size="small"></ha-circular-progress>`:this.confirmText||this.hacs.localize("confirm.yes")}</mwc-button
+          >
+        </mwc-button>
+      </hacs-dialog>
+    `:s``}},{kind:"method",key:"_confirmed",value:async function(){this._inProgress=!0,await this.confirm(),this._inProgress=!1,this._close()}},{kind:"method",key:"_close",value:function(){this.active=!1,this.dispatchEvent(new Event("hacs-dialog-closed",{bubbles:!0,composed:!0}))}}]}}),e);export{c as HacsProgressDialog};
