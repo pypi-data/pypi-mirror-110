5ab1d2204af1b18f138025c8050a54c038d07254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:49 +0000

    Add package varname/0.6.5/varname-0.6.5-py3-none-any.whl

---
+++ packages/varname/varname-0.6.5-py3-none-any.whl/varname/__init__.py
@@ -0,0 +1,14 @@
+"""Dark magics about variable names in python"""
+# pylint: disable=unused-import
+from .utils import (
+    config,
+    VarnameRetrievingError,
+    ImproperUseError,
+    QualnameNonUniqueError,
+    NonVariableArgumentError,
+    MultiTargetAssignmentWarning,
+    MaybeDecoratedFunctionWarning
+)
+from .core import varname, nameof, will, argname
+
+__version__ = "0.6.5"
