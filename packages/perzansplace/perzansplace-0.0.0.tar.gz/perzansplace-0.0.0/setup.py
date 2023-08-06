f44410dc2c9778f084f7d4ac969a7d89f75f909e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:00 +0000

    Add package perzansplace/0.0.0/perzansplace-0.0.0.tar.gz

---
+++ packages/perzansplace/perzansplace-0.0.0.tar.gz/perzansplace-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='perzansplace',
+    packages=["perzansplace"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
