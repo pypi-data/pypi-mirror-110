e47acc0164c2dcb4f62b263fe056ba6b6456b9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:26 +0000

    Add package nwb-conversion-tools/0.8.1/nwb-conversion-tools-0.8.1.tar.gz

---
+++ packages/nwb-conversion-tools/nwb-conversion-tools-0.8.1.tar.gz/nwb-conversion-tools-0.8.1/nwb_conversion_tools/datainterfaces/__init__.py
@@ -0,0 +1,68 @@
+from .ecephys.tutorial.ecephystutorialdatainterface import (
+    TutorialRecordingInterface,
+    TutorialSortingInterface
+)
+from .ecephys.neuroscope.neuroscopedatainterface import (
+    NeuroscopeRecordingInterface,
+    NeuroscopeLFPInterface,
+    NeuroscopeMultiRecordingTimeInterface,
+    NeuroscopeSortingInterface
+)
+from .ecephys.spikeglx.spikeglxdatainterface import SpikeGLXRecordingInterface, SpikeGLXLFPInterface
+from .ecephys.spikeinterface.sipickledatainterfaces import (
+    SIPickleRecordingExtractorInterface,
+    SIPickleSortingExtractorInterface
+)
+from .ecephys.intan.intandatainterface import IntanRecordingInterface
+from .ecephys.ced.ceddatainterface import CEDRecordingInterface
+from .ecephys.cellexplorer.cellexplorerdatainterface import CellExplorerSortingInterface
+from .ecephys.blackrock.blackrockdatainterface import BlackrockRecordingExtractorInterface, BlackrockSortingExtractorInterface
+from .ecephys.openephys.openephysdatainterface import OpenEphysRecordingExtractorInterface, OpenEphysSortingExtractorInterface
+from .ecephys.axona.axonadatainterface import (
+    AxonaRecordingExtractorInterface,
+    AxonaPositionDataInterface
+)
+
+from .ophys.caiman.caimandatainterface import CaimanSegmentationInterface
+from .ophys.cnmfe.cnmfedatainterface import CnmfeSegmentationInterface
+from .ophys.suite2p.suite2pdatainterface import Suite2pSegmentationInterface
+from .ophys.extract.extractdatainterface import ExtractSegmentationInterface
+from .ophys.sima.simadatainterface import SimaSegmentationInterface
+
+from .ophys.sbx.sbxdatainterface import SbxImagingInterface
+from .ophys.tiff.tiffdatainterface import TiffImagingInterface
+from .ophys.hdf5.hdf5datainterface import Hdf5ImagingInterface
+
+from .behavior.movie.moviedatainterface import MovieInterface
+
+
+interface_list = [
+    TutorialRecordingInterface,
+    TutorialSortingInterface,
+    NeuroscopeRecordingInterface,
+    NeuroscopeMultiRecordingTimeInterface,
+    NeuroscopeSortingInterface,
+    NeuroscopeLFPInterface,
+    SpikeGLXRecordingInterface,
+    SpikeGLXLFPInterface,
+    SIPickleRecordingExtractorInterface,
+    SIPickleSortingExtractorInterface,
+    IntanRecordingInterface,
+    CEDRecordingInterface,
+    CellExplorerSortingInterface,
+    BlackrockRecordingExtractorInterface,
+    BlackrockSortingExtractorInterface,
+    OpenEphysRecordingExtractorInterface,
+    OpenEphysSortingExtractorInterface,
+    AxonaRecordingExtractorInterface,
+    AxonaPositionDataInterface,
+    CaimanSegmentationInterface,
+    CnmfeSegmentationInterface,
+    Suite2pSegmentationInterface,
+    ExtractSegmentationInterface,
+    SimaSegmentationInterface,
+    SbxImagingInterface,
+    TiffImagingInterface,
+    Hdf5ImagingInterface,
+    MovieInterface
+]
