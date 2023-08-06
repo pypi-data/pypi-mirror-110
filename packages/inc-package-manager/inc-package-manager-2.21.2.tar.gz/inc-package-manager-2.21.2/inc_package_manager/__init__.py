1bda50973f323ef7456d3e30bc5c2fcb2ec53e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:34 +0000

    Add package inc-package-manager/2.21.2/inc-package-manager-2.21.2.tar.gz

---
+++ packages/inc-package-manager/inc-package-manager-2.21.2.tar.gz/inc-package-manager-2.21.2/inc_package_manager/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from dev0s.shortcuts import *
+sys.path.insert(1, dev0s.defaults.source_path(__file__, back=1))
+dev0s.system.env["INC_PACKAGE_MANAGER_API"] = True
+from apps.packages.packages import inc_package_manager
+
+# source path & version.
+from dev0s.shortcuts import Version, Directory, Files, gfp
+source = Directory(gfp.base(__file__))
+base = Directory(source.fp.base())
+version = Version(Files.load(source.join(".version")))
