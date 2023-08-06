e449d56bcf652c16aa406545a4b65ce3f4879e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:39 +0000

    Add package clear-skies/0.3.13/clear-skies-0.3.13.tar.gz

---
+++ packages/clear-skies/clear-skies-0.3.13.tar.gz/clear-skies-0.3.13/src/clearskies/contexts/test.py
@@ -0,0 +1,66 @@
+from ..authentication import public
+from ..mocks import InputOutput
+from ..di import StandardDependencies
+from ..backends import MemoryBackend
+from datetime import datetime, timezone
+from .build_context import build_context
+
+
+class Test:
+    _application = None
+    _handler = None
+    input_output = None
+    memory_backend = None
+    now = None
+
+    def __init__(self, di):
+        self.di = di
+
+    def configure(self, application):
+        self.now = datetime.now().replace(tzinfo=timezone.utc, microsecond=0)
+        self._application = application
+        self.input_output = InputOutput()
+        self.memory_backend = self.di.build(MemoryBackend, cache=False)
+        self.memory_backend.silent_on_missing_tables(silent=True)
+
+        self.di.bind('now', self.now)
+        self.di.bind('cursor_backend', self.memory_backend)
+
+    def __call__(self, method=None, body=None, headers=None, url=None):
+        if body is not None:
+            self.input_output.set_body(body)
+        if headers is not None:
+            self.input_output.set_request_headers(headers)
+        if method is not None:
+            self.input_output.set_request_method(method)
+        if url is not None:
+            self.input_output.set_request_url(url)
+
+        self._handler = self.di.build(self._application.handler_class, cache=False)
+        self._handler.configure({
+            **{'authentication': public()},
+            **self._application.handler_config,
+        })
+        return self._handler(self.input_output)
+
+    def bind(self, key, value):
+        self.di.bind(key, value)
+
+    def build(self, key):
+        return self.di.build(key)
+
+def test(
+    application,
+    di_class=StandardDependencies,
+    bindings=None,
+    binding_classes=None,
+    binding_modules=None
+):
+    return build_context(
+        Test,
+        application,
+        di_class,
+        bindings=bindings,
+        binding_classes=binding_classes,
+        binding_modules=binding_modules
+    )
