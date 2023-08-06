dbb70fc197ee2cd5e3802424dbec5fecee0f875f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:41 +0000

    Add package pytransform3d/1.9.1/pytransform3d-1.9.1.tar.gz

---
+++ packages/pytransform3d/pytransform3d-1.9.1.tar.gz/pytransform3d-1.9.1/pytransform3d/visualizer/__init__.py
@@ -0,0 +1,13 @@
+"""Optional 3D renderer based on Open3D's visualizer."""
+import warnings
+try:
+    import open3d as o3d
+    from ._artists import (Artist, Line3D, Frame, Trajectory, Camera, Box,
+                           Sphere, Cylinder, Mesh, Ellipsoid, Graph)
+    from ._figure import figure, Figure
+
+    __all__ = ["figure", "Figure", "Artist", "Line3D", "Frame", "Trajectory",
+               "Camera", "Box", "Sphere", "Cylinder", "Mesh", "Ellipsoid",
+               "Graph"]
+except ImportError:
+    warnings.warn("3D visualizer is not available. Install open3d.")
