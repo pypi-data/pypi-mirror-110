71ca2f64c152ffcd5c4f6c2ca89d31ab66d8b11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:21 +0000

    Add package thechef-filler/0.1.6/thechef_filler-0.1.6.tar.gz

---
+++ packages/thechef-filler/thechef_filler-0.1.6.tar.gz/thechef_filler-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['thechef_filler']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['thechef_filler = thechef_filler:main']}
+
+setup_kwargs = {
+    'name': 'thechef-filler',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'kohkubo',
+    'author_email': 'kohkubo@student.42tokyo.jp',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
