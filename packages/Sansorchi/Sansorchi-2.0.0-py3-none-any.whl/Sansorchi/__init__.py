4d352d18865380c337fabb26a33a74fb433d58fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:38 +0000

    Add package Sansorchi/2.0.0/Sansorchi-2.0.0-py3-none-any.whl

---
+++ packages/Sansorchi/Sansorchi-2.0.0-py3-none-any.whl/Sansorchi/__init__.py
@@ -0,0 +1,21 @@
+from .data import Json
+
+Json = Json["word"]
+
+
+def sansor(txt: str, bad=Json, **kwargs) -> str:
+    """ This function takes an obligatory input called
+    >>> txt and deletes it if there is an insult in it"""
+    split = txt.split(
+        " ")  # BUG: This creates bug line, Separate words from space.
+    # >>>>>>>>> Multi-part words are separated
+    a = ''
+    for i in split:
+        if i in bad:
+            badi = str(i[0]) + "**" + str(i[-1])
+            a += badi
+        else:
+            a += i
+        if split[-1] != i:
+            a += " "
+    return a
