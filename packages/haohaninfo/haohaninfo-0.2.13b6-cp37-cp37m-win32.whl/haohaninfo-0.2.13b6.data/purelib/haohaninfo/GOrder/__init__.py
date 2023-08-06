519b9185eab427296c772210a62eb44bf1cf2c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:45 +0000

    Add package haohaninfo/0.2.13b6/haohaninfo-0.2.13b6-cp37-cp37m-win32.whl

---
+++ packages/haohaninfo/haohaninfo-0.2.13b6-cp37-cp37m-win32.whl/haohaninfo-0.2.13b6.data/purelib/haohaninfo/GOrder/__init__.py
@@ -0,0 +1,3 @@
+from haohaninfo.GOrder.GOGetKBar import Login,GetIndicator,GetKBar as GetHistoryKBar,GetTAKBar,GetDFKBar,ResetTick
+from haohaninfo.GOrder.GOCommand import GOCommand
+from haohaninfo.GOrder.GOQuote import GOQuote
