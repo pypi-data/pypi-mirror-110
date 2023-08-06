ecc9d31fbe68af1039ff7e03e7b9ce186bae40e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package colourpick/1.0.0/colourpick-1.0.0.tar.gz

---
+++ packages/colourpick/colourpick-1.0.0.tar.gz/colourpick-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="colourpick",
+    version="1.0.0",
+    description="Calculation of the dominant (average) RGB value",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jay-chau/colourpick",
+    author="Jay Chau",
+    author_email="jay-chau@outlook.com",
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["src/colourpick"],
+    include_package_data=True,
+    install_requires=[
+        'opencv-python',
+        'scikit-learn',
+        'numpy',
+    ]
+)
