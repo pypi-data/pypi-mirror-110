1680895aa0f9341f3c7be6ea84532901e4a6c08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package potaru/0.3.0/potaru-0.3.0.tar.gz

---
+++ packages/potaru/potaru-0.3.0.tar.gz/potaru-0.3.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['potaru']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0', 'requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['potaru = potaru.__main__:cli']}
+
+setup_kwargs = {
+    'name': 'potaru',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Alon Gubkin',
+    'author_email': 'alon.gubkin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
