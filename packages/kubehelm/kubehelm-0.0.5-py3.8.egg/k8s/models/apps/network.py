c57e1a58dcd2dd2b5f8b40ecc646b0f9199d7706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:53 +0000

    Add package kubehelm/0.0.5/kubehelm-0.0.5-py3.8.egg

---
+++ packages/kubehelm/kubehelm-0.0.5-py3.8.egg/k8s/models/apps/network.py
@@ -0,0 +1,22 @@
+from k8s.models.scripts import RunAppScript
+from k8s.models.manifest import Manifest
+
+
+class Ingress(RunAppScript):
+    script_name = "ingress.bash"
+    allowed_methods = ["install", "update"]
+
+
+class Cert(RunAppScript):
+    script_name = "cert_manager.bash"
+    allowed_methods = ["install", "update"]
+
+
+class Issuerstaging(RunAppScript):
+    script_name = 'letsencrypt_staging.bash'
+    allowed_methods = ["install", "update"]
+
+
+class Issuerproduction(RunAppScript):
+    script_name = 'letsencrypt_production.bash'
+    allowed_methods = ["install", "update"]
